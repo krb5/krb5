d929b4c495f4e8f092641506174c98533af7b4eb
John Kohl <jtkohl@mit.edu>
1990-01-11 10:39:34 +0000

    add 1990
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@83 dc483132-0cff-0310-8789-dd5450dbe970

--- src/include/krb5/copyright.h
+++ src/include/krb5/copyright.h
@@ -1,4 +1,4 @@
 /*
- * Copyright (C) 1989 by the Massachusetts Institute of Technology,
- *  Cambridge, MA, USA.  All Rights Reserved.
+ * Copyright (C) 1989, 1990 by the Massachusetts Institute of Technology,
+ * Cambridge, MA, USA.  All Rights Reserved.
  */
