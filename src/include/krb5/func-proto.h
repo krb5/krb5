cc26d5f0e58e03157ec8ebaa494607512d995bbe
John Kohl <jtkohl@mit.edu>
1990-01-17 13:16:48 +0000

    *** empty log message ***
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@100 dc483132-0cff-0310-8789-dd5450dbe970

---
+++ src/include/krb5/func-proto.h
@@ -0,0 +1,19 @@
+/*
+ * $Source$
+ * $Author$
+ * $Id$
+ *
+ * Copyright 1990 by the Massachusetts Institute of Technology.
+ *
+ * For copying and distribution information, please see the file
+ * <krb5/mit-copyright.h>.
+ *
+ * Function prototypes for Kerberos V5 library.
+ */
+
+#include <krb5/copyright.h>
+
+#ifndef __KRB5_FUNC_PROTO__
+#define __KRB5_FUNC_PROTO__
+
+#endif /* __KRB5_FUNC_PROTO__ */
