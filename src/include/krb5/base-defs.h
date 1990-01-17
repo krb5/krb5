d2b6e9339897e7bda9ecb39198d1603dc09d5515
John Kohl <jtkohl@mit.edu>
1990-01-17 10:46:47 +0000

    add krb5_error_code, krb5_pointer rename octet, int16, int32
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@94 dc483132-0cff-0310-8789-dd5450dbe970

--- src/include/krb5/base-defs.h
+++ src/include/krb5/base-defs.h
@@ -18,17 +18,17 @@
 
 #include <krb5/wordsize.h>
 
-typedef octet		krb5_ui_1;
-typedef int16		krb5_ui_2;
-typedef int32		krb5_ui_4;
-typedef octet		krb5_boolean;
+typedef krb5_octet	krb5_ui_1;
+typedef krb5_int16	krb5_ui_2;
+typedef krb5_int32	krb5_ui_4;
+typedef krb5_octet	krb5_boolean;
 
 
-typedef int32		krb5_timestamp;
+typedef krb5_int32	krb5_timestamp;
 typedef krb5_ui_2	krb5_confounder;
-typedef	octet		krb5_msgtype;
-typedef	octet		krb5_kvno;
-typedef	int32		krb5_flags;
+typedef	krb5_octet	krb5_msgtype;
+typedef	krb5_octet	krb5_kvno;
+typedef	krb5_int32	krb5_flags;
 
 typedef	krb5_ui_2	krb5_addrtype;
 typedef krb5_ui_2	krb5_keytype;
@@ -36,9 +36,20 @@ typedef krb5_ui_2	krb5_enctype;
 typedef krb5_ui_2	krb5_cksumtype;
 typedef krb5_ui_2	krb5_authdatatype;
 
+typedef	krb5_int32	krb5_error_code;
+
 typedef struct _krb5_data {
     int length;
     char *data;
 } krb5_data;
+
+
+typedef	krb5_data **	krb5_principal;	/* array of strings */
+					/* CONVENTION: realm is first elem. */
+#ifdef __STDC__
+typedef	void * krb5_pointer;
+#else
+typedef char * krb5_pointer;
+#endif /* __STDC__ */
 
 #endif /* __KRB5_BASE_DEFS__ */
