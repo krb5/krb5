6d958ff2f027263d96daf99f3d3efbd24ebe8d00
John Kohl <jtkohl@mit.edu>
1990-01-18 11:19:42 +0000

    *** empty log message ***
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@114 dc483132-0cff-0310-8789-dd5450dbe970

---
+++ src/lib/krb5/os/timeofday.c
@@ -0,0 +1,42 @@
+/*
+ * $Source$
+ * $Author$
+ *
+ * Copyright 1990 by the Massachusetts Institute of Technology.
+ *
+ * For copying and distribution information, please see the file
+ * <krb5/mit-copyright.h>.
+ *
+ * libos: krb5_timeofday function for BSD 4.3 
+ */
+
+#ifndef	lint
+static char rcsid_timeofday_c[] =
+"$Id$";
+#endif	lint
+
+#include <krb5/copyright.h>
+
+#include <sys/time.h>			/* for timeval */
+#include <stdio.h>			/* needed for libos-proto.h */
+
+#include <krb5/config.h>
+#include <krb5/base-defs.h>
+#include <krb5/libos-proto.h>
+
+extern int errno;
+
+krb5_error_code
+krb5_timeofday(timeret)
+register krb5_int32 *timeret;
+{
+    struct timeval tv;
+
+    if (gettimeofday(&tv, (struct timezone *)0) == -1) {
+	/* failed, return errno */
+	return (krb5_error_code) errno;
+    }
+    *timeret = tv.tv_sec;
+    return 0;
+
+}
