ad740b14b4705f1543e0f48b722d79a74e3c9fa8
John Kohl <jtkohl@mit.edu>
1990-01-18 17:34:04 +0000

    fix typos remove formal parameter names
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@129 dc483132-0cff-0310-8789-dd5450dbe970

--- src/include/krb5/rcache.h
+++ src/include/krb5/rcache.h
@@ -23,19 +23,25 @@ typedef struct krb5_rc_st {
 
 typedef struct _krb5_rc_ops {
 	char *prefix;
-	int (*resolve) PROTOTYPE((krb5_rcache *id, char *residual));
-	int (*new) PROTOTYPE((krb5_rcache *id, struct _krb5_rc_ops *));
+	int (*resolve) PROTOTYPE((krb5_rcache *,
+				  char *));
+	int (*new) PROTOTYPE((krb5_rcache *,
+			      struct _krb5_rc_ops *));
 	char *(*get_name) PROTOTYPE((krb5_rcache));
-	int (*init) PROTOTYPE((krb5_rcache, krb5_timestamp));
+	int (*init) PROTOTYPE((krb5_rcache,
+			       krb5_timestamp));
 	int (*recover) PROTOTYPE((krb5_rcache));
 	int (*destroy) PROTOTYPE((krb5_rcache));
 	int (*close) PROTOTYPE((krb5_rcache));
-	int (*store) PROTOTYPE((krb5_rcache, krb5_tkt_authent *,
-				krb_boolean expunge));
-	int (*search) PROTOTYPE((krb5_rcache, krb5_tkt_authent *));
+	int (*store) PROTOTYPE((krb5_rcache,
+				krb5_tkt_authent *,
+				krb5_boolean));
+	int (*search) PROTOTYPE((krb5_rcache,
+				 krb5_tkt_authent *));
 	int (*get_span) PROTOTYPE((krb5_rcache));
-	int (*remove_cred) PROTOTYPE((krb5_rcache, krb5_tkt_authent *));
-	int (*expunge) PROTOTYPE((krb_acache));
+	int (*remove_cred) PROTOTYPE((krb5_rcache,
+				      krb5_tkt_authent *));
+	int (*expunge) PROTOTYPE((krb5_rcache));
 } krb5_rc_ops;
 
 
