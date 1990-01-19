02301adeeb1b2bffd6952ffdf20f77e148d11edb
John Kohl <jtkohl@mit.edu>
1990-01-19 15:32:30 +0000

    add !SABER to #ifdef around rcsid
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@141 dc483132-0cff-0310-8789-dd5450dbe970

--- src/prototype/prototype.c
+++ src/prototype/prototype.c
@@ -10,9 +10,9 @@
  * <<< Description >>>
  */
 
-#ifndef	lint
+#if !defined(lint) && !defined(SABER)
 static char <<< rcsid_module_c >>>[] =
 "$Id$";
-#endif	/* lint */
+#endif	/* !lint & !SABER */
 
 #include <krb5/copyright.h>
