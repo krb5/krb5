a46539a160b6bb03bbe6f013c46f0807bdac3a59
John Kohl <jtkohl@mit.edu>
1990-01-19 17:04:53 +0000

    add DEFAULT_DBM_FILE fix up prototypes
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@146 dc483132-0cff-0310-8789-dd5450dbe970

--- src/include/krb5/kdb_dbm.h
+++ src/include/krb5/kdb_dbm.h
@@ -16,38 +16,26 @@
 #ifndef __KRB5_KDB5_DBM__
 #define __KRB5_KDB5_DBM__
 
+#define	DEFAULT_DBM_FILE		"/krb5/principal"
+
 /* libkdb.spec */
-krb5_error_code krb5_dbm_db_set_name
-	PROTOTYPE((char *name ));
-krb5_error_code krb5_dbm_db_set_nonblocking
-	PROTOTYPE((krb5_boolean newmode,
-		   krb5_boolean *oldmode ));
-krb5_error_code krb5_dbm_db_init
-	PROTOTYPE((void ));
-krb5_error_code krb5_dbm_db_fini
-	PROTOTYPE((void ));
-krb5_error_code krb5_dbm_db_get_age
-	PROTOTYPE((char *db_name,
-		   krb5_timestamp *age ));
-krb5_error_code krb5_dbm_db_create
-	PROTOTYPE((char *db_name ));
-krb5_error_code krb5_dbm_db_rename
-	PROTOTYPE((char *from,
-		   char *to ));
-krb5_error_code krb5_dbm_db_get_principal
-	PROTOTYPE((krb5_principal searchfor,
-		   krb5_kdb_principal *principal,
-		   int *nprincs,
-		   krb5_boolean *more ));
-krb5_error_code krb5_dbm_db_free_principal
-	PROTOTYPE((krb5_kdb_principal *principal,
-		   int nprincs ));
-krb5_error_code krb5_dbm_db_put_principal
-	PROTOTYPE((krb5_kdb_principal *principal,
-		   int nprincs,
-		   int *nstored ));
+krb5_error_code krb5_dbm_db_set_name PROTOTYPE((char * ));
+krb5_error_code krb5_dbm_db_set_nonblocking PROTOTYPE((krb5_boolean,
+						       krb5_boolean * ));
+krb5_error_code krb5_dbm_db_init PROTOTYPE((void ));
+krb5_error_code krb5_dbm_db_fini PROTOTYPE((void ));
+krb5_error_code krb5_dbm_db_get_age PROTOTYPE((char *, time_t * ));
+krb5_error_code krb5_dbm_db_create PROTOTYPE((char * ));
+krb5_error_code krb5_dbm_db_rename PROTOTYPE((char *, char * ));
+krb5_error_code krb5_dbm_db_get_principal PROTOTYPE((krb5_principal,
+						     krb5_kdb_principal *,
+						     int *,
+						     krb5_boolean * ));
+void krb5_dbm_db_free_principal PROTOTYPE((krb5_kdb_principal *, int ));
+krb5_error_code krb5_dbm_db_put_principal PROTOTYPE((krb5_kdb_principal *,
+						     int * ));
 krb5_error_code krb5_dbm_db_iterate
-	PROTOTYPE((krb5_error_code (*func ) PROTOTYPE((krb5_pointer,
-						       krb5_kdb_principal *)),
-		   krb5_pointer iterate_arg ));
+    PROTOTYPE((krb5_error_code (*) PROTOTYPE((krb5_pointer,
+					      krb5_kdb_principal *)),
+	       krb5_pointer ));
 #endif /* __KRB5_KDB5_DBM__ */
