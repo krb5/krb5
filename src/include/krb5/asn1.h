85bd00593fd7c5b2794d3ad3348a7bb2b78594da
John Kohl <jtkohl@mit.edu>
1990-01-29 15:34:14 +0000

    *** empty log message ***
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@179 dc483132-0cff-0310-8789-dd5450dbe970

---
+++ src/include/krb5/asn1.h
@@ -0,0 +1,25 @@
+/*
+ * $Source$
+ * $Author$
+ * $Id$
+ *
+ * Copyright 1990 by the Massachusetts Institute of Technology.
+ *
+ * For copying and distribution information, please see the file
+ * <krb5/mit-copyright.h>.
+ *
+ * meta-include file for KRB5 asn.1/ISODE stuff
+ */
+
+#include <krb5/copyright.h>
+
+#ifndef __KRB5_ASN1__
+#define __KRB5_ASN1__
+
+#include <krb5/asn.1/KRB5-types.h>
+#include <krb5/asn.1/krb5_free.h>
+#include <krb5/asn.1/asn1defs.h>
+#include <krb5/asn.1/KRB5-types-aux.h>
+#include <krb5/asn.1/encode.h>
+
+#endif __KRB5_ASN1__
