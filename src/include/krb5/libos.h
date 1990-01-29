8b80aa190e5578730e534b9ea0f89e97043fb266
Bill Sommerfeld <wesommer@mit.edu>
1990-01-29 19:19:44 +0000

    Add prototype for function to get all the addresses of this host
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@191 dc483132-0cff-0310-8789-dd5450dbe970

--- src/include/krb5/libos.h
+++ src/include/krb5/libos.h
@@ -22,4 +22,7 @@
 #define	KRB5_LOCKMODE_DONTBLOCK	0x0004
 #define	KRB5_LOCKMODE_UNLOCK	0x0008
 
+/* get all the addresses of this host */
+extern int krb5_os_localaddr PROTOTYPE((krb5_address ***addr));
+
 #endif /* __KRB5_LIBOS__ */
