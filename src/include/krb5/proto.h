ab0c14d096fc48df4091cedf83b4b1030770d899
John Kohl <jtkohl@mit.edu>
1990-01-29 14:38:56 +0000

    need to cast to message types
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@174 dc483132-0cff-0310-8789-dd5450dbe970

--- src/include/krb5/proto.h
+++ src/include/krb5/proto.h
@@ -21,14 +21,14 @@
 
 /* Message types */
 
-#define	KRB5_AS_REQ		2	/* Req for initial authentication */
-#define	KRB5_AS_REP		4	/* Response to KRB_AS_REQ request */
-#define	KRB5_AP_REQ		6	/* application request to server */
-#define	KRB5_TGS_REQ		8	/* TGS request to server */
-#define	KRB5_AP_REP		10	/* Response to KRB_AP_REQ_MUTUAL */
-#define	KRB5_PRIV		12	/* Private application message */
-#define	KRB5_SAFE		14	/* Safe application message */
-#define	KRB5_TGS_REP		16	/* Response to KRB_TGS_REQ request */
-#define	KRB5_ERROR		32	/* Error response */
+#define	KRB5_AS_REQ	((krb5_msgtype)2) /* Req for initial authentication */
+#define	KRB5_AS_REP	((krb5_msgtype)4) /* Response to KRB_AS_REQ request */
+#define	KRB5_AP_REQ	((krb5_msgtype)6) /* application request to server */
+#define	KRB5_TGS_REQ	((krb5_msgtype)8) /* TGS request to server */
+#define	KRB5_AP_REP	((krb5_msgtype)10) /* Response to KRB_AP_REQ_MUTUAL */
+#define	KRB5_PRIV	((krb5_msgtype)12) /* Private application message */
+#define	KRB5_SAFE	((krb5_msgtype)14) /* Safe application message */
+#define	KRB5_TGS_REP	((krb5_msgtype)16) /* Response to KRB_TGS_REQ req */
+#define	KRB5_ERROR	((krb5_msgtype)32) /* Error response */
 
 #endif /* __KRB5_PROTO__ */
