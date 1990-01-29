871b83e61ef2dc346b4a6d0bb5606e6e84bae08f
John Kohl <jtkohl@mit.edu>
1990-01-29 16:33:10 +0000

    change to relative pathnames comment on #endif
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@182 dc483132-0cff-0310-8789-dd5450dbe970

--- src/include/krb5/asn1.h
+++ src/include/krb5/asn1.h
@@ -16,10 +16,10 @@
 #ifndef __KRB5_ASN1__
 #define __KRB5_ASN1__
 
-#include <krb5/asn.1/KRB5-types.h>
-#include <krb5/asn.1/krb5_free.h>
-#include <krb5/asn.1/asn1defs.h>
-#include <krb5/asn.1/KRB5-types-aux.h>
-#include <krb5/asn.1/encode.h>
+#include "../../asn.1/KRB5-types.h"
+#include "../../asn.1/krb5_free.h"
+#include "../../asn.1/asn1defs.h"
+#include "../../asn.1/KRB5-types-aux.h"
+#include "../../asn.1/encode.h"
 
-#endif __KRB5_ASN1__
+#endif /* __KRB5_ASN1__ */
