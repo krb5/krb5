a9e2c34d4de4323cbe8540698ca2edbbfb8f9de5
John Kohl <jtkohl@mit.edu>
1990-02-02 17:18:20 +0000

    make realm_of_tgt a macro add kdc_convert_key, decrypt_tgs_req
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@233 dc483132-0cff-0310-8789-dd5450dbe970

--- src/kdc/kdc_util.h
+++ src/kdc/kdc_util.h
@@ -18,19 +18,29 @@
 
 krb5_error_code check_hot_list PROTOTYPE((krb5_ticket *));
 krb5_boolean realm_compare PROTOTYPE((krb5_data *, krb5_principal));
-krb5_data * realm_of_tgt PROTOTYPE((krb5_ticket *));
 krb5_error_code compress_transited PROTOTYPE((krb5_data,
-						     krb5_principal,
-						     krb5_data *));
+					      krb5_principal,
+					      krb5_data *));
 krb5_error_code concat_authorization_data PROTOTYPE((krb5_authdata **,
 						     krb5_authdata **,
 						     krb5_authdata ***));
 krb5_error_code fetch_last_req_info PROTOTYPE((krb5_db_entry *,
 					       krb5_last_req_entry ***));
 
+krb5_error_code kdc_convert_key PROTOTYPE((krb5_keyblock *,
+					   krb5_keyblock *,
+					   int));
+krb5_error_code decrypt_tgs_req PROTOTYPE((krb5_tgs_req *));
+
+/* which way to convert key? */
+#define CONVERT_INTO_DB	0
+#define CONVERT_OUTOF_DB 1
+
 #define isset(flagfield, flag) (flagfield & (flag))
 #define set(flagfield, flag) (flagfield |= (flag))
 #define clear(flagfield, flag) (flagfield &= ~(flag))
+
+#define realm_of_tgt(ticket) ((ticket)->server[0])
 
 #ifndef	min
 #define	min(a, b)	((a) < (b) ? (a) : (b))
