0f0d822c86bfc4a61e3579c088fe3a1341e022b2
John Kohl <jtkohl@mit.edu>
1990-02-03 17:11:11 +0000

    fix prototypes add prototypes for process_as_req and process_tgs_req
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@254 dc483132-0cff-0310-8789-dd5450dbe970

--- src/kdc/kdc_util.h
+++ src/kdc/kdc_util.h
@@ -18,7 +18,7 @@
 
 krb5_error_code check_hot_list PROTOTYPE((krb5_ticket *));
 krb5_boolean realm_compare PROTOTYPE((krb5_data *, krb5_principal));
-krb5_error_code compress_transited PROTOTYPE((krb5_data,
+krb5_error_code compress_transited PROTOTYPE((krb5_data *,
 					      krb5_principal,
 					      krb5_data *));
 krb5_error_code concat_authorization_data PROTOTYPE((krb5_authdata **,
@@ -30,7 +30,21 @@ krb5_error_code fetch_last_req_info PROTOTYPE((krb5_db_entry *,
 krb5_error_code kdc_convert_key PROTOTYPE((krb5_keyblock *,
 					   krb5_keyblock *,
 					   int));
-krb5_error_code decrypt_tgs_req PROTOTYPE((krb5_tgs_req *));
+krb5_error_code decrypt_tgs_req PROTOTYPE((krb5_tgs_req *,
+					   krb5_fulladdr *));
+krb5_error_code kdc_process_tgs_req PROTOTYPE((krb5_tgs_req *,
+					       krb5_fulladdr *));
+
+/* do_as_req.c */
+krb5_error_code process_as_req PROTOTYPE((krb5_as_req *,
+					  krb5_fulladdr *,
+					  krb5_data ** ));
+
+/* do_tgs_req.c */
+krb5_error_code process_tgs_req PROTOTYPE((krb5_tgs_req *,
+					   krb5_fulladdr *,
+					   krb5_data ** ));
+
 
 /* which way to convert key? */
 #define CONVERT_INTO_DB	0
