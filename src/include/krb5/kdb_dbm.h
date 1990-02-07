4ed7a8f82092bfe384cab4d92665bed2746a07f6
John Kohl <jtkohl@mit.edu>
1990-02-07 17:41:51 +0000

    #define the DBM names to the real names, to avoid a level of indirection
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@305 dc483132-0cff-0310-8789-dd5450dbe970

--- src/include/krb5/kdb_dbm.h
+++ src/include/krb5/kdb_dbm.h
@@ -18,6 +18,20 @@
 
 #define	DEFAULT_DBM_FILE		"/krb5/principal"
 
+/* #define these to avoid an indirection function; for future implementations,
+   these may be redirected from a dispatch table/routine */
+#define krb5_dbm_db_set_name krb5_db_set_name
+#define krb5_dbm_db_set_nonblocking krb5_db_set_nonblocking
+#define krb5_dbm_db_init krb5_db_init
+#define krb5_dbm_db_fini krb5_db_fini
+#define krb5_dbm_db_get_age krb5_db_get_age
+#define krb5_dbm_db_create krb5_db_create
+#define krb5_dbm_db_rename krb5_db_rename
+#define krb5_dbm_db_get_principal krb5_db_get_principal
+#define krb5_dbm_db_free_principal krb5_db_free_principal
+#define krb5_dbm_db_put_principal krb5_db_put_principal
+#define krb5_dbm_db_iterate krb5_db_iterate
+
 /* libkdb.spec */
 krb5_error_code krb5_dbm_db_set_name PROTOTYPE((char * ));
 krb5_error_code krb5_dbm_db_set_nonblocking PROTOTYPE((krb5_boolean,
