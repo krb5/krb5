c9e5d80dcfdeaf11f5e668279a306300c6c7d970
John Kohl <jtkohl@mit.edu>
1990-03-23 16:34:39 +0000

    remove redundant field bits add KDC_TKT_COMMON_MASK
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@399 dc483132-0cff-0310-8789-dd5450dbe970

--- src/include/krb5/fieldbits.h
+++ src/include/krb5/fieldbits.h
@@ -53,6 +53,9 @@
 #define	KDC_OPT_RENEW			0x00000002
 #define	KDC_OPT_VALIDATE		0x00000001
 
+/* fields common which can be masked and copied */
+#define KDC_TKT_COMMON_MASK		0x7ea00000
+
 /* definitions for ap_options fields */
 /* ap_options are 32 bits; each host is responsible to put the 4 bytes
    representing these bits into net order before transmission */
@@ -131,42 +134,6 @@
 /* #define	TKT_FLG_RESERVED	0x00000004 */
 /* #define	TKT_FLG_RESERVED	0x00000002 */
 /* #define	TKT_FLG_RESERVED	0x00000001 */
-
-/* kdc_options for kdc_request */
-/* options is 32 bits; each host is responsible to put the 4 bytes
-   representing these bits into net order before transmission */
-/* #define	KDC_OPT_RESERVED	0x80000000 */
-#define	KDC_OPT_FORWARDABLE		0x40000000
-#define	KDC_OPT_FORWARDED		0x20000000
-#define	KDC_OPT_PROXIABLE		0x10000000
-#define	KDC_OPT_PROXY			0x08000000
-#define	KDC_OPT_ALLOW_POSTDATE		0x04000000
-#define	KDC_OPT_POSTDATED		0x02000000
-/* #define	KDC_OPT_UNUSED		0x01000000 */
-#define	KDC_OPT_RENEWABLE		0x00800000
-/* #define	KDC_OPT_UNUSED		0x00400000 */
-#define	KDC_OPT_DUPLICATE_SKEY		0x00200000
-/* #define	KDC_OPT_RESERVED	0x00100000 */
-/* #define	KDC_OPT_RESERVED	0x00080000 */
-/* #define	KDC_OPT_RESERVED	0x00040000 */
-/* #define	KDC_OPT_RESERVED	0x00020000 */
-/* #define	KDC_OPT_RESERVED	0x00010000 */
-/* #define	KDC_OPT_RESERVED	0x00008000 */
-/* #define	KDC_OPT_RESERVED	0x00004000 */
-/* #define	KDC_OPT_RESERVED	0x00002000 */
-/* #define	KDC_OPT_RESERVED	0x00001000 */
-/* #define	KDC_OPT_RESERVED	0x00000800 */
-/* #define	KDC_OPT_RESERVED	0x00000400 */
-/* #define	KDC_OPT_RESERVED	0x00000200 */
-/* #define	KDC_OPT_RESERVED	0x00000100 */
-/* #define	KDC_OPT_RESERVED	0x00000080 */
-/* #define	KDC_OPT_RESERVED	0x00000040 */
-/* #define	KDC_OPT_RESERVED	0x00000020 */
-#define	KDC_OPT_RENEWABLE_OK		0x00000010
-#define	KDC_OPT_ENC_TKT_IN_SKEY		0x00000008
-#define	KDC_OPT_REUSE_SKEY		0x00000004
-#define	KDC_OPT_RENEW			0x00000002
-#define	KDC_OPT_VALIDATE		0x00000001
 
 /* definitions for lr_type fields. */
 #define	LR_TYPE_THIS_SERVER_ONLY	0x8000
