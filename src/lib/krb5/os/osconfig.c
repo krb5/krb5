a7569c811171d04dc1d1d9ba6f6132842d0c7ddf
John Kohl <jtkohl@mit.edu>
1990-04-19 18:11:11 +0000

    add krb5_trans_file
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@532 dc483132-0cff-0310-8789-dd5450dbe970

--- src/lib/krb5/os/osconfig.c
+++ src/lib/krb5/os/osconfig.c
@@ -20,6 +20,7 @@ static char rcsid_config_fn_c [] =
 
 
 char *krb5_config_file = DEFAULT_CONFIG_FILENAME;
+char *krb5_trans_file = DEFAULT_TRANS_FILENAME;
 
 #ifdef KRB5_USE_INET
 char *krb5_kdc_udp_portname = KDC_PORTNAME;
