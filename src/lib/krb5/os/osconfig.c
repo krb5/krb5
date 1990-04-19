5e35cacd02e13b7c5d83d5c7637b0fa3b2018193
John Kohl <jtkohl@mit.edu>
1990-04-19 14:19:42 +0000

    add rcsid
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@513 dc483132-0cff-0310-8789-dd5450dbe970

--- src/lib/krb5/os/osconfig.c
+++ src/lib/krb5/os/osconfig.c
@@ -10,8 +10,14 @@
  * Definition of default config file pathname.
  */
 
+#if !defined(lint) && !defined(SABER)
+static char rcsid_config_fn_c [] =
+"$Id$";
+#endif	/* !lint & !SABER */
+
 #include <krb5/copyright.h>
 #include <krb5/osconf.h>
+
 
 char *krb5_config_file = DEFAULT_CONFIG_FILENAME;
 
