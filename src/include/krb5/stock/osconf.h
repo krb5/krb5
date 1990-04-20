7687232509eee699e9ed813405c555ca538d6a4a
John Kohl <jtkohl@mit.edu>
1990-04-20 15:05:16 +0000

    add DBM stuff add USE_DBM_LNAME, DEFAULT_LNAME_FILENAME, MAX_DGRAM_SIZE, MAX_SKDC_TIMEOUT SKDC_TIMEOUT_SHIFT, SKDC_TIMEOUT_1
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@545 dc483132-0cff-0310-8789-dd5450dbe970

--- src/include/krb5/stock/osconf.h
+++ src/include/krb5/stock/osconf.h
@@ -17,11 +17,24 @@
 #ifndef KRB5_OSCONF__
 #define KRB5_OSCONF__
 
+#ifdef ultrix
+#define ODBM
+#endif
+
 #define KRB5_USE_INET		/* Support IP address family */
+#define	USE_DBM_LNAME		/* Use a DBM database for the aname/lname
+				   conversion */
 
 #define	DEFAULT_CONFIG_FILENAME	"/etc/krb.conf"
 #define	DEFAULT_TRANS_FILENAME	"/etc/krb.realms"
+#define	DEFAULT_LNAME_FILENAME	"/etc/aname"
+
 #define KDC_PORTNAME	"kerberos5"	/* for /etc/services or equiv. */
+
+#define MAX_DGRAM_SIZE	4096
+#define MAX_SKDC_TIMEOUT 30
+#define SKDC_TIMEOUT_SHIFT 2		/* left shift of timeout for backoff */
+#define SKDC_TIMEOUT_1 1		/* seconds for first timeout */
 
 #ifdef POSIX_SIGTYPE
 #define sigtype void
