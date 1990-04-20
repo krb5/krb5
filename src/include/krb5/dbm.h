f714b2a052fe802d639fab9acd6a68eabb461221
John Kohl <jtkohl@mit.edu>
1990-04-20 11:35:45 +0000

    *** empty log message ***
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@536 dc483132-0cff-0310-8789-dd5450dbe970

---
+++ src/include/krb5/dbm.h
@@ -0,0 +1,38 @@
+/*
+ * $Source$
+ * $Author$
+ * $Id$
+ *
+ * Copyright 1990 by the Massachusetts Institute of Technology.
+ *
+ * For copying and distribution information, please see the file
+ * <krb5/copyright.h>.
+ *
+ * DBM/NDBM include file; deals with converting old-style to new-style.
+ */
+
+#include <krb5/copyright.h>
+
+#ifndef KRB5_DBM_COMPAT__
+#define KRB5_DBM_COMPAT__
+
+#ifndef ODBM
+#include <ndbm.h>
+#else /*ODBM*/
+#include <dbm.h>
+#endif /*ODBM*/
+
+#ifndef ODBM
+#define dbm_next(db,key) dbm_nextkey(db)
+#else /* OLD DBM */
+typedef char DBM;
+
+#define dbm_open(file, flags, mode) ((dbminit(file) == 0)?"":((char *)0))
+#define dbm_fetch(db, key) fetch(key)
+#define dbm_store(db, key, content, flag) store(key, content)
+#define dbm_firstkey(db) firstkey()
+#define dbm_next(db,key) nextkey(key)
+#define dbm_close(db) dbmclose()
+#endif /* OLD DBM */
+
+#endif /* KRB5_DBM_COMPAT__ */
