17de63dff78087547761a4fc6879c4c5214be3c1
John Kohl <jtkohl@mit.edu>
1990-04-20 15:04:05 +0000

    add krb5_lname_file, krb5_max_dgram_size, krb5_max_skdc_timeout, krb5_skdc_timeout_shift, krb5_skdc_timeout_1
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@543 dc483132-0cff-0310-8789-dd5450dbe970

--- src/lib/krb5/os/osconfig.c
+++ src/lib/krb5/os/osconfig.c
@@ -21,6 +21,14 @@ static char rcsid_config_fn_c [] =
 
 char *krb5_config_file = DEFAULT_CONFIG_FILENAME;
 char *krb5_trans_file = DEFAULT_TRANS_FILENAME;
+#ifdef USE_DBM_LNAME
+char *krb5_lname_file = DEFAULT_LNAME_FILENAME;
+#endif
+
+int krb5_max_dgram_size = MAX_DGRAM_SIZE;
+int krb5_max_skdc_timeout = MAX_SKDC_TIMEOUT;
+int krb5_skdc_timeout_shift = SKDC_TIMEOUT_SHIFT;
+int krb5_skdc_timeout_1 = SKDC_TIMEOUT_1;
 
 #ifdef KRB5_USE_INET
 char *krb5_kdc_udp_portname = KDC_PORTNAME;
