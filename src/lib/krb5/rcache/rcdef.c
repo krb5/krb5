09345406ecceb21e99135f73827b55eab046234e
John Kohl <jtkohl@mit.edu>
1990-05-04 14:07:18 +0000

    *** empty log message ***
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@718 dc483132-0cff-0310-8789-dd5450dbe970

---
+++ src/lib/krb5/rcache/rcdef.c
@@ -0,0 +1,34 @@
+/*
+ * $Source$
+ * $Author$
+ *
+ * Copyright 1990 by the Massachusetts Institute of Technology.
+ *
+ * For copying and distribution information, please see the file
+ * <krb5/copyright.h>.
+ *
+ * replay cache default operations vector.
+ */
+
+#if !defined(lint) && !defined(SABER)
+static char rcsid_rcdef_c[] =
+"$Id$";
+#endif	/* !lint & !SABER */
+
+#include <krb5/copyright.h>
+#include "rc_dfl.h"
+
+krb5_rc_ops krb5_rc_dfl_ops =
+ {
+  "dfl",
+  krb5_rc_dfl_init,
+  krb5_rc_dfl_recover,
+  krb5_rc_dfl_destroy,
+  krb5_rc_dfl_close,
+  krb5_rc_dfl_store,
+  krb5_rc_dfl_expunge,
+  krb5_rc_dfl_get_span,
+  krb5_rc_dfl_get_name,
+  krb5_rc_dfl_resolve
+ }
+;
