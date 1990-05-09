dd7fb40b3ee270d9b7222b4d91f7da588d4c06d8
John Kohl <jtkohl@mit.edu>
1990-05-09 14:39:17 +0000

    add des_random_key_seed
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@798 dc483132-0cff-0310-8789-dd5450dbe970

--- src/include/krb5/mit-des.h
+++ src/include/krb5/mit-des.h
@@ -16,13 +16,18 @@
 #ifndef KRB5_MIT_DES__
 #define KRB5_MIT_DES__
 
-typedef krb5_krb5_octet des_cblock[8];	/* crypto-block size */
+typedef krb5_octet des_cblock[8];	/* crypto-block size */
 
 /* Key schedule--used internally by DES routines to gain some speed */
 typedef struct des_ks_struct { des_cblock _; } des_key_schedule[16];
 
 #define DES_ENCRYPT	1
 #define DES_DECRYPT	0
+
+typedef struct des_ran_key_seed {
+    krb5_octet sequence_number[8];
+    des_key_schedule random_sequence_key;
+} des_random_key_seed;
 
 /* the first byte of the key is already in the keyblock */
 #define DES_KEYBLOCK_SZ	(sizeof(krb5_keyblock)+sizeof(des_cblock)-sizeof(krb5_octet))
@@ -35,5 +40,6 @@ typedef struct des_ks_struct { des_cblock _; } des_key_schedule[16];
 
 /* cryptosystem entry descriptor for MIT's DES encryption library */
 extern krb5_cryptosystem_entry  mit_des_cryptosystem_entry;
+extern krb5_checksum_entry	des_cbc_cksumtable_entry;
 
 #endif /* KRB5_MIT_DES__ */
