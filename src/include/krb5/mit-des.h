b1f155cb4b24c4addff24f66c7c6bcdfcb960b14
John Kohl <jtkohl@mit.edu>
1990-05-17 15:51:09 +0000

    change everything to be mit_des
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@877 dc483132-0cff-0310-8789-dd5450dbe970

--- src/include/krb5/mit-des.h
+++ src/include/krb5/mit-des.h
@@ -16,27 +16,26 @@
 #ifndef KRB5_MIT_DES__
 #define KRB5_MIT_DES__
 
-typedef krb5_octet des_cblock[8];	/* crypto-block size */
+typedef krb5_octet mit_des_cblock[8];	/* crypto-block size */
 
 /* Key schedule--used internally by DES routines to gain some speed */
-typedef struct des_ks_struct { des_cblock _; } des_key_schedule[16];
+typedef struct des_ks_struct { mit_des_cblock _; } mit_des_key_schedule[16];
 
-#define DES_ENCRYPT	1
-#define DES_DECRYPT	0
+#define MIT_DES_ENCRYPT	1
+#define MIT_DES_DECRYPT	0
 
-typedef struct des_ran_key_seed {
+typedef struct mit_des_ran_key_seed {
     krb5_octet sequence_number[8];
-    des_key_schedule random_sequence_key;
-} des_random_key_seed;
+    mit_des_key_schedule random_sequence_key;
+} mit_des_random_key_seed;
 
 /* the first byte of the key is already in the keyblock */
-#define DES_KEYBLOCK_SZ	(sizeof(krb5_keyblock)+sizeof(des_cblock)-sizeof(krb5_octet))
 
-#define DES_BLOCK_LENGTH 		(8*sizeof(krb5_octet))
-#define	DES_CBC_CRC_PAD_MINIMUM		CRC32_CKSUM_LENGTH
-#define DES_KEYSIZE		 	(8*sizeof(krb5_octet))
+#define MIT_DES_BLOCK_LENGTH 		(8*sizeof(krb5_octet))
+#define	MIT_DES_CBC_CRC_PAD_MINIMUM	CRC32_CKSUM_LENGTH
+#define MIT_DES_KEYSIZE		 	(8*sizeof(krb5_octet))
 
-#define DES_CBC_CKSUM_LENGTH		(4*sizeof(krb5_octet)) /* XXX ? */
+#define MIT_DES_CBC_CKSUM_LENGTH	(4*sizeof(krb5_octet))
 
 /* cryptosystem entry descriptor for MIT's DES encryption library */
 extern krb5_cryptosystem_entry  mit_des_cryptosystem_entry;
