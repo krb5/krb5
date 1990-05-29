d2c2c5072c6b689ca3f461400f29e22246b7ef63
John Kohl <jtkohl@mit.edu>
1990-05-29 12:05:22 +0000

    change default name interface
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@954 dc483132-0cff-0310-8789-dd5450dbe970

--- src/lib/krb5/ccache/ccdefault.c
+++ src/lib/krb5/ccache/ccdefault.c
@@ -18,15 +18,8 @@ static char rcsid_default_c[] =
 #include <krb5/copyright.h>
 #include <krb5/krb5.h>
 
-static krb5_ccache default_ccache;
-
-krb5_ccache krb5_cc_default()
+krb5_error_code krb5_cc_default(ccache)
+krb5_ccache *ccache;
 {
-    if (default_ccache == 0)
-	krb5_cc_resolve(krb5_cc_default_name(), &default_ccache);
-
-    /* ignore errors; any error will be fatal in future derefs by the
-       caller. */
-
-    return default_ccache;
+    return krb5_cc_resolve(krb5_cc_default_name(), ccache);
 }
