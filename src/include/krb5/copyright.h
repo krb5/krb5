765628c206c30ce07bfc2eb2ba78289b95326944
John Kohl <jtkohl@mit.edu>
1990-08-24 14:20:14 +0000

    add trademark info
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@1044 dc483132-0cff-0310-8789-dd5450dbe970

--- src/include/krb5/copyright.h
+++ src/include/krb5/copyright.h
@@ -1,4 +1,9 @@
 /*
  * Copyright (C) 1989, 1990 by the Massachusetts Institute of Technology,
  * Cambridge, MA, USA.  All Rights Reserved.
+ *
+ * Project Athena, Athena, Athena MUSE, Discuss, Hesiod, Kerberos,
+ * Moira, and Zephyr are trademarks of the Massachusetts Institute of
+ * Technology (MIT).  No commercial use of these trademarks may be made
+ * without prior written permission of MIT.
  */
