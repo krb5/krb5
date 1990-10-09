24391a6a79b7493a13920faeb0e9739b1d2796f1
John Kohl <jtkohl@mit.edu>
1990-10-09 16:47:23 +0000

    these prototypes are already in func-proto.h
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@1176 dc483132-0cff-0310-8789-dd5450dbe970

--- src/include/krb5/free.h
+++ src/include/krb5/free.h
@@ -23,28 +23,4 @@
 
 #define krb5_free_ap_rep_enc_part xfree
 
-/* krb5_free.c */
-void krb5_free_checksum PROTOTYPE((krb5_checksum *));
-void krb5_free_keyblock PROTOTYPE((krb5_keyblock *));
-void krb5_free_principal PROTOTYPE((krb5_principal ));
-void krb5_free_authenticator PROTOTYPE((krb5_authenticator *));
-void krb5_free_address PROTOTYPE((krb5_address **));
-void krb5_free_authdata PROTOTYPE((krb5_authdata **));
-void krb5_free_enc_tkt_part PROTOTYPE((krb5_enc_tkt_part *));
-void krb5_free_ticket PROTOTYPE((krb5_ticket *));
-void krb5_free_as_req PROTOTYPE((krb5_as_req *));
-void krb5_free_kdc_rep PROTOTYPE((krb5_kdc_rep *));
-void krb5_free_last_req PROTOTYPE((krb5_last_req_entry **));
-void krb5_free_enc_kdc_rep_part PROTOTYPE((krb5_enc_kdc_rep_part *));
-void krb5_free_error PROTOTYPE((krb5_error *));
-void krb5_free_ap_req PROTOTYPE((krb5_ap_req *));
-void krb5_free_ap_rep PROTOTYPE((krb5_ap_rep *));
-void krb5_free_tgs_req PROTOTYPE((krb5_tgs_req *));
-void krb5_free_real_tgs_req PROTOTYPE((krb5_real_tgs_req *));
-void krb5_free_tgs_req_enc_part PROTOTYPE((krb5_tgs_req_enc_part *));
-void krb5_free_safe PROTOTYPE((krb5_safe *));
-void krb5_free_priv PROTOTYPE((krb5_priv *));
-void krb5_free_priv_enc_part PROTOTYPE((krb5_priv_enc_part *));
-void krb5_free_creds PROTOTYPE((krb5_creds *));
-
 #endif /* KRB5_FREE__ */
