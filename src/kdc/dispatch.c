7caade61f51ed80e362fe3d0cc0a76a8afaedae6
John Kohl <jtkohl@mit.edu>
1990-10-10 17:26:57 +0000

    change data structure names
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@1232 dc483132-0cff-0310-8789-dd5450dbe970

--- src/kdc/dispatch.c
+++ src/kdc/dispatch.c
@@ -29,8 +29,8 @@ krb5_data **response;
 {
 
     krb5_error_code retval;
-    krb5_as_req *as_req;
-    krb5_tgs_req *tgs_req;
+    krb5_kdc_req *as_req;
+    krb5_kdc_req *tgs_req;
 
     /* decode incoming packet, and dispatch */
     /* try TGS_REQ first; they are more common! */
@@ -38,12 +38,12 @@ krb5_data **response;
     if (krb5_is_tgs_req(pkt)) {
 	if (!(retval = decode_krb5_tgs_req(pkt, &tgs_req))) {
 	    retval = process_tgs_req(tgs_req, from, response);
-	    krb5_free_tgs_req(tgs_req);
+	    krb5_free_kdc_req(tgs_req);
 	}
     } else if (krb5_is_as_req(pkt)) {
 	if (!(retval = decode_krb5_as_req(pkt, &as_req))) {
 	    retval = process_as_req(as_req, from, response);
-	    krb5_free_as_req(as_req);
+	    krb5_free_kdc_req(as_req);
 	}
     } else if (pkt->data[0] == 4)		/* XXX old version */
 	return(process_v4(pkt));
