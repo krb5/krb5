c87ac885b63bc43c77278d21b7bbe69d6fd37725
John Kohl <jtkohl@mit.edu>
1990-10-10 14:59:45 +0000

    fix numbers add KRB5_PADATA_AP_REQ
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@1214 dc483132-0cff-0310-8789-dd5450dbe970

--- src/include/krb5/proto.h
+++ src/include/krb5/proto.h
@@ -21,14 +21,17 @@
 
 /* Message types */
 
-#define	KRB5_AS_REQ	((krb5_msgtype)2) /* Req for initial authentication */
-#define	KRB5_AS_REP	((krb5_msgtype)4) /* Response to KRB_AS_REQ request */
-#define	KRB5_AP_REQ	((krb5_msgtype)6) /* application request to server */
-#define	KRB5_TGS_REQ	((krb5_msgtype)8) /* TGS request to server */
-#define	KRB5_AP_REP	((krb5_msgtype)10) /* Response to KRB_AP_REQ_MUTUAL */
-#define	KRB5_PRIV	((krb5_msgtype)12) /* Private application message */
-#define	KRB5_SAFE	((krb5_msgtype)14) /* Safe application message */
-#define	KRB5_TGS_REP	((krb5_msgtype)16) /* Response to KRB_TGS_REQ req */
-#define	KRB5_ERROR	((krb5_msgtype)32) /* Error response */
+#define	KRB5_AS_REQ	((krb5_msgtype)10) /* Req for initial authentication */
+#define	KRB5_AS_REP	((krb5_msgtype)11) /* Response to KRB_AS_REQ request */
+#define	KRB5_TGS_REQ	((krb5_msgtype)12) /* TGS request to server */
+#define	KRB5_TGS_REP	((krb5_msgtype)13) /* Response to KRB_TGS_REQ req */
+#define	KRB5_AP_REQ	((krb5_msgtype)14) /* application request to server */
+#define	KRB5_AP_REP	((krb5_msgtype)15) /* Response to KRB_AP_REQ_MUTUAL */
+#define	KRB5_SAFE	((krb5_msgtype)20) /* Safe application message */
+#define	KRB5_PRIV	((krb5_msgtype)21) /* Private application message */
+#define	KRB5_ERROR	((krb5_msgtype)30) /* Error response */
+
+/* PADATA types */
+#define	KRB5_PADATA_AP_REQ	((krb5_octet)1)
 
 #endif /* KRB5_PROTO__ */
