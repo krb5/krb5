de8efc8186251c8dc22b291a891921ffd4e2f148
John Kohl <jtkohl@mit.edu>
1990-11-08 15:21:10 +0000

    use memcpm rather than bcmp
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@1428 dc483132-0cff-0310-8789-dd5450dbe970

--- src/lib/crypto/des/weak_key.c
+++ src/lib/crypto/des/weak_key.c
@@ -68,7 +68,7 @@ mit_des_is_weak_key(key)
     mit_des_cblock *weak_p = weak;
 
     for (i = 0; i < (sizeof(weak)/sizeof(mit_des_cblock)); i++) {
-	if (!bcmp((char *)weak_p++,(char *)key,sizeof(mit_des_cblock)))
+	if (!memcmp((char *)weak_p++,(char *)key,sizeof(mit_des_cblock)))
 	    return 1;
     }
 
