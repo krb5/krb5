cfcc2c09f67ee38d168735fcc5eb42fc533dc360
John Kohl <jtkohl@mit.edu>
1991-01-03 15:04:07 +0000

    oops, mis-counted bits on primitive construction identifiers
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@1600 dc483132-0cff-0310-8789-dd5450dbe970

--- src/include/krb5/asn.1/encode.h
+++ src/include/krb5/asn.1/encode.h
@@ -243,49 +243,49 @@ krb5_error_code encode_krb5_as_req
    set or reset */
 #define krb5_is_krb_ticket(dat)\
 	((dat) && (dat)->length && ((dat)->data[0] == 0x61 ||\
-				    (dat)->data[0] == 0x21))
+				    (dat)->data[0] == 0x41))
 #define krb5_is_krb_authenticator(dat)\
 	((dat) && (dat)->length && ((dat)->data[0] == 0x62 ||\
-				    (dat)->data[0] == 0x22))
+				    (dat)->data[0] == 0x42))
 #define krb5_is_as_req(dat)\
 	((dat) && (dat)->length && ((dat)->data[0] == 0x6a ||\
-				    (dat)->data[0] == 0x2a))
+				    (dat)->data[0] == 0x4a))
 #define krb5_is_as_rep(dat)\
 	((dat) && (dat)->length && ((dat)->data[0] == 0x6b ||\
-				    (dat)->data[0] == 0x2b))
+				    (dat)->data[0] == 0x4b))
 #define krb5_is_tgs_req(dat)\
 	((dat) && (dat)->length && ((dat)->data[0] == 0x6c ||\
-				    (dat)->data[0] == 0x2c))
+				    (dat)->data[0] == 0x4c))
 #define krb5_is_tgs_rep(dat)\
 	((dat) && (dat)->length && ((dat)->data[0] == 0x6d ||\
-				    (dat)->data[0] == 0x2d))
+				    (dat)->data[0] == 0x4d))
 #define krb5_is_ap_req(dat)\
 	((dat) && (dat)->length && ((dat)->data[0] == 0x6e ||\
-				    (dat)->data[0] == 0x2e))
+				    (dat)->data[0] == 0x4e))
 #define krb5_is_ap_rep(dat)\
 	((dat) && (dat)->length && ((dat)->data[0] == 0x6f ||\
-				    (dat)->data[0] == 0x2f))
+				    (dat)->data[0] == 0x4f))
 #define krb5_is_krb_safe(dat)\
 	((dat) && (dat)->length && ((dat)->data[0] == 0x74 ||\
-				    (dat)->data[0] == 0x34))
+				    (dat)->data[0] == 0x54))
 #define krb5_is_krb_priv(dat)\
 	((dat) && (dat)->length && ((dat)->data[0] == 0x75 ||\
-				    (dat)->data[0] == 0x35))
+				    (dat)->data[0] == 0x55))
 #define krb5_is_krb_enc_as_rep_part(dat)\
 	((dat) && (dat)->length && ((dat)->data[0] == 0x79 ||\
-				    (dat)->data[0] == 0x39))
+				    (dat)->data[0] == 0x59))
 #define krb5_is_krb_enc_tgs_rep_part(dat)\
 	((dat) && (dat)->length && ((dat)->data[0] == 0x7a ||\
-				    (dat)->data[0] == 0x3a))
+				    (dat)->data[0] == 0x5a))
 #define krb5_is_krb_enc_ap_rep_part(dat)\
 	((dat) && (dat)->length && ((dat)->data[0] == 0x7b ||\
-				    (dat)->data[0] == 0x3b))
+				    (dat)->data[0] == 0x5b))
 #define krb5_is_krb_enc_krb_priv_part(dat)\
 	((dat) && (dat)->length && ((dat)->data[0] == 0x7c ||\
-				    (dat)->data[0] == 0x3c))
+				    (dat)->data[0] == 0x5c))
 #define krb5_is_krb_error(dat)\
 	((dat) && (dat)->length && ((dat)->data[0] == 0x7e ||\
-				    (dat)->data[0] == 0x3e))
+				    (dat)->data[0] == 0x5e))
 
 
 #endif /* KRB5_ENCODE_DEFS__ */
