914f50d07acfdd9abdd05a2c78ceb1d785e82075
John Kohl <jtkohl@mit.edu>
1991-01-08 15:19:46 +0000

    changes to move the conversion outside the store routines
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@1624 dc483132-0cff-0310-8789-dd5450dbe970

--- src/include/krb5/rcache.h
+++ src/include/krb5/rcache.h
@@ -21,13 +21,20 @@ typedef struct krb5_rc_st {
     krb5_pointer data;
 } *krb5_rcache;
 
+typedef struct _krb5_donot_replay {
+    char *server;			/* null-terminated */
+    char *client;			/* null-terminated */
+    krb5_int32 cusec;
+    krb5_timestamp ctime;
+} krb5_donot_replay;
+
 typedef struct _krb5_rc_ops {
     char *type;
     krb5_error_code (*init)PROTOTYPE((krb5_rcache,krb5_deltat)); /* create */
     krb5_error_code (*recover)PROTOTYPE((krb5_rcache)); /* open */
     krb5_error_code (*destroy)PROTOTYPE((krb5_rcache));
     krb5_error_code (*close)PROTOTYPE((krb5_rcache));
-    krb5_error_code (*store)PROTOTYPE((krb5_rcache,krb5_tkt_authent *));
+    krb5_error_code (*store)PROTOTYPE((krb5_rcache,krb5_donot_replay *));
     krb5_error_code (*expunge)PROTOTYPE((krb5_rcache));
     krb5_error_code (*get_span)PROTOTYPE((krb5_rcache,krb5_deltat *));
     char *(*get_name)PROTOTYPE((krb5_rcache));
@@ -41,12 +48,14 @@ krb5_error_code krb5_rc_resolve_full PROTOTYPE((krb5_rcache *,char *));
 char *krb5_rc_get_type PROTOTYPE((krb5_rcache));
 char *krb5_rc_default_type PROTOTYPE((void));
 char *krb5_rc_default_name PROTOTYPE((void));
+krb5_error_code krb5_auth_to_rep PROTOTYPE((krb5_tkt_authent *,
+					    krb5_donot_replay *));
 
 #define krb5_rc_initialize(id, span) (*(id)->ops->init)(id, span)
 #define krb5_rc_recover(id) (*(id)->ops->recover)(id)
 #define krb5_rc_destroy(id) (*(id)->ops->destroy)(id)
 #define krb5_rc_close(id) (*(id)->ops->close)(id)
-#define krb5_rc_store(id, authent) (*(id)->ops->store)(id, authent)
+#define krb5_rc_store(id, dontreplay) (*(id)->ops->store)(id, dontreplay)
 #define krb5_rc_expunge(id) (*(id)->ops->expunge)(id)
 #define krb5_rc_get_lifespan(id, spanp) (*(id)->ops->get_span)(id, spanp)
 #define krb5_rc_get_name(id) (*(id)->ops->get_name)(id)
