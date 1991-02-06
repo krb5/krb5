3a8decf869c6583d3cd4cbdf81bc2332510803cb
John Kohl <jtkohl@mit.edu>
1991-02-06 13:12:04 +0000

    *** empty log message ***
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@1653 dc483132-0cff-0310-8789-dd5450dbe970

---
+++ src/lib/krb5/ccache/ccdefops.c
@@ -0,0 +1,17 @@
+/*
+ * $Source$
+ * $Author$
+ *
+ * Copyright 1990 by the Massachusetts Institute of Technology.
+ *
+ * For copying and distribution information, please see the file
+ * <krb5/copyright.h>.
+ *
+ * Default credentials cache determination.  This is a separate file
+ * so that the user can more easily override it.
+ */
+
+#include "file/fcc.h"
+#include "stdio/scc.h"
+
+krb5_cc_ops *krb5_cc_dfl_ops = &krb5_cc_file_ops;
