775b82eeacb2f488ed464ec763cc45fc394f7371
John Kohl <jtkohl@mit.edu>
1991-02-08 13:22:25 +0000

    point folks to the agreements
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@1657 dc483132-0cff-0310-8789-dd5450dbe970

--- src/include/krb5/copyright.h
+++ src/include/krb5/copyright.h
@@ -2,6 +2,9 @@
  * Copyright (C) 1989, 1990, 1991 by the Massachusetts Institute of Technology,
  * Cambridge, MA, USA.  All Rights Reserved.
  *
+ * The copying and use of this software is governed by the licensing
+ * and test agreements signed separately.  No other use is permitted.
+ *
  * Project Athena, Athena, Athena MUSE, Discuss, Hesiod, Kerberos,
  * Moira, and Zephyr are trademarks of the Massachusetts Institute of
  * Technology (MIT).  No commercial use of these trademarks may be made
