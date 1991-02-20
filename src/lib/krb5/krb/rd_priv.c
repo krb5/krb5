82c6875bf662aba40e9ea17cc88bc0b21eaabb5e
John Kohl <jtkohl@mit.edu>
1991-02-20 15:46:02 +0000

    remove spurious comma
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@1738 dc483132-0cff-0310-8789-dd5450dbe970

--- src/lib/krb5/krb/rd_priv.c
+++ src/lib/krb5/krb/rd_priv.c
@@ -63,7 +63,7 @@ OLDDECLARG(const krb5_address *, recv_addr)
 OLDDECLARG(krb5_int32, seq_number)
 OLDDECLARG(krb5_int32, priv_flags)
 OLDDECLARG(krb5_pointer, i_vector)
-OLDDECLARG(krb5_rcache, rcache),
+OLDDECLARG(krb5_rcache, rcache)
 OLDDECLARG(krb5_data *, outbuf)
 {
     krb5_error_code retval;
