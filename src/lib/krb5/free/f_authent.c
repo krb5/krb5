8790876c444859f3b04eaaae46b76401d3300760
John Kohl <jtkohl@mit.edu>
1991-02-21 15:24:33 +0000

    plug memory leak
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@1756 dc483132-0cff-0310-8789-dd5450dbe970

--- src/lib/krb5/free/f_authent.c
+++ src/lib/krb5/free/f_authent.c
@@ -2,7 +2,8 @@
  * $Source$
  * $Author$
  *
- * Copyright 1990 by the Massachusetts Institute of Technology.
+ * Copyright 1990,1991 by the Massachusetts Institute of Technology.
+ * All Rights Reserved.
  *
  * For copying and distribution information, please see the file
  * <krb5/copyright.h>.
@@ -24,7 +25,7 @@ krb5_free_authenticator(val)
 krb5_authenticator *val;
 {
     if (val->checksum)
-	xfree(val->checksum);
+	krb5_free_checksum(val->checksum);
     if (val->client)
 	krb5_free_principal(val->client);
     if (val->subkey)
