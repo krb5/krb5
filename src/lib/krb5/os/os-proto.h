e213589bfb771060e740a58878e53cf2827cadbb
John Kohl <jtkohl@mit.edu>
1991-02-25 15:36:28 +0000

    add krb5_unpack_full_ipaddr, krb5_make_full_ipaddr
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@1778 dc483132-0cff-0310-8789-dd5450dbe970

--- src/lib/krb5/os/os-proto.h
+++ src/lib/krb5/os/os-proto.h
@@ -3,15 +3,14 @@
  * $Author$
  * $Id$
  *
- * Copyright 1990 by the Massachusetts Institute of Technology.
+ * Copyright 1990,1991 by the Massachusetts Institute of Technology.
+ * All Rights Reserved.
  *
  * For copying and distribution information, please see the file
  * <krb5/copyright.h>.
  *
  * LIBOS internal function prototypes.
  */
-
-#include <krb5/copyright.h>
 
 #ifndef KRB5_LIBOS_INT_PROTO__
 #define KRB5_LIBOS_INT_PROTO__
@@ -22,5 +21,25 @@ krb5_error_code krb5_locate_kdc
 	       struct sockaddr **,
 	       int *));
 #endif
+
+#ifdef KRB5_USE_INET
+krb5_error_code krb5_unpack_full_ipaddr
+    PROTOTYPE((krb5_address *,
+	       krb5_int32 *,
+	       krb5_int16 *));
+
+#ifdef NARROW_PROTOTYPES
+krb5_error_code krb5_make_full_ipaddr
+    PROTOTYPE((krb5_int32,
+	       krb5_int16,
+	       krb5_address **));
+#else
+krb5_error_code krb5_make_full_ipaddr
+    PROTOTYPE((krb5_int32,
+	       int,			/* unsigned short promotes to signed
+					   int */
+	       krb5_address **));
+#endif /* NARROW_PROTOTYPES */
+#endif /* KRB5_USE_INET */
 
 #endif /* KRB5_LIBOS_INT_PROTO__ */
