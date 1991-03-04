43a6a05d1f06a7b45eda4a41ccc8114e6cc0ab25
John Kohl <jtkohl@mit.edu>
1991-03-04 13:12:37 +0000

    /
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@1843 dc483132-0cff-0310-8789-dd5450dbe970

---
+++ src/appl/simple/simple.h
@@ -0,0 +1,16 @@
+/*
+ * $Source$
+ * $Author$
+ * $Id$
+ *
+ * Copyright 1988,1991 by the Massachusetts Institute of Technology.
+ * All Rights Reserved.
+ *
+ * For copying and distribution information, please see the file
+ * <krb5/copyright.h>.
+ *
+ * Common definitions for the simple UDP-based Kerberos-mediated
+ * server & client applications.
+ */
+
+#define SERVICE	"sample"
