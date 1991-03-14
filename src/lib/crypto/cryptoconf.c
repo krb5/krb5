84f6b1f78fa64a79b9daa2dd04314816b2836d66
John Kohl <jtkohl@mit.edu>
1991-03-14 11:48:24 +0000

    update copyright
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@1909 dc483132-0cff-0310-8789-dd5450dbe970

--- src/lib/crypto/cryptoconf.c
+++ src/lib/crypto/cryptoconf.c
@@ -2,7 +2,8 @@
  * $Source$
  * $Author$
  *
- * Copyright 1990 by the Massachusetts Institute of Technology.
+ * Copyright 1990,1991 by the Massachusetts Institute of Technology.
+ * All Rights Reserved.
  *
  * For copying and distribution information, please see the file
  * <krb5/copyright.h>.
@@ -15,7 +16,6 @@ static char rcsid_cryptoconf_c[] =
 "$Id$";
 #endif	/* !lint & !SABER */
 
-#include <krb5/copyright.h>
 #include <krb5/config.h>
 #include <krb5/osconf.h>
 #include <krb5/krb5.h>
