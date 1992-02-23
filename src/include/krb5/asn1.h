541ba6414537ce52b8412c4cd575b752c63ab27a
John Carr <jfc@mit.edu>
1992-02-23 12:05:47 +0000

    Change include path from "asn.1/foo" to <krb5/asn.1/foo>
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@2211 dc483132-0cff-0310-8789-dd5450dbe970

--- src/include/krb5/asn1.h
+++ src/include/krb5/asn1.h
@@ -30,9 +30,9 @@
 #ifndef KRB5_ASN1__
 #define KRB5_ASN1__
 
-#include "asn.1/KRB5-types.h"
-#include "asn.1/asn1defs.h"
-#include "asn.1/KRB5-types-aux.h"
-#include "asn.1/encode.h"
+#include <krb5/asn.1/KRB5-types.h>
+#include <krb5/asn.1/asn1defs.h>
+#include <krb5/asn.1/KRB5-types-aux.h>
+#include <krb5/asn.1/encode.h>
 
 #endif /* KRB5_ASN1__ */
