1b19ee707a04739966938c2b9a7e16ebd270047f
Theodore Tso <tytso@mit.edu>
1992-08-27 22:40:29 +0000

    *** empty log message ***
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@2374 dc483132-0cff-0310-8789-dd5450dbe970

---
+++ src/lib/krb5/free/f_pwd_data.c
@@ -0,0 +1,44 @@
+/*
+ * $Source$
+ * $Author$
+ *
+ * Copyright 1990,1991 by the Massachusetts Institute of Technology.
+ * All Rights Reserved.
+ *
+ * Export of this software from the United States of America is assumed
+ *   to require a specific license from the United States Government.
+ *   It is the responsibility of any person or organization contemplating
+ *   export to obtain such a license before exporting.
+ * 
+ * WITHIN THAT CONSTRAINT, permission to use, copy, modify, and
+ * distribute this software and its documentation for any purpose and
+ * without fee is hereby granted, provided that the above copyright
+ * notice appear in all copies and that both that copyright notice and
+ * this permission notice appear in supporting documentation, and that
+ * the name of M.I.T. not be used in advertising or publicity pertaining
+ * to distribution of the software without specific, written prior
+ * permission.  M.I.T. makes no representations about the suitability of
+ * this software for any purpose.  It is provided "as is" without express
+ * or implied warranty.
+ * 
+ *
+ * krb5_free_pwd_data()
+ */
+
+#if !defined(lint) && !defined(SABER)
+static char rcsid_f_pwd_data [] =
+"$Id$";
+#endif	/* !lint & !SABER */
+
+#include <krb5/krb5.h>
+#include <krb5/ext-proto.h>
+
+void
+krb5_free_pwd_data(val)
+krb5_pwd_data *val;
+{
+    if (val->element)
+	krb5_free_pwd_sequences(val->element);
+    xfree(val);
+    return;
+}
---
+++ src/lib/krb5/free/f_pwd_seq.c
@@ -0,0 +1,45 @@
+/*
+ * $Source$
+ * $Author$
+ *
+ * Copyright 1990,1991 by the Massachusetts Institute of Technology.
+ * All Rights Reserved.
+ *
+ * Export of this software from the United States of America is assumed
+ *   to require a specific license from the United States Government.
+ *   It is the responsibility of any person or organization contemplating
+ *   export to obtain such a license before exporting.
+ * 
+ * WITHIN THAT CONSTRAINT, permission to use, copy, modify, and
+ * distribute this software and its documentation for any purpose and
+ * without fee is hereby granted, provided that the above copyright
+ * notice appear in all copies and that both that copyright notice and
+ * this permission notice appear in supporting documentation, and that
+ * the name of M.I.T. not be used in advertising or publicity pertaining
+ * to distribution of the software without specific, written prior
+ * permission.  M.I.T. makes no representations about the suitability of
+ * this software for any purpose.  It is provided "as is" without express
+ * or implied warranty.
+ * 
+ *
+ * krb5_free_pwd_sequences()
+ */
+
+#if !defined(lint) && !defined(SABER)
+static char rcsid_f_pwd_seq [] =
+"$Id$";
+#endif	/* !lint & !SABER */
+
+#include <krb5/krb5.h>
+#include <krb5/ext-proto.h>
+
+void
+krb5_free_pwd_sequences(val)
+passwd_phrase_element **val;
+{
+    if ((*val)->passwd)
+	xfree((*val)->passwd);
+    if ((*val)->phrase)
+	xfree((*val)->phrase);
+    return;
+}
