99081e9069e6aba144c770bfca7161ef7f4629cc
Theodore Tso <tytso@mit.edu>
1992-09-24 23:08:25 +0000

    *** empty log message ***
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@2384 dc483132-0cff-0310-8789-dd5450dbe970

---
+++ src/admin/edit/kdb5_edit.h
@@ -0,0 +1,52 @@
+/*
+ * $Source$
+ * $Author$
+ *
+ * Copyright 1992 by the Massachusetts Institute of Technology.
+ * All Rights Reserved.
+ *
+ * Export of this software from the United States of America is assumed
+ *   to require a specific license from the United States Government.
+ *   It is the responsibility of any person or organization contemplating
+ *   export to obtain such a license before exporting.
+ * 
+ * WITHIN THAT CONSTRAINT, permission to use, copy, modify, and
+ * distribute this software and its documentation for any purpose and
+ * without fee is hereby granted, provided that the above copyright
+ * notice appear in all copies and that both that copyright notice and
+ * this permission notice appear in supporting documentation, and that
+ * the name of M.I.T. not be used in advertising or publicity pertaining
+ * to distribution of the software without specific, written prior
+ * permission.  M.I.T. makes no representations about the suitability of
+ * this software for any purpose.  It is provided "as is" without express
+ * or implied warranty.
+ * 
+ */
+
+#define REALM_SEP	'@'
+#define REALM_SEP_STR	"@"
+
+extern char *progname;
+extern char *Err_no_database;
+
+struct mblock {
+    krb5_deltat max_life;
+    krb5_deltat max_rlife;
+    krb5_timestamp expiration;
+    krb5_flags flags;
+    krb5_kvno mkvno;
+};
+
+/* krb5_kvno may be narrow */
+#include <krb5/widen.h>
+void add_key PROTOTYPE((char const *, char const *, krb5_const_principal,
+			const krb5_keyblock *, krb5_kvno, struct saltblock *));
+void enter_rnd_key PROTOTYPE((char **, const krb5_principal, krb5_kvno));
+void enter_pwd_key PROTOTYPE((char *, char *, krb5_const_principal,
+			      krb5_const_principal, krb5_kvno, int));
+int set_dbname_help PROTOTYPE((char *, char *));
+
+#include <krb5/narrow.h>
+
+
+
