16b7b9460f1e55d12c5c7080bbb1787f7aeb1917
Theodore Tso <tytso@mit.edu>
1993-06-03 11:47:12 +0000

    Changed function delcaration to support doing two ways of checksumming a TGS_REQ, because of an ASN.1 encoding screwup.
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@2576 dc483132-0cff-0310-8789-dd5450dbe970

--- src/kdc/kdc_util.h
+++ src/kdc/kdc_util.h
@@ -51,6 +51,7 @@ krb5_error_code kdc_convert_key PROTOTYPE((krb5_keyblock *,
 					   int));
 krb5_error_code kdc_process_tgs_req PROTOTYPE((krb5_kdc_req *,
 					       const krb5_fulladdr *,
+					       krb5_data *,
 					       krb5_tkt_authent **));
 
 krb5_error_code kdc_get_server_key PROTOTYPE((krb5_ticket *,
@@ -63,7 +64,7 @@ krb5_error_code process_as_req PROTOTYPE((krb5_kdc_req *,
 					  krb5_data ** ));
 
 /* do_tgs_req.c */
-krb5_error_code process_tgs_req PROTOTYPE((krb5_kdc_req *,
+krb5_error_code process_tgs_req PROTOTYPE((krb5_data *,
 					   const krb5_fulladdr *,
 					   int, 
 					   krb5_data ** ));
