7f85a380aacb800293832f46f74430d92311d320
Theodore Tso <tytso@mit.edu>
1993-10-15 16:27:18 +0000

    Added krb5_free_authenticator_contents()
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@2668 dc483132-0cff-0310-8789-dd5450dbe970

---
+++ src/lib/krb5/free/f_auth_cnt.c
@@ -0,0 +1,50 @@
+/*
+ * $Source$
+ * $Author$
+ *
+ * Copyright 1990,1991 by the Massachusetts Institute of Technology.
+ * All Rights Reserved.
+ * All Rights Reserved.
+ *
+ * Export of this software from the United States of America may
+ *   require a specific license from the United States Government.
+ *   It is the responsibility of any person or organization contemplating
+ *   export to obtain such a license before exporting.
+ * 
+ * WITHIN THAT CONSTRAINT, permission to use, copy, modify, and
+ * distribute this software and its documentation for any purpose and
+ * without fee is hereby granted, provided that the above copyright
+ * notice appear in all copies and that both that copyright notice and
+ * this permission notice appear in supporting documentation, and that
+ * the name of M.I.T. not be used in advertising or publicity pertaining
+ * to distribution of the software without specific, written prior
+ * permission.  M.I.T. makes no representations about the suitability of
+ * this software for any purpose.  It is provided "as is" without express
+ * or implied warranty.
+ * 
+ *
+ * krb5_free_authenticator()
+ */
+
+#if !defined(lint) && !defined(SABER)
+static char rcsid_f_authent_c [] =
+"$Id$";
+#endif	/* !lint & !SABER */
+
+#include <krb5/krb5.h>
+#include <krb5/ext-proto.h>
+
+void
+krb5_free_authenticator_contents(val)
+krb5_authenticator *val;
+{
+    if (val->checksum)
+	krb5_free_checksum(val->checksum);
+    if (val->client)
+	krb5_free_principal(val->client);
+    if (val->subkey)
+	krb5_free_keyblock(val->subkey);
+    if (val->authorization_data)        
+       krb5_free_authdata(val->authorization_data);
+    return;
+}
