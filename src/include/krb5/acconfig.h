a58ac1b844b03d10ab35725dfe8eb5aeb15f8c74
Mark Eichin <eichin@mit.edu>
1994-06-09 23:38:25 +0000

    use autoconf to set config variables
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@3691 dc483132-0cff-0310-8789-dd5450dbe970

---
+++ src/include/krb5/acconfig.h
@@ -0,0 +1,27 @@
+/* just stuff needed by kerberos 5 */
+
+#undef ANSI_STDIO
+#undef BITS32
+#undef HAS_ANSI_CONST
+#undef HAS_ANSI_VOLATILE
+#undef HAS_STDLIB_H
+#undef HAS_STRDUP
+#undef HAS_VOID_TYPE
+#undef KRB5_PROVIDE_PROTOTYPES
+#undef NEED_SYS_FCNTL_H
+#undef NO_NESTED_PROTOTYPES
+#undef NO_STDLIB_H
+#undef NO_YYLINENO
+#undef POSIX_FILE_LOCKS
+#undef POSIX_SIGTYPE
+#undef POSIX_TERMIOS
+#undef POSIX_TYPES
+#undef STDARG_PROTOTYPES
+#undef USE_DIRENT_H
+#undef USE_STRING_H
+#undef WAIT_USES_INT
+#undef krb5_sigtype
+#undef HAS_UNISTD_H
+#undef KRB5_USE_INET
+#undef ODBM
+#undef USE_SYS_TIME_H
