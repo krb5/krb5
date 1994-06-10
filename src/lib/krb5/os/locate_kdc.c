31859eb7bf349728385c5b16f238f1f1447d49bd
Theodore Tso <tytso@mit.edu>
1994-06-10 18:57:09 +0000

    Use the precompile ports as the default if the proper ones aren't in /etc/services.
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@3713 dc483132-0cff-0310-8789-dd5450dbe970

--- src/lib/krb5/os/locate_kdc.c
+++ src/lib/krb5/os/locate_kdc.c
@@ -68,8 +68,8 @@ krb5_locate_kdc(realm, addr_pp, naddrs)
     struct hostent *hp;
     struct servent *sp;
 #ifdef KRB5_USE_INET
-    u_short udpport = 0;		/* 0 is an invalid UDP port #. */
-    u_short sec_udpport = 0;		/* 0 is an invalid UDP port #. */
+    u_short udpport = KRB5_DEFAULT_PORT;
+    u_short sec_udpport = KRB5_DEFAULT_SEC_PORT;
 #endif
 
     hostlist = 0;
