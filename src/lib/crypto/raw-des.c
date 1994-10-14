e4d3983a4b6d8aa1ab6d9cea3248936b43032475
Theodore Tso <tytso@mit.edu>
1994-10-14 17:29:10 +0000

    Move the various flavors of cryptosystem support -- raw des, des-md5, des-crc to the top level crypto library.
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@4512 dc483132-0cff-0310-8789-dd5450dbe970

---
+++ src/lib/crypto/raw-des.c
@@ -0,0 +1,115 @@
+/*
+ * lib/crypto/des-crc.32
+ *
+ * Copyright 1994 by the Massachusetts Institute of Technology.
+ * All Rights Reserved.
+ *
+ * Export of this software from the United States of America may
+ *   require a specific license from the United States Government.
+ *   It is the responsibility of any person or organization contemplating
+ *   export to obtain such a license before exporting.
+ * 
+ * WITHIN THAT CONSTRAINT, permission to use, copy, modify, and
+ * distribute this software and its documentation for any purpose and
+ * without fee is hereby granted, provided that the above copyright
+ * notice appear in all copies and that both that copyright notice and
+ * this permission notice appear in supporting documentation, and that
+ * the name of M.I.T. not be used in advertising or publicity pertaining
+ * to distribution of the software without specific, written prior
+ * permission.  M.I.T. makes no representations about the suitability of
+ * this software for any purpose.  It is provided "as is" without express
+ * or implied warranty.
+ * 
+ */
+
+#include <krb5/krb5.h>
+#include <krb5/ext-proto.h>
+#include <krb5/los-proto.h>
+#include <krb5/crc-32.h>
+
+#include "./des/des_int.h"
+
+static krb5_error_code mit_raw_des_encrypt_func
+    PROTOTYPE(( krb5_const_pointer, krb5_pointer, const size_t,
+               krb5_encrypt_block *, krb5_pointer ));
+
+static krb5_error_code mit_raw_des_decrypt_func
+    PROTOTYPE(( krb5_const_pointer, krb5_pointer, const size_t,
+               krb5_encrypt_block *, krb5_pointer ));
+
+static krb5_cryptosystem_entry mit_raw_des_cryptosystem_entry = {
+    0,
+    mit_raw_des_encrypt_func,
+    mit_raw_des_decrypt_func,
+    mit_des_process_key,
+    mit_des_finish_key,
+    mit_des_string_to_key,
+    mit_des_init_random_key,
+    mit_des_finish_random_key,
+    mit_des_random_key,
+    sizeof(mit_des_cblock),
+    0,
+    sizeof(mit_des_cblock),
+    ETYPE_RAW_DES_CBC,
+    KEYTYPE_DES
+    };
+
+krb5_cs_table_entry krb5_raw_des_cst_entry = {
+    0,
+    &mit_raw_des_cryptosystem_entry,
+    0
+    };
+
+static krb5_error_code
+    mit_raw_des_decrypt_func(DECLARG(krb5_const_pointer, in),
+			     DECLARG(krb5_pointer, out),
+			     DECLARG(const size_t, size),
+			     DECLARG(krb5_encrypt_block *, key),
+			     DECLARG(krb5_pointer, ivec))
+OLDDECLARG(krb5_const_pointer, in)
+OLDDECLARG(krb5_pointer, out)
+OLDDECLARG(const size_t, size)
+OLDDECLARG(krb5_encrypt_block *, key)
+OLDDECLARG(krb5_pointer, ivec)
+{
+    return (mit_des_cbc_encrypt (in, 
+				 out, 
+				 size, 
+				 (struct mit_des_ks_struct *)key->priv, 
+				 ivec ? ivec : key->key->contents,
+				 MIT_DES_DECRYPT));
+}
+
+static krb5_error_code
+    mit_raw_des_encrypt_func(DECLARG(krb5_const_pointer, in),
+			     DECLARG(krb5_pointer, out),
+			     DECLARG(const size_t, size),
+			     DECLARG(krb5_encrypt_block *, key),
+			     DECLARG(krb5_pointer, ivec))
+OLDDECLARG(krb5_const_pointer, in)
+OLDDECLARG(krb5_pointer, out)
+OLDDECLARG(const size_t, size)
+OLDDECLARG(krb5_encrypt_block *, key)
+OLDDECLARG(krb5_pointer, ivec)
+{
+   int sumsize;
+
+   /* round up to des block size */
+
+   sumsize =  krb5_roundup(size, sizeof(mit_des_cblock));
+
+   /* assemble crypto input into the output area, then encrypt in place. */
+
+   memset((char *)out, 0, sumsize);
+   memcpy((char *)out, (char *)in, size);
+
+    /* We depend here on the ability of this DES implementation to
+       encrypt plaintext to ciphertext in-place. */
+    return (mit_des_cbc_encrypt (out, 
+				 out, 
+				 sumsize, 
+				 (struct mit_des_ks_struct *)key->priv, 
+				 ivec ? ivec : key->key->contents,
+				 MIT_DES_ENCRYPT));
+}
+
