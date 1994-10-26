a19783f85bc30cb396b1b4c9308ecc90bdc2cfe8
Theodore Tso <tytso@mit.edu>
1994-10-26 03:36:19 +0000

    Once more, with the current type!
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@4572 dc483132-0cff-0310-8789-dd5450dbe970

--- src/tests/hammer/kdc5_hammer.c
+++ src/tests/hammer/kdc5_hammer.c
@@ -103,7 +103,7 @@ main(argc, argv)
     int depth;
     char ctmp[4096], ctmp2[BUFSIZ], stmp[4096], stmp2[BUFSIZ];
     krb5_principal client_princ;
-    errcode_t retval;
+    krb5_error_code retval;
 
     krb5_init_ets();
 
