b3a9fed1e98b4f5e107e0f97f84edb20e112b1bf
Theodore Tso <tytso@mit.edu>
1994-11-08 02:23:44 +0000

    Use POSIX method of obtaining fd table size, if available
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@4630 dc483132-0cff-0310-8789-dd5450dbe970

--- src/appl/bsd/getdtablesize.c
+++ src/appl/bsd/getdtablesize.c
@@ -1,8 +1,19 @@
+#ifdef HAVE_UNISTD_H
+#include <unistd.h>
+#endif
+#include <limits.h>
+
+#ifdef _SC_OPEN_MAX
+int getdtablesize() {
+    return sysconf(_SC_OPEN_MAX);
+}
+#else
+#include <sys/resource.h>
 /* Placed in the Public Domain by Mark Eichin, Cygnus Support 1994 */
 
-#include <sys/resource.h>
 int getdtablesize() {
-  struct rlimit rl;
-  getrlimit(RLIMIT_NOFILE, &rl);
-  return rl.rlim_cur;
+    struct rlimit rl;
+    getrlimit(RLIMIT_NOFILE, &rl);
+    return rl.rlim_cur;
 }
+#endif
