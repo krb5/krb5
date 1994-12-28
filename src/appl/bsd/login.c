5e63793b786ce14c68b8da449134ddb4f4ecda84
Richard Basch <probe@mit.edu>
1994-12-28 21:51:29 +0000

    Removed a reference to CBAUD that caused compilation errors
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@4772 dc483132-0cff-0310-8789-dd5450dbe970

--- src/appl/bsd/login.c
+++ src/appl/bsd/login.c
@@ -1285,13 +1285,11 @@ char *speeds[] = {
 #define	NSPEEDS	(sizeof(speeds) / sizeof(speeds[0]))
 
 #ifdef POSIX_TERMIOS
-#ifndef CBAUD
 /* this must be in sync with the list above */
 speed_t b_speeds[] = {
 	B0, B50, B75, B110, B134, B150, B200, B300, B600,
 	B1200, B1800, B2400, B4800, B9600, B19200, B38400,
 };
-#endif
 #endif
 
 doremoteterm(tp)
