99ffce1bda90caba8f6dcfe94783735979021a1b
Theodore Tso <tytso@mit.edu>
1995-02-10 22:59:00 +0000

    Remove needless #include of <krb5/asn.1/encode.h>
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@4941 dc483132-0cff-0310-8789-dd5450dbe970

--- src/kadmin/client/kadmin_msnd.c
+++ src/kadmin/client/kadmin_msnd.c
@@ -45,7 +45,6 @@
 #include <krb5/asn1.h>
 #include <krb5/config.h>
 #include <krb5/base-defs.h>
-#include <krb5/asn.1/encode.h>
 #include <krb5/adm_err.h>
 #include <krb5/errors.h>
 #include <krb5/kdb5_err.h>
