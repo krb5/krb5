6e90b794d221f0b98a72c5e2977daa11bfb61768
Theodore Tso <tytso@mit.edu>
1995-05-27 01:45:10 +0000

    Added new function, des_cblock_print_file(), which is referenced by libkrb4.a.  (Even though it's never used, some OS's demand that all of the references in a shared library have to be resolved.)
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@5906 dc483132-0cff-0310-8789-dd5450dbe970

---
+++ src/lib/des425/util.c
@@ -0,0 +1,38 @@
+/*
+ * $Source$
+ * $Author$
+ *
+ * Copyright 1988 by the Massachusetts Institute of Technology.
+ *
+ * For copying and distribution information, please see the file
+ * <mit-copyright.h>.
+ *
+ * Miscellaneous debug printing utilities
+ */
+
+#ifndef	lint
+static char rcsid_util_c[] =
+"$Header$";
+#endif	lint
+
+#include <stdio.h>
+
+/* Application include files */
+#include "k5-int.h"
+#include "des.h"
+
+des_cblock_print_file(x, fp)
+    des_cblock *x;
+    FILE *fp;
+{
+    unsigned char *y = (unsigned char *) x;
+    register int i = 0;
+    fprintf(fp," 0x { ");
+
+    while (i++ < 8) {
+	fprintf(fp,"%x",*y++);
+	if (i < 8)
+	    fprintf(fp,", ");
+    }
+    fprintf(fp," }");
+}
