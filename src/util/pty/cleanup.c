a1fc76083d7ddc56d7ad2d994250d903a31a6311
Sam Hartman <hartmans@mit.edu>
1995-08-03 19:26:56 +0000

    Attempt to HUP processes associated with the pty when it is revoked if vhangup is not available.
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@6402 dc483132-0cff-0310-8789-dd5450dbe970

--- src/util/pty/cleanup.c
+++ src/util/pty/cleanup.c
@@ -45,6 +45,22 @@ long pty_cleanup (slave, pid, update_utmp)
 #endif
 #ifdef HAVE_REVOKE
     revoke(slave);
+    /*
+     * Revoke isn't guaranteed to send a SIGHUP to the processes it
+     * dissociates from the terminal.  The best solution without a Posix
+     * mechanism for forcing a hangup is to killpg() the process
+     * group of the pty.  This will at least kill the shell and
+     * hopefully, the child processes.  This is not always the case, however.
+     * If the shell puts each job in a process group and doesn't pass
+     * along SIGHUP, all processes may not die.
+     */
+    if ( pid > 0 ) {
+    #ifdef HAVE_KILLPG
+	killpg(pid, SIGHUP);
+	#else
+	kill( -(pid), SIGHUP );
+#endif /*HAVE_KILLPG*/
+    }
 #else /* HAVE_REVOKE*/
     #ifdef VHANG_LAST
     if ( retval = ( pty_open_ctty( slave, &fd ))) 
