1fa3d45695d9e25132797b802167d333ee92529c
Chris Provenzano <proven@mit.edu>
1995-09-06 19:02:18 +0000

    * osconf.h : s/keytype/enctype/g, s/KEYTYPE/ENCTYPE/g
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@6704 dc483132-0cff-0310-8789-dd5450dbe970

--- src/include/krb5/stock/osconf.h
+++ src/include/krb5/stock/osconf.h
@@ -55,7 +55,7 @@
 #define	DEFAULT_KDC_PROFILE	"@PREFIX/lib/krb5kdc/kdc.prof"
 #define	KDC_PROFILE_ENV		"KRB5_KDC_PROFILE"
 
-#define	DEFAULT_KDC_KEYTYPE	KEYTYPE_DES_CBC_CRC
+#define	DEFAULT_KDC_ENCTYPE	ENCTYPE_DES_CBC_CRC
 #define KDCRCACHE		"dfl:krb5kdc_rcache"
 
 #define KDC_PORTNAME		"kerberos" /* for /etc/services or equiv. */
