f1d9fbc297704e875edd1d2daf3ebc76fae8e9a3
Mark Eichin <eichin@mit.edu>
1995-10-02 19:23:54 +0000

    * kinit.c (KRB5_DEFAULT_LIFE): default life should be 10 hours, 	not eight, for practical/social reasons.
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@6919 dc483132-0cff-0310-8789-dd5450dbe970

--- src/clients/kinit/kinit.c
+++ src/clients/kinit/kinit.c
@@ -34,7 +34,7 @@
 #endif
 
 #define KRB5_DEFAULT_OPTIONS 0
-#define KRB5_DEFAULT_LIFE 60*60*8 /* 8 hours */
+#define KRB5_DEFAULT_LIFE 60*60*10 /* 10 hours */
 
 extern int optind;
 extern char *optarg;
