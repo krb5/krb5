a94eba01951fee60833ef2d42f3e224ba23cc2dc
Theodore Tso <tytso@mit.edu>
1995-11-01 03:07:54 +0000

    Make mit_des_finish_key() safe to call even if there is no key that needs to be freed.
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@7009 dc483132-0cff-0310-8789-dd5450dbe970

--- src/lib/crypto/des/finish_key.c
+++ src/lib/crypto/des/finish_key.c
@@ -37,8 +37,10 @@ krb5_error_code
 mit_des_finish_key (eblock)
     krb5_encrypt_block FAR * eblock;
 {
-    memset((char *)eblock->priv, 0, (size_t) eblock->priv_size);
-    free(eblock->priv);
+    if (eblock->priv) {
+	memset((char *)eblock->priv, 0, (size_t) eblock->priv_size);
+	free(eblock->priv);
+    }
     eblock->priv = 0;
     eblock->priv_size = 0;
     /* free/clear other stuff here? */
