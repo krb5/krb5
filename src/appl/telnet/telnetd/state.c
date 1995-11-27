edb56f18a74e4346f475572ca42d97421aa3cd54
Tom Yu <tlyu@mit.edu>
1995-11-27 21:00:40 +0000

    * state.c: use P properly so it won't break non-ansi compilers
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@7126 dc483132-0cff-0310-8789-dd5450dbe970

--- src/appl/telnet/telnetd/state.c
+++ src/appl/telnet/telnetd/state.c
@@ -44,7 +44,7 @@ unsigned char	will[] = { IAC, WILL, '%', 'c', 0 };
 unsigned char	wont[] = { IAC, WONT, '%', 'c', 0 };
 int	not42 = 1;
 
-static int envvarok(P(char *));
+static int envvarok P((char *));
 
 /*
  * Buffer for sub-options, and macros
