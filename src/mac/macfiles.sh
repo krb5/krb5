1beb992505eeb0faa107ebe140aa7a6a2a53d3e3
Theodore Tso <tytso@mit.edu>
1996-02-07 05:30:24 +0000

    Folded in danw's changes to allow building Makefiles for the Macintosh.  We now can build MPW makefiles which are interpreted by CodeWarrior.
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@7444 dc483132-0cff-0310-8789-dd5450dbe970

---
+++ src/mac/macfiles.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+for DIR do
+  for SDIR in `sed -n -e 's/MAC_SUBDIRS.*=//p' $DIR/Makefile.in`; do
+    awk '/SRCS?[ 	]*=/, /[^\\]$/' $DIR/$SDIR/Makefile.in | \
+      tr ' 	' '\012\012' | sed -n -e 's|.*[/)]\([A-Za-z0-9_]*\.c\).*|\1|' -e 's|\(.*\.c\)|'$DIR/$SDIR'/\1|p';
+    ls -1 $DIR/$SDIR/*.h 2> /dev/null
+    mac/macfiles.sh $DIR/$SDIR;
+  done
+done
