4b82a8bc0b9e9e7f8e117260275c2e0e38722111
Theodore Tso <tytso@mit.edu>
1996-03-20 05:18:11 +0000

    macfiles.sh: Improved matching of the SRCS declaration.  It must not 	be commented out; it must start on the first column of the line.
    Makefile.tmpl (KH): Fix typos in Dan's original template.
    
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@7681 dc483132-0cff-0310-8789-dd5450dbe970

--- src/mac/macfiles.sh
+++ src/mac/macfiles.sh
@@ -2,7 +2,7 @@
 
 for DIR do
   for SDIR in `sed -n -e 's/MAC_SUBDIRS.*=//p' $DIR/Makefile.in`; do
-    awk '/SRCS?[ 	]*=/, /[^\\]$/' $DIR/$SDIR/Makefile.in | \
+    awk '/^SRCS?[ 	]*=/, /[^\\]$/' $DIR/$SDIR/Makefile.in | \
       tr ' 	' '\012\012' | sed -n -e 's|.*[/)]\([A-Za-z0-9_]*\.c\).*|\1|' -e 's|\(.*\.c\)|'$DIR/$SDIR'/\1|p';
     ls -1 $DIR/$SDIR/*.h 2> /dev/null
     mac/macfiles.sh $DIR/$SDIR;
