be6c0a0e433dd8176ec31567bd92b057d8885314
Richard Basch <probe@mit.edu>
1996-04-01 15:15:28 +0000

    Leading/trailing colons should not be there based on the variable usage. Solaris test was incorrect.
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@7746 dc483132-0cff-0310-8789-dd5450dbe970

--- src/appl/bsd/loginpaths.h
+++ src/appl/bsd/loginpaths.h
@@ -1,19 +1,19 @@
 /* here are actual path values from each operating system supported. */
 /* LPATH is from rlogin, for login.c; RPATH is from rsh, for rshd.c */
 #ifdef sun
-#ifdef solaris20
-#define RPATH ":/usr/bin"
-#define LPATH "/usr/bin:"
+#ifdef __SVR4
+#define RPATH "/usr/bin"
+#define LPATH "/usr/bin"
 #else
 /* sun3 and sun4 */
-#define LPATH ":/usr/ucb:/bin:/usr/bin"
-#define RPATH ":/usr/ucb:/bin:/usr/bin"
+#define LPATH "/usr/ucb:/bin:/usr/bin"
+#define RPATH "/usr/ucb:/bin:/usr/bin"
 #endif
 #endif
 
 #ifdef __ultrix
-#define LPATH ":/usr/ucb:/bin:/usr/bin"
-#define RPATH ":/usr/ucb:/bin:/usr/bin"
+#define LPATH "/usr/ucb:/bin:/usr/bin"
+#define RPATH "/usr/ucb:/bin:/usr/bin"
 #endif
 
 #ifdef hpux
@@ -23,24 +23,24 @@
 #endif
 
 #ifdef NeXT
-#define LPATH ":/usr/ucb:/bin:/usr/bin:/usr/local/bin"
-#define RPATH "/bin:/usr/ucb:/usr/bin:"
+#define LPATH "/usr/ucb:/bin:/usr/bin:/usr/local/bin"
+#define RPATH "/bin:/usr/ucb:/usr/bin"
 #endif
 
 #ifdef _IBMR2
 /* 3.2.0 */ 
-#define LPATH "/usr/bin:/etc:/usr/sbin:/usr/ucb:/usr/bin/X11:/sbin"
-#define RPATH ":/usr/ucb:/bin:/usr/bin:/usr/bin/X11"
+#define LPATH "/usr/bin:/usr/ucb:/usr/bin/X11"
+#define RPATH "/usr/bin:/usr/ucb:/usr/bin/X11"
 #endif
 
 #ifdef __SCO__
 #define LPATH "/bin:/usr/bin:/usr/dbin:/usr/ldbin"
-#define RPATH "/bin:/usr/bin:/usr/local/bin::"
+#define RPATH "/bin:/usr/bin:/usr/local/bin"
 #endif
 
 #ifdef sgi
-#define LPATH ":/usr/sbin:/usr/bsd:/usr/bin:/bin:/usr/bin/X11"
-#define RPATH ":/usr/sbin:/usr/bsd:/usr/bin:/bin:/usr/bin/X11"
+#define LPATH "/usr/sbin:/usr/bsd:/usr/bin:/bin:/usr/bin/X11"
+#define RPATH "/usr/sbin:/usr/bsd:/usr/bin:/bin:/usr/bin/X11"
 #endif
 
 #ifdef linux
@@ -56,13 +56,13 @@
 #ifdef __alpha
 #ifdef __osf__
 #define LPATH "/usr/bin:."
-#define RPATH "/usr/bin:/bin:"
+#define RPATH "/usr/bin:/bin"
 #endif
 #endif
 
 #ifdef __pyrsoft
 #ifdef MIPSEB
-#define RPATH ":/bin:/usr/bin"
+#define RPATH "/bin:/usr/bin"
 #define LPATH "/usr/bin:/usr/ccs/bin:/usr/ucb:."
 #endif
 #endif
@@ -77,7 +77,7 @@
 #ifndef LPATH
 #ifdef __svr4__
 /* taken from unixware, sirius... */
-#define RPATH ":/bin:/usr/bin:/usr/X/bin"
+#define RPATH "/bin:/usr/bin:/usr/X/bin"
 #define LPATH "/usr/bin:/usr/dbin:/usr/dbin"
 #endif
 #endif
