ac8082b7387985c68eaada39d14399496dbcd42d
Ezra Peisach <epeisach@mit.edu>
1996-05-10 16:55:07 +0000

    * shs.c (longReverse): Remove extraneous \. 		(expand): Start #define in first column.
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@7976 dc483132-0cff-0310-8789-dd5450dbe970

--- src/lib/crypto/sha/shs.c
+++ src/lib/crypto/sha/shs.c
@@ -48,10 +48,10 @@
    for this information */
 
 #ifdef NEW_SHS
-  #define expand(W,i) ( W[ i & 15 ] = ROTL( 1, ( W[ i & 15 ] ^ W[ i - 14 & 15 ] ^ \
+#define expand(W,i) ( W[ i & 15 ] = ROTL( 1, ( W[ i & 15 ] ^ W[ i - 14 & 15 ] ^ \
                                                  W[ i - 8 & 15 ] ^ W[ i - 3 & 15 ] ) ) )
 #else
-  #define expand(W,i) ( W[ i & 15 ] ^= W[ i - 14 & 15 ] ^ W[ i - 8 & 15 ] ^ W[ i - 3 & 15 ] )
+#define expand(W,i) ( W[ i & 15 ] ^= W[ i - 14 & 15 ] ^ W[ i - 8 & 15 ] ^ W[ i - 3 & 15 ] )
 #endif /* NEW_SHS */
 
 /* The prototype SHS sub-round.  The fundamental sub-round is:
@@ -230,7 +230,7 @@ void longReverse( LONG *buffer, int byteCount )
     byteCount /= sizeof( LONG );
     while( byteCount-- ) {
         value = *buffer;
-        value = ( ( value & 0xFF00FF00L ) >> 8  ) | \
+        value = ( ( value & 0xFF00FF00L ) >> 8  ) | 
                 ( ( value & 0x00FF00FFL ) << 8 );
         *buffer++ = ( value << 16 ) | ( value >> 16 );
     }
