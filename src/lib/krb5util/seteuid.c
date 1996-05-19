36dc00f03da4c6be53018ee7702c5752f63fad8a
Sam Hartman <hartmans@mit.edu>
1996-05-19 18:56:50 +0000

    As per mail describing the ksu problem, invent a krb5util function to properly set the euid on all systems where it is possible. Ksu cannot be used without this function in a secure manner.
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@8052 dc483132-0cff-0310-8789-dd5450dbe970

---
+++ src/lib/krb5util/seteuid.c
@@ -0,0 +1,53 @@
+/*
+ * krb5_seteuid: Attempt to set the effective user ID of the current process
+ * in such a way it can be restored lated.
+ *
+ * Copyright 1996 by the Massachusetts Institute of Technology.
+ *
+ * 
+ * Permission to use, copy, modify, and distribute this software and
+ * its documentation for any purpose and without fee is hereby
+ * granted, provided that the above copyright notice appear in all
+ * copies and that both that copyright notice and this permission
+ * notice appear in supporting documentation, and that the name of
+ * M.I.T. not be used in advertising or publicity pertaining to
+ * distribution of the software without specific, written prior
+ * permission.  M.I.T. makes no representations about the suitability
+ * of this software for any purpose.  It is provided "as is" without
+ * express or implied warranty.
+ * 
+ */
+
+
+#ifdef HAVE_UNISTD_H
+#include <unistd.h>
+#endif
+
+#ifdef HAVE_STDLIB_H
+#include <stdlib.h>
+#endif
+
+#include <errno.h>
+
+int krb5_seteuid( euid)
+  uid_t euid;
+{
+  #if defined(_POSIX_SAVED_IDS) && defined(HAVE_SETEUID)
+  return  (seteuid(euid)) ;
+#else
+# if defined(HAVE_SETRESUID)
+    return (setresuid(getuid(), euid, getuid())) ;
+# else
+#  if defined(HAVE_SETREUID)
+    return setreuid(geteuid(), euid); 
+#else /*HAVE_SETREUID*/
+    /* You need to add a case to deal with this operating system.*/
+    errno = EPERM;
+  return -1;
+  
+#  endif /* HAVE_SETREUID */
+# endif /* HAVE_SETRESUID */
+#endif /* _POSIX_SAVED_IDS */
+
+
+}
