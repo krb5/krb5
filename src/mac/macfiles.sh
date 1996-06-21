338187d7930c688767f24e15a9d1e5551f7c1d5e
Theodore Tso <tytso@mit.edu>
1996-06-21 15:30:56 +0000

    Call macfiles.sh explicitly using /bin/sh, so that it works even if the script doesn't have execute permission set.
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@8532 dc483132-0cff-0310-8789-dd5450dbe970

--- src/mac/macfiles.sh
+++ src/mac/macfiles.sh
@@ -7,6 +7,6 @@ for DIR do
     awk '/^SRCS?[ 	]*=/, /[^\\]$/' $DIR/$SDIR/Makefile.in | \
       tr ' 	' '\012\012' | sed -n -e 's|.*[/)]\([A-Za-z0-9_]*\.c\).*|\1|' -e 's|\(.*\.c\)|'$DIR/$SDIR'/\1|p';
     ls -1 $DIR/$SDIR/*.h 2> /dev/null
-    mac/macfiles.sh $DIR/$SDIR;
+    /bin/sh mac/macfiles.sh $DIR/$SDIR;
   done
 done
