7ac506884990e2b0fc2ebacb08a3c48f99ad2a38
Theodore Tso <tytso@mit.edu>
1996-07-23 04:39:49 +0000

    Add contents of util/db2/acconfig.h to the top-level acconfig.h so that util/reconf works correctly.
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@8779 dc483132-0cff-0310-8789-dd5450dbe970

--- src/acconfig.h
+++ src/acconfig.h
@@ -38,3 +38,26 @@
 
 /* Define if Kerberos V4 backwards compatibility should be supported */
 #undef KRB5_KRB4_COMPAT
+
+/*
+ * The stuff following here is taken from util/db2/acconfig.h
+ */
+
+#undef ssize_t
+
+/* BSD4.3, non-posix types */
+
+#undef u_char
+#undef u_short
+#undef u_int
+#undef u_long
+
+/* sized types used by db internals */
+
+#undef int8_t
+#undef u_int8_t
+#undef int16_t
+#undef u_int16_t
+#undef int32_t
+#undef u_int32_t
+
