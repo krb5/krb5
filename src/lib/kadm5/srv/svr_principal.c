26415263231eaafd37f5c361a63e5ab75feb8703
Sam Hartman <hartmans@mit.edu>
1996-07-27 02:23:23 +0000

    It's not an out-of-space error if malloc(0) returns 0. So, when copying key_data, only call malloc if the length is non-zero.
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@8850 dc483132-0cff-0310-8789-dd5450dbe970

--- src/lib/kadm5/srv/svr_principal.c
+++ src/lib/kadm5/srv/svr_principal.c
@@ -57,19 +57,21 @@ kadm5_ret_t krb5_copy_key_data_contents(context, from, to)
      idx = (from->key_data_ver == 1 ? 1 : 2);
 
      for (i = 0; i < idx; i++) {
-	  to->key_data_contents[i] = malloc(from->key_data_length[i]);
-	  if (to->key_data_contents[i] == NULL) {
-	       for (i = 0; i < idx; i++) {
-		    if (to->key_data_contents[i]) {
-			 memset(to->key_data_contents[i], 0,
-				to->key_data_length[i]);
-			 free(to->key_data_contents[i]);
-		    }
-	       }
-	       return ENOMEM;
-	  }
-	  memcpy(to->key_data_contents[i], from->key_data_contents[i],
-		 from->key_data_length[i]);
+       if ( from->key_data_length[i] ) {
+	 to->key_data_contents[i] = malloc(from->key_data_length[i]);
+	 if (to->key_data_contents[i] == NULL) {
+	   for (i = 0; i < idx; i++) {
+	     if (to->key_data_contents[i]) {
+	       memset(to->key_data_contents[i], 0,
+		      to->key_data_length[i]);
+	       free(to->key_data_contents[i]);
+	     }
+	   }
+	   return ENOMEM;
+	 }
+	 memcpy(to->key_data_contents[i], from->key_data_contents[i],
+		from->key_data_length[i]);
+       }
      }
      return 0;
 }
