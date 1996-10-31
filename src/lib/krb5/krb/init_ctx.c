262aba61c0065a3a96b80ea2e3a41a657e74d4f7
Theodore Tso <tytso@mit.edu>
1996-10-31 18:57:20 +0000

    Make it more obvious that default ticket lifetimes is not yet supported.
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@9267 dc483132-0cff-0310-8789-dd5450dbe970

--- src/lib/krb5/krb/init_ctx.c
+++ src/lib/krb5/krb/init_ctx.c
@@ -69,10 +69,12 @@ krb5_init_context(context)
 			    0, 5 * 60, &tmp);
 	ctx->clockskew = tmp;
 
-	/* Default ticket lifetime is 
+#if 0
+	/* Default ticket lifetime is currently not supported */
 	profile_get_integer(ctx->profile, "libdefaults", "tkt_lifetime",
 			    0, 10 * 60 * 60, &tmp);
-	ctx->clockskew = tmp;
+	ctx->tkt_lifetime = tmp;
+#endif
 
 	/* DCE 1.1 and below only support CKSUMTYPE_RSA_MD4 (2)  */
 	/* DCE add kdc_req_checksum_type = 2 to krb5.conf */
