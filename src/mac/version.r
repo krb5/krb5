acbfa341cec15cbf41dff5ab54a901176b4984fb
Theodore Tso <tytso@mit.edu>
1996-11-08 22:44:52 +0000

    Makefile.tmpl: Add in version resource
    version.r: New file, containing the version resource.
    
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@9348 dc483132-0cff-0310-8789-dd5450dbe970

---
+++ src/mac/version.r
@@ -0,0 +1,9 @@
+#include <SysTypes.r>
+#include <Types.r>
+
+resource 'vers' (1) {
+	0x00, 0x07, beta, 0x01,
+	verUS
+	"Beta 7 Build 1",
+	"Beta 7 Build 1, Copyright 1996 Massachusetts Institute of Technology"
+};
