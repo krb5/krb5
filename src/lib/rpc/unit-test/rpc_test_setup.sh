c1ea1d244ffd9bbba553d664abe80879a57a5c44
Tom Yu <tlyu@mit.edu>
1996-11-12 02:05:38 +0000

    * rpc_test_setup.sh: Use a temp file rather than a here document 	feeding into eval because Ultrix sh doesn't deal.
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@9379 dc483132-0cff-0310-8789-dd5450dbe970

--- src/lib/rpc/unit-test/rpc_test_setup.sh
+++ src/lib/rpc/unit-test/rpc_test_setup.sh
@@ -13,9 +13,9 @@ DUMMY=${TCLUTIL=$TESTDIR/tcl/util.t}; export TCLUTIL
 DUMMY=${MAKE_KEYTAB=$TESTDIR/scripts/make-host-keytab.pl}
 
 # If it's set, set it to true
-VERBOSE=${VERBOSE_TEST:+true}
+VERBOSE=${VERBOSE_TEST+true}
 # Otherwise, set it to false
-DUMMY=${VERBOSE:=false}
+DUMMY=${VERBOSE=false}
 
 if $VERBOSE; then
 	REDIRECT=
@@ -28,7 +28,7 @@ PATH=$TOP/install/admin:$PATH; export PATH
 CANON_HOST=`$QUALNAME`
 export CANON_HOST
 
-eval $CLNTTCL <<'EOF' $REDIRECT
+cat - > /tmp/rpc_test_setup$$ <<\EOF
 source $env(TCLUTIL)
 set h $env(CANON_HOST)
 puts stdout [ovsec_kadm_init admin admin $OVSEC_KADM_ADMIN_SERVICE null $OVSEC_KADM_STRUCT_VERSION $OVSEC_KADM_API_VERSION_1 server_handle]
@@ -38,6 +38,8 @@ puts stdout [ovsec_kadm_create_principal $server_handle [simple_principal notser
 puts stdout [ovsec_kadm_randkey_principal $server_handle notserver/$h key]
 puts stdout [ovsec_kadm_destroy $server_handle]
 EOF
+eval "$CLNTTCL $REDIRECT < /tmp/rpc_test_setup$$"
+rm /tmp/rpc_test_setup$$
 
 rm -f $RPC_TEST_SRVTAB
 
