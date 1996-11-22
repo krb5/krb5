62031599ff39a49510e00b0407ad280c5e0ed455
Barry Jaspan <bjaspan@mit.edu>
1996-11-22 21:08:58 +0000

    use sizeof instead of h_length to determine  	number of bytes of addr to copy from DNS response [krb5-misc/211]
    git-svn-id: svn://anonsvn.mit.edu/krb5/branches/V1_0_BRANCH@9523 dc483132-0cff-0310-8789-dd5450dbe970

--- src/appl/telnet/telnet/commands.c
+++ src/appl/telnet/telnet/commands.c
@@ -2454,9 +2454,10 @@ tn(argc, argv)
 		sin.sin_family = host->h_addrtype;
 #if	defined(h_addr)		/* In 4.3, this is a #define */
 		memcpy((caddr_t)&sin.sin_addr,
-				host->h_addr_list[0], host->h_length);
+		       host->h_addr_list[0], sizeof(sin.sin_addr));
 #else	/* defined(h_addr) */
-		memcpy((caddr_t)&sin.sin_addr, host->h_addr, host->h_length);
+		memcpy((caddr_t)&sin.sin_addr, host->h_addr, 
+		       sizeof(sin.sin_addr)); 
 #endif	/* defined(h_addr) */
 		strncpy(_hostname, host->h_name, sizeof(_hostname));
 		_hostname[sizeof(_hostname)-1] = '\0';
@@ -2546,9 +2547,9 @@ tn(argc, argv)
 		perror((char *)0);
 		host->h_addr_list++;
 		memcpy((caddr_t)&sin.sin_addr, 
-			host->h_addr_list[0], host->h_length);
+			host->h_addr_list[0], sizeof(sin.sin_addr));
 		memcpy((caddr_t)&hostaddr,
-			host->h_addr_list[0], host->h_length);
+		       host->h_addr_list[0], sizeof(sin.sin_addr));
 		(void) NetClose(net);
 		continue;
 	    }
@@ -3055,9 +3056,10 @@ sourceroute(arg, cpp, lenp)
 		} else if (host = gethostbyname(cp)) {
 #if	defined(h_addr)
 			memcpy((caddr_t)&sin_addr,
-				host->h_addr_list[0], host->h_length);
+				host->h_addr_list[0], sizeof(sin_addr));
 #else
-			memcpy((caddr_t)&sin_addr, host->h_addr, host->h_length);
+			memcpy((caddr_t)&sin_addr, host->h_addr, 
+			       sizeof(sin_addr));
 #endif
 		} else {
 			*cpp = cp;
