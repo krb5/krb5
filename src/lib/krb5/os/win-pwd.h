1a5411452348001e4992f6fd13536e92928e3641
Richard Basch <probe@mit.edu>
1997-02-25 06:16:30 +0000

    win-pwd.rc: Windows dialog information for password prompting win-pwd.h: supporting definitions for Windows dialog read_pwd.c: Support Windows dialog & console-mode input for krb5_read_password Makefile.in: Link in win-pwd.obj (win32)
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@9971 dc483132-0cff-0310-8789-dd5450dbe970

---
+++ src/lib/krb5/os/win-pwd.h
@@ -0,0 +1,28 @@
+/*
+ * lib/krb5/os/win-pwd.h
+ *
+ * Copyright 1997 by the Massachusetts Institute of Technology.
+ * All Rights Reserved.
+ *
+ * Export of this software from the United States of America may
+ *   require a specific license from the United States Government.
+ *   It is the responsibility of any person or organization contemplating
+ *   export to obtain such a license before exporting.
+ * 
+ * WITHIN THAT CONSTRAINT, permission to use, copy, modify, and
+ * distribute this software and its documentation for any purpose and
+ * without fee is hereby granted, provided that the above copyright
+ * notice appear in all copies and that both that copyright notice and
+ * this permission notice appear in supporting documentation, and that
+ * the name of M.I.T. not be used in advertising or publicity pertaining
+ * to distribution of the software without specific, written prior
+ * permission.  M.I.T. makes no representations about the suitability of
+ * this software for any purpose.  It is provided "as is" without express
+ * or implied warranty.
+ *
+ */
+
+#define ID_READ_PWD_DIALOG  10000
+#define ID_READ_PWD_PROMPT  10001
+#define ID_READ_PWD_PROMPT2 10002
+#define ID_READ_PWD_PWD     10003
