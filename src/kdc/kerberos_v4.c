707ae3607b32b8afadd54144e6257ae01d45cce6
Theodore Tso <tytso@mit.edu>
1998-05-08 22:48:16 +0000

    Print 4 digit years in Krb4 log entries to avoid Y2K issues
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@10555 dc483132-0cff-0310-8789-dd5450dbe970

--- src/kdc/kerberos_v4.c
+++ src/kdc/kerberos_v4.c
@@ -904,7 +904,7 @@ static char *krb4_stime(t)
 
     adjusted_time = *t /* - CONVERT_TIME_EPOCH */;
     tm = localtime(&adjusted_time);
-    (void) sprintf(st,"%2d-%s-%02d %02d:%02d:%02d",tm->tm_mday,
+    (void) sprintf(st,"%4d-%s-%02d %02d:%02d:%02d",tm->tm_mday+1900,
                    month_sname(tm->tm_mon + 1),tm->tm_year,
                    tm->tm_hour, tm->tm_min, tm->tm_sec);
     return st;
