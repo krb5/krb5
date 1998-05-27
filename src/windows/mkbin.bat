4af09b98586163bd9d5ea62a1ffe7621fee0e908
Theodore Tso <tytso@mit.edu>
1998-05-27 19:37:14 +0000

    mkbin.bat: Added convenience batch file which copies all of the 	appropriate files out of the build tree into a single kbin directory
    Makefile.in: Add the windows/lib directory to the list of directories
    	to be built (and cleaned).
    
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@10595 dc483132-0cff-0310-8789-dd5450dbe970

---
+++ src/windows/mkbin.bat
@@ -0,0 +1,8 @@
+mkdir kbin
+copy lib\*.dll kbin
+copy windows\cns\krb5.exe kbin
+copy windows\wintel\telnet.exe kbin
+copy windows\gss\gss.exe kbin
+copy clients\klist\klist.exe kbin
+copy clients\kinit\kinit.exe kbin 
+copy clients\kdestroy\kdestroy.exe kbin
