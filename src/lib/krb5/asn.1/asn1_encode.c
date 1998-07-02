67a9928b569e3e13b5151d829b5af71dd2a538cc
Theodore Tso <tytso@mit.edu>
1998-07-02 22:36:50 +0000

    asn1_encode.c: Make the magic Macintosh EPOCH offset be 70 years 	instead of 66 years, since CodeWarrior Pro 2 now bases everything off 	of 1900.
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@10619 dc483132-0cff-0310-8789-dd5450dbe970

--- src/lib/krb5/asn.1/asn1_encode.c
+++ src/lib/krb5/asn.1/asn1_encode.c
@@ -184,7 +184,7 @@ asn1_error_code asn1_encode_ia5string(buf, len, val, retlen)
 }
 
 #ifdef _MACINTOSH
-#define EPOCH ((66 * 365 * 24 * 60 * 60) + (17 *  24 * 60 * 60) + (getTimeZoneOffset() * 60 * 60))
+#define EPOCH ((70 * 365 * 24 * 60 * 60) + (17 *  24 * 60 * 60) + (getTimeZoneOffset() * 60 * 60))
 #else
 #define EPOCH (0)
 #endif
