388fb30ee9e9ba6d069fe9565baf348321e92512
Miro Jurisic <meeroh@mit.edu>
1998-07-14 15:18:46 +0000

    Added new Mac files
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@10636 dc483132-0cff-0310-8789-dd5450dbe970

---
+++ src/mac/K5.CFM.c
@@ -0,0 +1,59 @@
+/*
+ *   Copyright (C) 1997 by the Massachusetts Institute of Technology
+ *   All rights reserved.
+ *
+ *   For copying and distribution information, please see the file
+ *   COPYRIGHT.
+ */
+ 
+ 
+#include <CodeFragments.h>
+
+#include "ShlibTestTrack.h"
+ 
+#include "krb5_err.h"
+#include "kv5m_err.h"
+#include "asn1_err.h"
+#include "kdb5_err.h"
+#include "profile.h"
+#include "adm_err.h"
+
+
+OSErr __initializeK5(CFragInitBlockPtr ibp);
+void __terminateGSSK5glue(void);
+
+OSErr __initializeK5(CFragInitBlockPtr ibp)
+{
+	OSErr	err = noErr;
+	
+	/* Do normal init of the shared library */
+	err = __initialize();
+	
+	/* Initialize the error tables */
+	if (err == noErr) {
+	    add_error_table(&et_krb5_error_table);
+	    add_error_table(&et_kv5m_error_table);
+	    add_error_table(&et_kdb5_error_table);
+	    add_error_table(&et_asn1_error_table);
+	    add_error_table(&et_prof_error_table);
+	    add_error_table(&et_kadm_error_table);
+	    
+	    /* TestTrack the shared library */
+	    err = ShlibTestTrack(ibp);
+	}
+	
+	return err;
+}
+
+void __terminateK5(void)
+{
+	
+    remove_error_table(&et_krb5_error_table);
+    remove_error_table(&et_kv5m_error_table);
+    remove_error_table(&et_kdb5_error_table);
+    remove_error_table(&et_asn1_error_table);
+    remove_error_table(&et_prof_error_table);
+    remove_error_table(&et_kadm_error_table);
+
+	__terminate();
+}
