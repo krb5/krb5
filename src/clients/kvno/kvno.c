6b0d72f66ecdb59970bb49026e718b6dcfe0079f
Marc Horowitz <marc@mit.edu>
1998-07-24 22:45:23 +0000

    add a new app to acquire a ticket for a service and print out the kvno
    git-svn-id: svn://anonsvn.mit.edu/krb5/branches/marc-3des@10735 dc483132-0cff-0310-8789-dd5450dbe970

---
+++ src/clients/kvno/kvno.c
@@ -0,0 +1,141 @@
+#include <stdio.h>
+#include <string.h>
+#include <krb5.h>
+
+extern int optind;
+extern char *optarg;
+
+void usage(char *argv0)
+{
+    char *cmd;
+
+    cmd = strrchr(argv0, '/');
+    cmd = cmd?(cmd+1):argv0;
+
+    fprintf(stderr, "usage: %s [-e etype] service1 service2 ...\n", cmd);
+
+    exit(1);
+}
+
+int main(int argc, char *argv[])
+{
+    krb5_context context;
+    krb5_error_code ret;
+    int option, i, errors;
+    char *etypestr = 0;
+    int quiet = 0;
+    krb5_enctype etype;
+    krb5_ccache ccache;
+    krb5_principal me;
+    krb5_creds in_creds, *out_creds;
+    krb5_ticket *ticket;
+    char *princ;
+
+    if (ret = krb5_init_context(&context)) {
+	com_err(argv[0], ret, "while initializing krb5 library");
+	exit(1);
+    }
+
+    while ((option = getopt(argc, argv, "e:hq")) != -1) {
+	switch (option) {
+	case 'e':
+	    etypestr = optarg;
+	    break;
+	case 'h':
+	    usage(argv[0]);
+	    break;
+	case 'q':
+	    quiet = 1;
+	    break;
+	default:
+	    usage(argv[0]);
+	    break;
+	}
+    }
+
+    if ((argc - optind) < 1)
+	usage(argv[0]);
+
+    if (etypestr) {
+	if (ret = krb5_string_to_enctype(etypestr, &etype)) {
+	    com_err(argv[0], ret, "while converting etype");
+	    exit(1);
+	}
+    } else {
+	etype = 0;
+    }
+
+    if (ret = krb5_cc_default(context, &ccache)) {
+	com_err(argv[0], ret, "while opening ccache");
+	exit(1);
+    }
+
+    if (ret = krb5_cc_get_principal(context, ccache, &me)) {
+	com_err(argv[0], ret, "while getting client principal name");
+	exit(1);
+    }
+
+    errors = 0;
+
+    for (i = optind; i < argc; i++) {
+	memset(&in_creds, 0, sizeof(in_creds));
+
+	in_creds.client = me;
+
+	if (ret = krb5_parse_name(context, argv[i], &in_creds.server)) {
+	    if (!quiet)
+		fprintf(stderr, "%s: %s while parsing principal name\n",
+			argv[i], error_message(ret));
+	    errors++;
+	    continue;
+	}
+
+	if (ret = krb5_unparse_name(context, in_creds.server, &princ)) {
+	    fprintf(stderr, "%s: %s while printing principal name\n",
+		    argv[i], error_message(ret));
+	    errors++;
+	    continue;
+	}
+
+	in_creds.keyblock.enctype = etype;
+
+	ret = krb5_get_credentials(context, 0, ccache, &in_creds, &out_creds);
+
+	krb5_free_principal(context, in_creds.server);
+
+	if (ret) {
+	    free(princ);
+
+	    fprintf(stderr, "%s: %s while getting credentials\n",
+		    princ, error_message(ret));
+	    errors++;
+	    continue;
+	}
+
+	/* we need a native ticket */
+	if (ret = decode_krb5_ticket(&out_creds->ticket, &ticket)) {
+	    krb5_free_creds(context, out_creds);
+	    free(princ);
+
+	    fprintf(stderr, "princ: %s while decoding ticket\n",
+		    argv[i], error_message(ret));
+	    errors++;
+	    continue;
+	}
+	    
+	if (!quiet)
+	    printf("%s: kvno = %d\n", princ, ticket->enc_part.kvno);
+
+	krb5_free_ticket(context, ticket);
+	krb5_free_creds(context, out_creds);
+	free(princ);
+    }
+
+    krb5_free_principal(context, me);
+    krb5_cc_close(context, ccache);
+
+    if (errors)
+	exit(1);
+
+    exit(0);
+}
