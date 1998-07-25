d7eefdd4bf9d1efc7bf0c2171bac5dd264463eef
Marc Horowitz <marc@mit.edu>
1998-07-25 22:46:24 +0000

    new file
    git-svn-id: svn://anonsvn.mit.edu/krb5/branches/marc-3des@10742 dc483132-0cff-0310-8789-dd5450dbe970

---
+++ src/lib/crypto/keyed_checksum_types.c
@@ -0,0 +1,65 @@
+#include "k5-int.h"
+#include "etypes.h"
+#include "cksumtypes.h"
+
+static int etype_match(e1, e2)
+     krb5_enctype e1, e2;
+{
+    int i1, i2;
+
+    for (i1=0; i1<krb5_enctypes_length; i1++) 
+	if (krb5_enctypes_list[i1].etype == e1)
+	    break;
+
+    for (i2=0; i2<krb5_enctypes_length; i2++) 
+	if (krb5_enctypes_list[i2].etype == e2)
+	    break;
+
+    return((i1 < krb5_enctypes_length) &&
+	   (i2 < krb5_enctypes_length) ||
+	   (krb5_enctypes_list[i1].enc == krb5_enctypes_list[i2].enc));
+}
+
+krb5_error_code
+krb5_c_num_keyed_checksum_types(context, enctype, count)
+     krb5_context context;
+     krb5_enctype enctype;
+     unsigned int *count;
+{
+    int i;
+    unsigned int c;
+
+    c = 0;
+    for (i=0; i<krb5_cksumtypes_length; i++)
+	if ((krb5_cksumtypes_list[i].keyhash &&
+	     etype_match(krb5_cksumtypes_list[i].keyed_etype, enctype)) ||
+	    (krb5_cksumtypes_list[i].flags & KRB5_CKSUMFLAG_DERIVE))
+	    c++;
+
+    *count = c;
+
+    return(0);
+}
+
+krb5_error_code
+krb5_c_keyed_checksum_types(context, enctype, count, cksumtypes)
+     krb5_context context;
+     krb5_enctype enctype;
+     unsigned int count;
+     krb5_cksumtype *cksumtypes;
+{
+    int i;
+    unsigned int c;
+
+    c = 0;
+    for (i=0; i<krb5_cksumtypes_length && c < count; i++) {
+	if ((krb5_cksumtypes_list[i].keyhash &&
+	     etype_match(krb5_cksumtypes_list[i].keyed_etype, enctype)) ||
+	    (krb5_cksumtypes_list[i].flags & KRB5_CKSUMFLAG_DERIVE)) {
+	    cksumtypes[c] = krb5_cksumtypes_list[i].ctype;
+	    c++;
+	}
+    }
+
+    return(0);
+}
