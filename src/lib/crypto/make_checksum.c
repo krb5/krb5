a7f27ccfd598ff64b884cc496ca0b87d0454b595
Marc Horowitz <marc@mit.edu>
1998-07-25 22:52:34 +0000

    for keyed hashes, verify that the key enctype is compatible
    git-svn-id: svn://anonsvn.mit.edu/krb5/branches/marc-3des@10745 dc483132-0cff-0310-8789-dd5450dbe970

--- src/lib/crypto/make_checksum.c
+++ src/lib/crypto/make_checksum.c
@@ -1,5 +1,6 @@
 #include "k5-int.h"
 #include "cksumtypes.h"
+#include "etypes.h"
 
 krb5_error_code
 krb5_c_make_checksum(context, cksumtype, key, usage, input, cksum)
@@ -10,7 +11,7 @@ krb5_c_make_checksum(context, cksumtype, key, usage, input, cksum)
      krb5_const krb5_data *input;
      krb5_checksum *cksum;
 {
-    int i;
+    int i, e1, e2;
     krb5_data data;
     krb5_error_code ret;
     size_t cksumlen;
@@ -34,20 +35,47 @@ krb5_c_make_checksum(context, cksumtype, key, usage, input, cksum)
     data.data = cksum->contents;
 
     if (krb5_cksumtypes_list[i].keyhash) {
+	/* check if key is compatible */
+
+	if (krb5_cksumtypes_list[i].keyed_etype) {
+	    for (e1=0; e1<krb5_enctypes_length; e1++) 
+		if (krb5_enctypes_list[e1].etype ==
+		    krb5_cksumtypes_list[i].keyed_etype)
+		    break;
+
+	    for (e2=0; e2<krb5_enctypes_length; e2++) 
+		if (krb5_enctypes_list[e2].etype == key->enctype)
+		    break;
+
+	    if ((e1 == krb5_enctypes_length) ||
+		(e2 == krb5_enctypes_length) ||
+		(krb5_enctypes_list[e1].enc != krb5_enctypes_list[e2].enc)) {
+		ret = KRB5_BAD_ENCTYPE;
+		goto cleanup;
+	    }
+	}
+
 	ret = (*(krb5_cksumtypes_list[i].keyhash->hash))(key, 0, input, &data);
     } else if (krb5_cksumtypes_list[i].flags & KRB5_CKSUMFLAG_DERIVE) {
+	/* any key is ok */
+
 	ret = krb5_dk_make_checksum(krb5_cksumtypes_list[i].hash,
 				    key, usage, input, &data);
     } else {
+	/* no key is used */
+
 	ret = (*(krb5_cksumtypes_list[i].hash->hash))(1, input, &data);
     }
 
+    if (!ret) {
+	cksum->magic = KV5M_CHECKSUM;
+	cksum->checksum_type = cksumtype;
+    }
+
+cleanup:
     if (ret) {
 	memset(cksum->contents, 0, cksum->length);
 	free(cksum->contents);
-    } else {
-	cksum->magic = KV5M_CHECKSUM;
-	cksum->checksum_type = cksumtype;
     }
 
     return(ret);
