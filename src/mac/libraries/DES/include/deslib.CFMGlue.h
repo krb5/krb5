c4c87485e71f66fc0c89bed9d1e06d413ab4e52c
Miro Jurisic <meeroh@mit.edu>
1998-10-23 05:05:22 +0000

    Added Mac DES shared library
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@10987 dc483132-0cff-0310-8789-dd5450dbe970

---
+++ src/mac/libraries/DES/include/deslib.CFMGlue.h
@@ -0,0 +1,8 @@
+#ifndef __DESLIB_CFMGLUE__
+#define __DESLIB_CFMGLUE__
+
+/* Prototype for checking if the library is there */
+
+Boolean DESLibraryIsPresent(void);
+
+#endif /* __DESLIB_CFMGLUE__ */
