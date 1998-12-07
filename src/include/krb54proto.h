4223340a38a70b486b07f074c0963c5e8ac1f44a
Tom Yu <tlyu@mit.edu>
1998-12-07 00:46:32 +0000

    * krb54proto.h: Fix decomp_tkt_krb5 to use KRB4_32, instead of a 	nonexistent type.
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@11089 dc483132-0cff-0310-8789-dd5450dbe970

--- src/include/krb54proto.h
+++ src/include/krb54proto.h
@@ -8,8 +8,8 @@ extern krb5_error_code krb54_get_service_keyblock
 		   krb5_keyblock FAR * keyblock));
 extern int decomp_tkt_krb5
 	PROTOTYPE((KTEXT tkt, unsigned char *flags, char *pname,
-		   char *pinstance, char *prealm, KRB5_K4_U32 *paddress,
-		   des_cblock session, int *life, KRB5_K4_U32 *time_sec, 
+		   char *pinstance, char *prealm, unsigned KRB4_32 *paddress,
+		   des_cblock session, int *life, unsigned KRB4_32 *time_sec, 
 		   char *sname, char *sinstance, krb5_keyblock *k5key));
 extern int krb_set_key_krb5
 	PROTOTYPE ((krb5_context ctx, krb5_keyblock *key));
