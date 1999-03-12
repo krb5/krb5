63ecc293b3e494c18b36284fad086153a0807dde
Theodore Tso <tytso@mit.edu>
1999-03-12 00:00:22 +0000

    Makefile.in (DLL_FILE_DEF): Tell the Makefile template that we are 	building object files for the GSSAPI DLL.
    krb5_gss_glue.c: Change use of KRB5_DLLIMP to be GSS_DLLIMP.
    
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@11261 dc483132-0cff-0310-8789-dd5450dbe970

--- src/lib/gssapi/krb5/krb5_gss_glue.c
+++ src/lib/gssapi/krb5/krb5_gss_glue.c
@@ -26,7 +26,7 @@
 
 #include "gssapiP_krb5.h"
 
-KRB5_DLLIMP OM_uint32 KRB5_CALLCONV
+GSS_DLLIMP OM_uint32 KRB5_CALLCONV
 gss_accept_sec_context(minor_status, context_handle, verifier_cred_handle,
 		       input_token, input_chan_bindings, src_name, mech_type, 
 		       output_token, ret_flags, time_rec, delegated_cred_handle)
@@ -55,7 +55,7 @@ gss_accept_sec_context(minor_status, context_handle, verifier_cred_handle,
 				      delegated_cred_handle));
 }
 
-KRB5_DLLIMP OM_uint32 KRB5_CALLCONV
+GSS_DLLIMP OM_uint32 KRB5_CALLCONV
 gss_acquire_cred(minor_status, desired_name, time_req, desired_mechs,
 		 cred_usage, output_cred_handle, actual_mechs, time_rec)
      OM_uint32 FAR *minor_status;
@@ -78,7 +78,7 @@ gss_acquire_cred(minor_status, desired_name, time_req, desired_mechs,
 }
 
 /* V2 */
-KRB5_DLLIMP OM_uint32 KRB5_CALLCONV
+GSS_DLLIMP OM_uint32 KRB5_CALLCONV
 gss_add_cred(minor_status, input_cred_handle, desired_name, desired_mech,
 	     cred_usage, initiator_time_req, acceptor_time_req,
 	     output_cred_handle, actual_mechs, initiator_time_rec,
@@ -103,7 +103,7 @@ gss_add_cred(minor_status, input_cred_handle, desired_name, desired_mech,
 }
 
 /* V2 */
-KRB5_DLLIMP OM_uint32 KRB5_CALLCONV
+GSS_DLLIMP OM_uint32 KRB5_CALLCONV
 gss_add_oid_set_member(minor_status, member_oid, oid_set)
     OM_uint32	FAR *minor_status;
     gss_OID	member_oid;
@@ -112,7 +112,7 @@ gss_add_oid_set_member(minor_status, member_oid, oid_set)
     return(generic_gss_add_oid_set_member(minor_status, member_oid, oid_set));
 }
 
-KRB5_DLLIMP OM_uint32 KRB5_CALLCONV
+GSS_DLLIMP OM_uint32 KRB5_CALLCONV
 gss_compare_name(minor_status, name1, name2, name_equal)
      OM_uint32 FAR *minor_status;
      gss_name_t name1;
@@ -123,7 +123,7 @@ gss_compare_name(minor_status, name1, name2, name_equal)
 				name2, name_equal));
 }
 
-KRB5_DLLIMP OM_uint32 KRB5_CALLCONV
+GSS_DLLIMP OM_uint32 KRB5_CALLCONV
 gss_context_time(minor_status, context_handle, time_rec)
      OM_uint32 FAR *minor_status;
      gss_ctx_id_t context_handle;
@@ -134,7 +134,7 @@ gss_context_time(minor_status, context_handle, time_rec)
 }
 
 /* V2 */
-KRB5_DLLIMP OM_uint32 KRB5_CALLCONV
+GSS_DLLIMP OM_uint32 KRB5_CALLCONV
 gss_create_empty_oid_set(minor_status, oid_set)
     OM_uint32	FAR *minor_status;
     gss_OID_set	FAR *oid_set;
@@ -142,7 +142,7 @@ gss_create_empty_oid_set(minor_status, oid_set)
     return(generic_gss_create_empty_oid_set(minor_status, oid_set));
 }
 
-KRB5_DLLIMP OM_uint32 KRB5_CALLCONV
+GSS_DLLIMP OM_uint32 KRB5_CALLCONV
 gss_delete_sec_context(minor_status, context_handle, output_token)
      OM_uint32 FAR *minor_status;
      gss_ctx_id_t FAR *context_handle;
@@ -152,7 +152,7 @@ gss_delete_sec_context(minor_status, context_handle, output_token)
 				      context_handle, output_token));
 }
 
-KRB5_DLLIMP OM_uint32 KRB5_CALLCONV
+GSS_DLLIMP OM_uint32 KRB5_CALLCONV
 gss_display_name(minor_status, input_name, output_name_buffer, output_name_type)
      OM_uint32 FAR *minor_status;
      gss_name_t input_name;
@@ -163,7 +163,7 @@ gss_display_name(minor_status, input_name, output_name_buffer, output_name_type)
 				output_name_buffer, output_name_type));
 }
 
-KRB5_DLLIMP OM_uint32 KRB5_CALLCONV
+GSS_DLLIMP OM_uint32 KRB5_CALLCONV
 gss_display_status(minor_status, status_value, status_type,
 		   mech_type, message_context, status_string)
      OM_uint32 FAR *minor_status;
@@ -179,7 +179,7 @@ gss_display_status(minor_status, status_value, status_type,
 }
 
 /* V2 */
-KRB5_DLLIMP OM_uint32 KRB5_CALLCONV
+GSS_DLLIMP OM_uint32 KRB5_CALLCONV
 gss_export_sec_context(minor_status, context_handle, interprocess_token)
      OM_uint32		FAR *minor_status;
      gss_ctx_id_t	FAR *context_handle;
@@ -191,7 +191,7 @@ gss_export_sec_context(minor_status, context_handle, interprocess_token)
 }
 
 /* V2 */
-KRB5_DLLIMP OM_uint32 KRB5_CALLCONV
+GSS_DLLIMP OM_uint32 KRB5_CALLCONV
 gss_get_mic(minor_status, context_handle, qop_req,
 	    message_buffer, message_token)
      OM_uint32		FAR *minor_status;
@@ -204,7 +204,7 @@ gss_get_mic(minor_status, context_handle, qop_req,
 			    qop_req, message_buffer, message_token));
 }
 
-KRB5_DLLIMP OM_uint32 KRB5_CALLCONV
+GSS_DLLIMP OM_uint32 KRB5_CALLCONV
 gss_import_name(minor_status, input_name_buffer, input_name_type, output_name)
      OM_uint32 FAR *minor_status;
      gss_buffer_t input_name_buffer;
@@ -216,7 +216,7 @@ gss_import_name(minor_status, input_name_buffer, input_name_type, output_name)
 }
 
 /* V2 */
-KRB5_DLLIMP OM_uint32 KRB5_CALLCONV
+GSS_DLLIMP OM_uint32 KRB5_CALLCONV
 gss_import_sec_context(minor_status, interprocess_token, context_handle)
      OM_uint32		FAR *minor_status;
      gss_buffer_t	interprocess_token;
@@ -227,7 +227,7 @@ gss_import_sec_context(minor_status, interprocess_token, context_handle)
 				      context_handle));
 }
 
-KRB5_DLLIMP OM_uint32 KRB5_CALLCONV
+GSS_DLLIMP OM_uint32 KRB5_CALLCONV
 gss_indicate_mechs(minor_status, mech_set)
      OM_uint32 FAR *minor_status;
      gss_OID_set FAR *mech_set;
@@ -235,7 +235,7 @@ gss_indicate_mechs(minor_status, mech_set)
    return(krb5_gss_indicate_mechs(minor_status, mech_set));
 }
 
-KRB5_DLLIMP OM_uint32 KRB5_CALLCONV
+GSS_DLLIMP OM_uint32 KRB5_CALLCONV
 gss_init_sec_context(minor_status, claimant_cred_handle, context_handle,
 		     target_name, mech_type, req_flags, time_req,
 		     input_chan_bindings, input_token, actual_mech_type,
@@ -262,7 +262,7 @@ gss_init_sec_context(minor_status, claimant_cred_handle, context_handle,
 				    time_rec));
 }
 
-KRB5_DLLIMP OM_uint32 KRB5_CALLCONV
+GSS_DLLIMP OM_uint32 KRB5_CALLCONV
 gss_inquire_context(minor_status, context_handle, initiator_name, acceptor_name,
 		    lifetime_rec, mech_type, ret_flags,
 		    locally_initiated, open)
@@ -282,7 +282,7 @@ gss_inquire_context(minor_status, context_handle, initiator_name, acceptor_name,
 				   open));
 }
 
-KRB5_DLLIMP OM_uint32 KRB5_CALLCONV
+GSS_DLLIMP OM_uint32 KRB5_CALLCONV
 gss_inquire_cred(minor_status, cred_handle, name, lifetime_ret,
 		 cred_usage, mechanisms)
      OM_uint32 FAR *minor_status;
@@ -297,7 +297,7 @@ gss_inquire_cred(minor_status, cred_handle, name, lifetime_ret,
 }
 
 /* V2 */
-KRB5_DLLIMP OM_uint32 KRB5_CALLCONV
+GSS_DLLIMP OM_uint32 KRB5_CALLCONV
 gss_inquire_cred_by_mech(minor_status, cred_handle, mech_type, name,
 			 initiator_lifetime, acceptor_lifetime, cred_usage)
      OM_uint32		FAR *minor_status;
@@ -314,7 +314,7 @@ gss_inquire_cred_by_mech(minor_status, cred_handle, mech_type, name,
 }
 
 /* V2 */
-KRB5_DLLIMP OM_uint32 KRB5_CALLCONV
+GSS_DLLIMP OM_uint32 KRB5_CALLCONV
 gss_inquire_names_for_mech(minor_status, mechanism, name_types)
     OM_uint32	FAR *minor_status;
     gss_OID	mechanism;
@@ -326,7 +326,7 @@ gss_inquire_names_for_mech(minor_status, mechanism, name_types)
 }
 
 /* V2 */
-KRB5_DLLIMP OM_uint32 KRB5_CALLCONV
+GSS_DLLIMP OM_uint32 KRB5_CALLCONV
 gss_oid_to_str(minor_status, oid, oid_str)
     OM_uint32		FAR *minor_status;
     gss_OID		oid;
@@ -335,7 +335,7 @@ gss_oid_to_str(minor_status, oid, oid_str)
     return(generic_gss_oid_to_str(minor_status, oid, oid_str));
 }
 
-KRB5_DLLIMP OM_uint32 KRB5_CALLCONV
+GSS_DLLIMP OM_uint32 KRB5_CALLCONV
 gss_process_context_token(minor_status, context_handle, token_buffer)
      OM_uint32 FAR *minor_status;
      gss_ctx_id_t context_handle;
@@ -345,7 +345,7 @@ gss_process_context_token(minor_status, context_handle, token_buffer)
 					 context_handle, token_buffer));
 }
 
-KRB5_DLLIMP OM_uint32 KRB5_CALLCONV
+GSS_DLLIMP OM_uint32 KRB5_CALLCONV
 gss_release_cred(minor_status, cred_handle)
      OM_uint32 FAR *minor_status;
      gss_cred_id_t FAR *cred_handle;
@@ -353,7 +353,7 @@ gss_release_cred(minor_status, cred_handle)
    return(krb5_gss_release_cred(minor_status, cred_handle));
 }
 
-KRB5_DLLIMP OM_uint32 KRB5_CALLCONV
+GSS_DLLIMP OM_uint32 KRB5_CALLCONV
 gss_release_name(minor_status, input_name)
      OM_uint32 FAR *minor_status;
      gss_name_t FAR *input_name;
@@ -361,7 +361,7 @@ gss_release_name(minor_status, input_name)
    return(krb5_gss_release_name(minor_status, input_name));
 }
 
-KRB5_DLLIMP OM_uint32 KRB5_CALLCONV
+GSS_DLLIMP OM_uint32 KRB5_CALLCONV
 gss_release_buffer(minor_status, buffer)
      OM_uint32 FAR *minor_status;
      gss_buffer_t buffer;
@@ -371,7 +371,7 @@ gss_release_buffer(minor_status, buffer)
 }
 
 /* V2 */
-KRB5_DLLIMP OM_uint32 KRB5_CALLCONV
+GSS_DLLIMP OM_uint32 KRB5_CALLCONV
 gss_release_oid(minor_status, oid)
      OM_uint32	FAR *minor_status;
      gss_OID	FAR *oid;
@@ -379,7 +379,7 @@ gss_release_oid(minor_status, oid)
     return(krb5_gss_release_oid(minor_status, oid));
 }
 
-KRB5_DLLIMP OM_uint32 KRB5_CALLCONV
+GSS_DLLIMP OM_uint32 KRB5_CALLCONV
 gss_release_oid_set(minor_status, set)
      OM_uint32 FAR * minor_status;
      gss_OID_set FAR *set;
@@ -388,7 +388,7 @@ gss_release_oid_set(minor_status, set)
 }
 
 /* V1 only */
-KRB5_DLLIMP OM_uint32 KRB5_CALLCONV
+GSS_DLLIMP OM_uint32 KRB5_CALLCONV
 gss_seal(minor_status, context_handle, conf_req_flag, qop_req,
 	 input_message_buffer, conf_state, output_message_buffer)
      OM_uint32 FAR *minor_status;
@@ -404,7 +404,7 @@ gss_seal(minor_status, context_handle, conf_req_flag, qop_req,
 			conf_state, output_message_buffer));
 }
 
-KRB5_DLLIMP OM_uint32 KRB5_CALLCONV
+GSS_DLLIMP OM_uint32 KRB5_CALLCONV
 gss_sign(minor_status, context_handle,
 	      qop_req, message_buffer, 
 	      message_token)
@@ -419,7 +419,7 @@ gss_sign(minor_status, context_handle,
 }
 
 /* V2 */
-KRB5_DLLIMP OM_uint32 KRB5_CALLCONV
+GSS_DLLIMP OM_uint32 KRB5_CALLCONV
 gss_verify_mic(minor_status, context_handle,
 	       message_buffer, token_buffer, qop_state)
      OM_uint32		FAR *minor_status;
@@ -433,7 +433,7 @@ gss_verify_mic(minor_status, context_handle,
 }
 
 /* V2 */
-KRB5_DLLIMP OM_uint32 KRB5_CALLCONV
+GSS_DLLIMP OM_uint32 KRB5_CALLCONV
 gss_wrap(minor_status, context_handle, conf_req_flag, qop_req,
 	 input_message_buffer, conf_state, output_message_buffer)
     OM_uint32		FAR *minor_status;
@@ -450,7 +450,7 @@ gss_wrap(minor_status, context_handle, conf_req_flag, qop_req,
 }
 
 /* V2 */
-KRB5_DLLIMP OM_uint32 KRB5_CALLCONV
+GSS_DLLIMP OM_uint32 KRB5_CALLCONV
 gss_str_to_oid(minor_status, oid_str, oid)
     OM_uint32		FAR *minor_status;
     gss_buffer_t	oid_str;
@@ -460,7 +460,7 @@ gss_str_to_oid(minor_status, oid_str, oid)
 }
 
 /* V2 */
-KRB5_DLLIMP OM_uint32 KRB5_CALLCONV
+GSS_DLLIMP OM_uint32 KRB5_CALLCONV
 gss_test_oid_set_member(minor_status, member, set, present)
     OM_uint32	FAR *minor_status;
     gss_OID	member;
@@ -472,7 +472,7 @@ gss_test_oid_set_member(minor_status, member, set, present)
 }
 
 /* V1 only */
-KRB5_DLLIMP OM_uint32 KRB5_CALLCONV
+GSS_DLLIMP OM_uint32 KRB5_CALLCONV
 gss_unseal(minor_status, context_handle, input_message_buffer,
 	   output_message_buffer, conf_state, qop_state)
      OM_uint32 FAR *minor_status;
@@ -488,7 +488,7 @@ gss_unseal(minor_status, context_handle, input_message_buffer,
 }
 
 /* V2 */
-KRB5_DLLIMP OM_uint32 KRB5_CALLCONV
+GSS_DLLIMP OM_uint32 KRB5_CALLCONV
 gss_unwrap(minor_status, context_handle, input_message_buffer, 
 	   output_message_buffer, conf_state, qop_state)
     OM_uint32		FAR *minor_status;
@@ -503,7 +503,7 @@ gss_unwrap(minor_status, context_handle, input_message_buffer,
 }
 
 /* V1 only */
-KRB5_DLLIMP OM_uint32 KRB5_CALLCONV
+GSS_DLLIMP OM_uint32 KRB5_CALLCONV
 gss_verify(minor_status, context_handle, message_buffer,
 	   token_buffer, qop_state)
      OM_uint32 FAR *minor_status;
@@ -520,7 +520,7 @@ gss_verify(minor_status, context_handle, message_buffer,
 }
 
 /* V2 interface */
-KRB5_DLLIMP OM_uint32 KRB5_CALLCONV
+GSS_DLLIMP OM_uint32 KRB5_CALLCONV
 gss_wrap_size_limit(minor_status, context_handle, conf_req_flag,
 		    qop_req, req_output_size, max_input_size)
     OM_uint32		FAR *minor_status;
@@ -536,7 +536,7 @@ gss_wrap_size_limit(minor_status, context_handle, conf_req_flag,
 }
 
 /* V2 interface */
-KRB5_DLLIMP OM_uint32 KRB5_CALLCONV
+GSS_DLLIMP OM_uint32 KRB5_CALLCONV
 gss_canonicalize_name(minor_status, input_name, mech_type, output_name)
 	OM_uint32  *minor_status;
 	const gss_name_t input_name;
@@ -549,7 +549,7 @@ gss_canonicalize_name(minor_status, input_name, mech_type, output_name)
 
 
 /* V2 interface */
-KRB5_DLLIMP OM_uint32 KRB5_CALLCONV
+GSS_DLLIMP OM_uint32 KRB5_CALLCONV
 gss_export_name(minor_status, input_name, exported_name)
 	OM_uint32  *minor_status;
 	const gss_name_t input_name;
@@ -559,7 +559,7 @@ gss_export_name(minor_status, input_name, exported_name)
 }
 
 /* V2 interface */
-KRB5_DLLIMP OM_uint32 KRB5_CALLCONV
+GSS_DLLIMP OM_uint32 KRB5_CALLCONV
 gss_duplicate_name(minor_status, input_name, dest_name)
 	OM_uint32  *minor_status;
 	const gss_name_t input_name;
