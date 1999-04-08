ff2f6b386fb350dce7e43e45084e0c2a307ec2b4
Miro Jurisic <meeroh@mit.edu>
1999-04-08 17:13:44 +0000

    Updated to 1.1b13
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@11343 dc483132-0cff-0310-8789-dd5450dbe970

--- src/mac/version.r
+++ src/mac/version.r
@@ -7,10 +7,10 @@
 #endif
 
 resource 'vers' (1) {
-	0x01, 0x10, beta, 0x12,
+	0x01, 0x10, beta, 0x13,
 	verUS,
-	"1.1b12",
-	"1.1b12, Copyright 1996-1999 Massachusetts Institute of Technology"
+	"1.1b13",
+	"1.1b13, Copyright 1996-1999 Massachusetts Institute of Technology"
 };
 
 resource 'vers' (2) {
