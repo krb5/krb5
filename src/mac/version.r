a0820abe874a337a08f28c86c1c4927a4078c00f
Miro Jurisic <meeroh@mit.edu>
1999-04-23 19:32:57 +0000

    Updated to 2.0fc3
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@11389 dc483132-0cff-0310-8789-dd5450dbe970

--- src/mac/version.r
+++ src/mac/version.r
@@ -9,8 +9,8 @@
 #include "MIT Kerberos.r"
 
 resource 'vers' (1) {
-	2, 0, final, 2,
+	2, 0, final, 3,
 	verUS,
-	"2.0fc2",
-	"2.0fc2, Copyright 1996-1999 Massachusetts Institute of Technology"
+	"2.0fc3",
+	"2.0fc3, Copyright 1996-1999 Massachusetts Institute of Technology"
 };
