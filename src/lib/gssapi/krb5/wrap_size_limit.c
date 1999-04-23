2458087ce6466432459c7d3c746ce927e9dcdab9
Theodore Tso <tytso@mit.edu>
1999-04-23 04:39:55 +0000

    wrap_size_limit.c (krb5_gss_wrap_size_limit): Fix wrap_size limit so 	that it correctly calculates its results, and underestimates the 	correct size instead of overestimating it, and not returning zero all 	the time.  (Which it used to do after the March 25 fix.)
    git-svn-id: svn://anonsvn.mit.edu/krb5/branches/V1_0_BRANCH@11381 dc483132-0cff-0310-8789-dd5450dbe970

--- src/lib/gssapi/krb5/wrap_size_limit.c
+++ src/lib/gssapi/krb5/wrap_size_limit.c
@@ -39,8 +39,9 @@ krb5_gss_wrap_size_limit(minor_status, context_handle, conf_req_flag,
 {
     krb5_context	context;
     krb5_gss_ctx_id_rec	*ctx;
-    OM_uint32		cfsize;
     OM_uint32		ohlen;
+    OM_uint32		data_size, conflen;
+    int			overhead;
 
     if (GSS_ERROR(kg_get_context(minor_status, &context)))
        return(GSS_S_FAILURE);
@@ -64,17 +65,22 @@ krb5_gss_wrap_size_limit(minor_status, context_handle, conf_req_flag,
     }
 
     /* Calculate the token size and subtract that from the output size */
-    cfsize = (conf_req_flag) ? kg_confounder_size(&ctx->enc) : 0;
+    overhead = 7 + ctx->mech_used->length;
+    data_size = req_output_size;
+    if (conf_req_flag) {
+	    conflen = kg_confounder_size(&ctx->enc);
+	    data_size = (conflen + data_size + 8) & (~7);
+    }
     ohlen = g_token_size((gss_OID) ctx->mech_used,
-			 (unsigned int) (req_output_size + cfsize +
-					 ctx->cksum_size + 14));
+			 (unsigned int) (data_size + ctx->cksum_size + 14))
+	    - req_output_size;
 
-    if (ohlen < req_output_size)
+    if (ohlen+overhead < req_output_size)
 	    /*
 	     * Cannot have trailer length that will cause us to pad over
 	     * our length
 	     */
-	    *max_input_size = (req_output_size - ohlen - 1) & (~7);
+ 	    *max_input_size = (req_output_size - ohlen - overhead) & (~7);
     else
 	    *max_input_size = 0;
     *minor_status = 0;
