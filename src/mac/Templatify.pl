bcbbc106a28254cb10acdb88a3d0c90e1799d184
Miro Jurisic <meeroh@mit.edu>
1999-05-27 20:48:41 +0000

    Removed metric buttload of old Mac crap that hasn't been used for years
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@11480 dc483132-0cff-0310-8789-dd5450dbe970

--- src/mac/Templatify.pl
+++
@@ -1,5 +0,0 @@
-#!/usr/local/bin/perl -w
-$_=<STDIN>;
-$_=<STDIN> while $_!~/^#\s*TEMPLATE BEGINS HERE\s*$/;
-$_=<STDIN> while /^# *TEMPLATE BEGINS HERE\s*$/;
-while (<STDIN>) { y#\245:\304\266#\*/:\\#; print; }
--- src/mac/gss/GSSSample.prefix.h
+++
@@ -1,5 +0,0 @@
-#define GSSAPI_V2
-
-
-//#include <PP_Prefix.h>
-#include <PP_DebugHeaders.h>
