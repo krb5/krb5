baf88aed426cf865c2eb23e43812a4348a4f7ac8
Tom Yu <tlyu@mit.edu>
1999-10-27 00:32:45 +0000

    pullup Makefile hacking from trunk
    git-svn-id: svn://anonsvn.mit.edu/krb5/branches/krb5-1-1@11885 dc483132-0cff-0310-8789-dd5450dbe970

--- src/lib/krb5/asn.1/asn1buf.h
+++ src/lib/krb5/asn.1/asn1buf.h
@@ -170,14 +170,14 @@ asn1_error_code asn1buf_insert_charstring
    modifies  *buf
    effects   Inserts the contents of s (a character array of length len)
               into the buffer *buf, expanding the buffer if necessary.
-	     Returns ENOMEM if memory is exhuasted. */
+	     Returns ENOMEM if memory is exhausted. */
 
 asn1_error_code asn1buf_remove_octet
 	PROTOTYPE((asn1buf *buf, asn1_octet *o));
 /* requires  *buf is allocated
    effects   Returns *buf's current octet in *o and advances to
               the next octet.
-	     Returns ASN1_OVERRUN if *buf has already been exhuasted. */
+	     Returns ASN1_OVERRUN if *buf has already been exhausted. */
 #define asn1buf_remove_octet(buf,o) \
   (((buf)->next > (buf)->bound) \
    ? ASN1_OVERRUN \
