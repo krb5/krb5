8ca6124d45dd2d7f1dcae2ba11318ddf9e9ad27d
Ken Raeburn <raeburn@mit.edu>
2000-06-30 23:57:42 +0000

    * rpc_test_setup.sh: Error out if server_handle doesn't get set in the Tcl script
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@12495 dc483132-0cff-0310-8789-dd5450dbe970

--- src/lib/rpc/unit-test/rpc_test_setup.sh
+++ src/lib/rpc/unit-test/rpc_test_setup.sh
@@ -27,6 +27,7 @@ cat - > /tmp/rpc_test_setup$$ <<\EOF
 source $env(TCLUTIL)
 set h $env(CANON_HOST)
 puts stdout [ovsec_kadm_init admin admin $OVSEC_KADM_ADMIN_SERVICE null $OVSEC_KADM_STRUCT_VERSION $OVSEC_KADM_API_VERSION_1 server_handle]
+if ![info exists server_handle] { exit 1 }
 puts stdout [ovsec_kadm_create_principal $server_handle [simple_principal server/$h] {OVSEC_KADM_PRINCIPAL} admin]
 puts stdout [ovsec_kadm_randkey_principal $server_handle server/$h key]
 puts stdout [ovsec_kadm_create_principal $server_handle [simple_principal notserver/$h] {OVSEC_KADM_PRINCIPAL} admin]
@@ -34,6 +35,11 @@ puts stdout [ovsec_kadm_randkey_principal $server_handle notserver/$h key]
 puts stdout [ovsec_kadm_destroy $server_handle]
 EOF
 eval "$CLNTTCL $REDIRECT < /tmp/rpc_test_setup$$"
+if test $? != 0 ; then
+    rm /tmp/rpc_test_setup$$
+    echo 1>&2 error setting up database for tests
+    exit 1
+fi
 rm /tmp/rpc_test_setup$$
 
 rm -f $RPC_TEST_SRVTAB
