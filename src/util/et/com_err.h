b6a42e596c12485ed6c34f1934f91084dbc5c2cf
Ken Raeburn <raeburn@mit.edu>
2000-07-02 20:52:00 +0000

    back out format-attribute change
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@12510 dc483132-0cff-0310-8789-dd5450dbe970

--- src/util/et/com_err.h
+++ src/util/et/com_err.h
@@ -69,11 +69,7 @@ extern "C" {
 #endif
 
 KRB5_DLLIMP extern void KRB5_CALLCONV_C com_err
-	ET_STDARG_P((const char FAR *, errcode_t, const char FAR *, ...))
-#if !defined (__cplusplus) && (__GNUC__ > 2 || (__GNUC__ == 2 && __GNUC_MINOR__ >= 7))
-       __attribute__ ((__format__ (__printf__, 3, 4)))
-#endif
-	;
+	ET_STDARG_P((const char FAR *, errcode_t, const char FAR *, ...));
 KRB5_DLLIMP extern void KRB5_CALLCONV com_err_va
 	ET_P((const char FAR *whoami, errcode_t code, const char FAR *fmt,
 	      va_list ap));
