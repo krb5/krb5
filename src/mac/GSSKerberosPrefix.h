ea2a3a8b36efef6f3dc44b106551b8b739b7f373
Alexandra Ellwood <lxs@mit.edu>
2000-09-11 21:40:02 +0000

    shell scripts to generate header files and com_err error tables and a prefix file for the ProjectBuilder Mac OS X build system
    git-svn-id: svn://anonsvn.mit.edu/krb5/branches/krb5-1-2@12648 dc483132-0cff-0310-8789-dd5450dbe970

---
+++ src/mac/GSSKerberosPrefix.h
@@ -0,0 +1,60 @@
+#include <ConditionalMacros.h>
+
+#define SIZEOF_LONG		4
+#define SIZEOF_INT		4
+#define SIZEOF_SHORT		2
+
+#define	KRB5_DLLIMP		
+#define	GSS_DLLIMP		
+#define KRB5_CALLCONV		
+#define KRB5_CALLCONV_C		
+#define	FAR			
+
+#define	krb5_sigtype		void
+
+/*#define USE_CCACHEAPI*/
+
+#define HAVE_NETINET_IN_H	1
+#define HAVE_SYS_STAT_H		1
+#define	HAVE_SYS_PARAM_H	1
+#define	HAVE_UNISTD_H		1
+#define	HAVE_STDLIB_H		1
+#define	HAVE_STDARG_H		1
+#define HAVE_SYS_TYPES_H	1
+#define	HAVE_PATHS_H		1
+#define	HAVE_REGEX_H		1
+#define	HAVE_REGEXP_H		1
+#define	HAVE_FCNTL_H		1
+#define	HAVE_MEMORY_H		1
+
+#define	HAVE_STAT		1
+#define	HAVE_ACCESS		1
+#define	HAVE_FLOCK		1
+
+#define	HAVE_FCHMOD		1
+#define	HAVE_CHMOD		1
+
+#define	HAVE_STRFTIME		1
+#define	HAVE_GETEUID		1
+
+#define	HAVE_SETENV		1
+#define	HAVE_UNSETENV		1
+#define	HAVE_GETENV		1
+
+#define	HAVE_SETSID		1
+#define	HAVE_GETHOSTBYNAME2	1
+
+#define	HAVE_VFPRINTF		1
+#define	HAVE_VSPRINTF		1
+
+#define	HAVE_STRDUP		1
+#define	HAVE_STRCASECMP		1
+#define	HAVE_STRERROR		1
+#define	HAVE_MEMMOVE		1
+#define	HAVE_DAEMON		1
+#define	HAVE_GETUID		1
+#define	HAVE_SSCANF		1
+#define	HAVE_SYSLOG		1
+#define	HAVE_REGEXEC		1
+#define	HAVE_REGCOMP		1
+#define	HAVE_SA_LEN		1
