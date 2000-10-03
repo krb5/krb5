af271f39e70c3dcc76ac0334eb4b73a6843c252e
Ezra Peisach <epeisach@mit.edu>
2000-10-03 16:04:36 +0000

    * add_cred.c (krb5_gss_add_cred): krb5_cc_get_type() and         krb5_cc_get_name() return const char *.
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@12710 dc483132-0cff-0310-8789-dd5450dbe970

--- src/lib/gssapi/krb5/add_cred.c
+++ src/lib/gssapi/krb5/add_cred.c
@@ -166,7 +166,8 @@ krb5_gss_add_cred(minor_status, input_cred_handle,
 	/* make a copy */
 	krb5_gss_cred_id_t new_cred;
 	char *kttype, ktboth[1024];
-	char *cctype, *ccname, ccboth[1024];
+	const char *cctype, *ccname;
+	char ccboth[1024];
 
 	if ((new_cred =
 	     (krb5_gss_cred_id_t) xmalloc(sizeof(krb5_gss_cred_id_rec)))
