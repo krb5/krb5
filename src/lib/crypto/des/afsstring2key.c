f95f67bd8edef21bd52954501c847858307c3b8a
Ken Raeburn <raeburn@mit.edu>
2001-01-21 01:27:53 +0000

    missed a variable that should've been made const
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@12929 dc483132-0cff-0310-8789-dd5450dbe970

--- src/lib/crypto/des/afsstring2key.c
+++ src/lib/crypto/des/afsstring2key.c
@@ -278,7 +278,7 @@ static const char	P[] = {
  * For some reason, they give a 0-origin
  * index, unlike everything else.
  */
-static char	S[8][64] = {
+static const char	S[8][64] = {
 	{14, 4,13, 1, 2,15,11, 8, 3,10, 6,12, 5, 9, 0, 7,
 	  0,15, 7, 4,14, 2,13, 1,10, 6,12,11, 9, 5, 3, 8,
 	  4, 1,14, 8,13, 6, 2,11,15,12, 9, 7, 3,10, 5, 0,
