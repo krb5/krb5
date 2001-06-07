52e601ce99f2f53817d40fbfa948b631858d2a84
Ezra Peisach <epeisach@mit.edu>
2001-06-07 17:51:04 +0000

    * vfy_increds.c (krb5_verify_init_creds): Get rid of a variable 	that was set in a conditional and never used afterwards.
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@13290 dc483132-0cff-0310-8789-dd5450dbe970

--- src/lib/krb5/krb/vfy_increds.c
+++ src/lib/krb5/krb/vfy_increds.c
@@ -103,17 +103,16 @@ krb5_verify_init_creds(krb5_context context,
        /* this means there is no keying material.  This is ok, as long as
 	  it is not prohibited by the configuration */
 
-       krb5_error_code ret2;
        int nofail;
 
        if (options &&
 	   (options->flags & KRB5_VERIFY_INIT_CREDS_OPT_AP_REQ_NOFAIL)) {
 	   if (options->ap_req_nofail)
 	       goto cleanup;
-       } else if ((ret2 = krb5_libdefault_boolean(context,
-						  &creds->client->realm,
-						  "verify_ap_req_nofail",
-						  &nofail))
+       } else if (krb5_libdefault_boolean(context,
+					  &creds->client->realm,
+					  "verify_ap_req_nofail",
+					  &nofail)
 		  == 0) {
 	   if (nofail)
 	       goto cleanup;
