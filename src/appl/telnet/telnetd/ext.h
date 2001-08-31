527323a7fa7eec8f0854c837b91dd9cfc10b1bce
Ken Raeburn <raeburn@mit.edu>
2001-08-31 03:23:34 +0000

    * ext.h: Always use prototype declarations
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@13706 dc483132-0cff-0310-8789-dd5450dbe970

--- src/appl/telnet/telnetd/ext.h
+++ src/appl/telnet/telnetd/ext.h
@@ -88,108 +88,100 @@ extern char *unptyip;  /* pointer to remaining characters in buffer */
 extern int	pty, net;
 extern int	SYNCHing;		/* we are in TELNET SYNCH mode */
 
-#ifndef	P
-# ifdef	__STDC__
-#  define P(x)	x
-# else
-#  define P(x)	()
-# endif
-#endif
-
 extern void
-	_termstat P((void)),
-	add_slc P((int, int, int)),
-	check_slc P((void)),
-	change_slc P((int, int, int)),
-	cleanup P((int)),
-	clientstat P((int, int, int)),
-	copy_termbuf P((char *, int)),
-	deferslc P((void)),
-	defer_terminit P((void)),
-	do_opt_slc P((unsigned char *, int)),
-	doeof P((void)),
-	dooption P((int)),
-	dontoption P((int)),
-	edithost P((char *, char *)),
-	fatal P((int, const char *)),
-	fatalperror P((int, const char *)),
-	get_slc_defaults P((void)),
-	init_env P((void)),
-	init_termbuf P((void)),
-	interrupt P((void)),
-	localstat P((void)),
-	flowstat P((void)),
-	netclear P((void)),
-	netflush P((void)),
+	_termstat (void),
+	add_slc (int, int, int),
+	check_slc (void),
+	change_slc (int, int, int),
+	cleanup (int),
+	clientstat (int, int, int),
+	copy_termbuf (char *, int),
+	deferslc (void),
+	defer_terminit (void),
+	do_opt_slc (unsigned char *, int),
+	doeof (void),
+	dooption (int),
+	dontoption (int),
+	edithost (char *, char *),
+	fatal (int, const char *),
+	fatalperror (int, const char *),
+	get_slc_defaults (void),
+	init_env (void),
+	init_termbuf (void),
+	interrupt (void),
+	localstat (void),
+	flowstat (void),
+	netclear (void),
+	netflush (void),
 #ifdef DIAGNOSTICS
-	printoption P((char *, int)),
-	printdata P((char *, char *, int)),
-	printsub P((int, unsigned char *, int)),
+	printoption (char *, int),
+	printdata (char *, char *, int),
+	printsub (int, unsigned char *, int),
 #endif
-	ptyflush P((void)),
-	putchr P((int)),
-	putf P((char *, char *)),
-	recv_ayt P((void)),
-	send_do P((int, int)),
-	send_dont P((int, int)),
-	send_slc P((void)),
-	send_status P((void)),
-	send_will P((int, int)),
-	send_wont P((int, int)),
-	sendbrk P((void)),
-	sendsusp P((void)),
-	set_termbuf P((void)),
-	start_login P((char *, int, char *)),
-	start_slc P((int)),
-	startslave P((char *, int, char *)),
-	suboption P((void)),
-	telrcv P((void)),
-	ttloop P((void)),
-	tty_binaryin P((int)),
-	tty_binaryout P((int));
+	ptyflush (void),
+	putchr (int),
+	putf (char *, char *),
+	recv_ayt (void),
+	send_do (int, int),
+	send_dont (int, int),
+	send_slc (void),
+	send_status (void),
+	send_will (int, int),
+	send_wont (int, int),
+	sendbrk (void),
+	sendsusp (void),
+	set_termbuf (void),
+	start_login (char *, int, char *),
+	start_slc (int),
+	startslave (char *, int, char *),
+	suboption (void),
+	telrcv (void),
+	ttloop (void),
+	tty_binaryin (int),
+	tty_binaryout (int);
 
 extern int
-	end_slc P((unsigned char **)),
-	getnpty P((void)),
+	end_slc (unsigned char **),
+	getnpty (void),
 #ifndef convex
-	getpty P((int *)),
+	getpty (int *),
 #endif
-	login_tty P((int)),
-	spcset P((int, cc_t *, cc_t **)),
-	stilloob P((int)),
-	terminit P((void)),
-	termstat P((void)),
-	tty_flowmode P((void)),
-	tty_restartany P((void)),
-	tty_isbinaryin P((void)),
-	tty_isbinaryout P((void)),
-	tty_iscrnl P((void)),
-	tty_isecho P((void)),
-	tty_isediting P((void)),
-	tty_islitecho P((void)),
-	tty_isnewmap P((void)),
-	tty_israw P((void)),
-	tty_issofttab P((void)),
-	tty_istrapsig P((void)),
-	tty_linemode P((void));
+	login_tty (int),
+	spcset (int, cc_t *, cc_t **),
+	stilloob (int),
+	terminit (void),
+	termstat (void),
+	tty_flowmode (void),
+	tty_restartany (void),
+	tty_isbinaryin (void),
+	tty_isbinaryout (void),
+	tty_iscrnl (void),
+	tty_isecho (void),
+	tty_isediting (void),
+	tty_islitecho (void),
+	tty_isnewmap (void),
+	tty_israw (void),
+	tty_issofttab (void),
+	tty_istrapsig (void),
+	tty_linemode (void);
 
 extern void
-	tty_rspeed P((int)),
-	tty_setecho P((int)),
-	tty_setedit P((int)),
-	tty_setlinemode P((int)),
-	tty_setlitecho P((int)),
-	tty_setsig P((int)),
-	tty_setsofttab P((int)),
-	tty_tspeed P((int)),
-	willoption P((int)),
-	wontoption P((int));
+	tty_rspeed (int),
+	tty_setecho (int),
+	tty_setedit (int),
+	tty_setlinemode (int),
+	tty_setlitecho (int),
+	tty_setsig (int),
+	tty_setsofttab (int),
+	tty_tspeed (int),
+	willoption (int),
+	wontoption (int);
 
-extern void netprintf P((const char *, ...));
-extern void netprintf_urg P((const char *fmt, ...));
-extern void netprintf_noflush P((const char *fmt, ...));
-extern int netwrite P((const char *, size_t));
-extern void netputs P((const char *));
+extern void netprintf (const char *, ...);
+extern void netprintf_urg (const char *fmt, ...);
+extern void netprintf_noflush (const char *fmt, ...);
+extern int netwrite (const char *, size_t);
+extern void netputs (const char *);
 
 #ifdef	ENCRYPTION
 extern char	*nclearto;
