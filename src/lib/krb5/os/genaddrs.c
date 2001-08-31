b36e716a6270bc9e39e7d7bc162276fb90dea0c9
Ken Raeburn <raeburn@mit.edu>
2001-08-31 05:03:10 +0000

    fix conditionals
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@13711 dc483132-0cff-0310-8789-dd5450dbe970

--- src/lib/krb5/os/genaddrs.c
+++ src/lib/krb5/os/genaddrs.c
@@ -64,7 +64,7 @@ static void *cvtaddr (struct sockaddr_storage *a, struct addrpair *ap)
 	SET (ap->port, ss2sin(a)->sin_port, ADDRTYPE_IPPORT);
 	SET (ap->addr, ss2sin(a)->sin_addr, ADDRTYPE_INET);
 	return a;
-#ifdef AF_INET6
+#ifdef KRB5_USE_INET6
     case AF_INET6:
 	SET (ap->port, ss2sin6(a)->sin6_port, ADDRTYPE_IPPORT);
 	if (IN6_IS_ADDR_V4MAPPED (&ss2sin6(a)->sin6_addr)) {
