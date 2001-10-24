f0fe7018949eaec61d5a664ee30cc0a2120208ac
Ezra Peisach <epeisach@mit.edu>
2001-10-24 15:23:49 +0000

    * configure.in: Add optional argument to AC_DEFINE to provide         comment in generated header file.
            * acconfig.h: Remove int32_t and u_int32_t, handled by configure.in
    
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@13843 dc483132-0cff-0310-8789-dd5450dbe970

--- src/util/db2/acconfig.h
+++ src/util/db2/acconfig.h
@@ -3,8 +3,8 @@
 /* BSD4.3, non-posix types */
 
 #undef u_char
-#undef u_short
 #undef u_int
+#undef u_short
 #undef u_long
 
 /* sized types used by db internals */
@@ -13,5 +13,3 @@
 #undef u_int8_t
 #undef int16_t
 #undef u_int16_t
-#undef int32_t
-#undef u_int32_t
