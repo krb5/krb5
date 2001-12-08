63018bad00f532a24631f35f24e98727a539efae
Tom Yu <tlyu@mit.edu>
2001-12-08 01:57:17 +0000

    * update_utmp.c (PTY_GETUTXENT): Fix typo.  Thanks to Shawn 	Stepper. [fixes krb5-build/1020] [pullup from 1.2.3]
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@14057 dc483132-0cff-0310-8789-dd5450dbe970

--- src/util/pty/update_utmp.c
+++ src/util/pty/update_utmp.c
@@ -338,7 +338,7 @@
 #else
 #define PTY_STRUCT_UTMPX struct utmp
 #define PTY_SETUTXENT setutent
-#define PTY_GETUTXENT gettutent
+#define PTY_GETUTXENT getutent
 #define PTY_GETUTXLINE getutline
 #define PTY_PUTUTXLINE pututline
 #define PTY_ENDUTXENT endutent
@@ -532,7 +532,10 @@ pty_update_utmp(int process_type, int pid, const char *username,
 #ifdef __hpux
     strcpy(utmp_id, cp);
 #else
-    sprintf(utmp_id, "kl%s", cp);
+    if (len > 2 && *(cp - 1) != '/')
+      sprintf(utmp_id, "k%s", cp - 1);
+    else
+      sprintf(utmp_id, "k0%s", cp);
 #endif
     strncpy(utx.ut_id, utmp_id, sizeof(utx.ut_id));
     /*
