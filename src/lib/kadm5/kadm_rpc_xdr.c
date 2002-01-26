9047e27a9c9ff68c0f554a4473995fc7d8a1a779
Ken Raeburn <raeburn@mit.edu>
2002-01-26 03:12:17 +0000

    * kadm_rpc_xdr.c (xdr_krb5_kvno): Return an error if the key version number won't fit in the one-byte encoding we currently use.
    git-svn-id: svn://anonsvn.mit.edu/krb5/branches/krb5-1-2-2-branch@14118 dc483132-0cff-0310-8789-dd5450dbe970

--- src/lib/kadm5/kadm_rpc_xdr.c
+++ src/lib/kadm5/kadm_rpc_xdr.c
@@ -135,8 +135,14 @@ xdr_krb5_kvno(XDR *xdrs, krb5_kvno *objp)
 
 	tmp = '\0'; /* for purify, else xdr_u_char performs a umr */
 
-	if (xdrs->x_op == XDR_ENCODE)
+	if (xdrs->x_op == XDR_ENCODE) {
 		tmp = (unsigned char) *objp;
+		/* We can't change the protocol right now, so let's
+		   just reject (legitimate!) values that won't fit in
+		   our broken one-byte encoding.  */
+		if (tmp != *objp)
+		    return FALSE;
+	}
 
 	if (!xdr_u_char(xdrs, &tmp))
 		return (FALSE);
