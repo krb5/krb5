e30a561176bd8e146e551875d9d590c3f7797923
Ken Raeburn <raeburn@mit.edu>
2002-03-07 01:54:59 +0000

    8-bit-kvno workarounds from 1.2.4
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@14243 dc483132-0cff-0310-8789-dd5450dbe970

--- src/kadmin/ktutil/ktutil_funcs.c
+++ src/kadmin/ktutil/ktutil_funcs.c
@@ -504,9 +504,18 @@ krb5_error_code ktutil_write_srvtab(context, list, name)
 		lp1 = prev->next;
 	    }
 	    lp1->entry = lp->entry;
-	} else if (lp1->entry->vno < lp->entry->vno)
-	    /* Check if lp->entry is newer kvno; if so, update */
-	    lp1->entry = lp->entry;
+	} else {
+	    /* This heuristic should be roughly the same as in the
+	       keytab-reading code in libkrb5.  */
+	    int offset = 0;
+	    if (lp1->entry->vno > 240 || lp->entry->vno > 240) {
+		offset = 128;
+	    }
+#define M(X) (((X) + offset) % 256)
+	    if (M(lp1->entry->vno) < M(lp->entry->vno))
+		/* Check if lp->entry is newer kvno; if so, update */
+		lp1->entry = lp->entry;
+	}
     }
     umask(0077); /*Changing umask for all of ktutil is OK
 		  * We don't ever write out anything that should use
