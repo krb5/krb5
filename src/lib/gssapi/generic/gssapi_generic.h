7b3a73d83014b4a72fe18cd54e4f5ed586e2c3ac
Alexandra Ellwood <lxs@mit.edu>
2002-05-05 18:01:50 +0000

    gssapi_generic.h: allow inclusion by C++
    git-svn-id: svn://anonsvn.mit.edu/krb5/branches/krb5-1-2-2-branch@14429 dc483132-0cff-0310-8789-dd5450dbe970

--- src/lib/gssapi/generic/gssapi_generic.h
+++ src/lib/gssapi/generic/gssapi_generic.h
@@ -33,6 +33,16 @@
 #include <gssapi/gssapi.h>
 #endif
 
+#if defined(__cplusplus) && !defined(GSSAPIGENERIC_BEGIN_DECLS)
+#define GSSAPIGENERIC_BEGIN_DECLS	extern "C" {
+#define GSSAPIGENERIC_END_DECLS	}
+#else
+#define GSSAPIGENERIC_BEGIN_DECLS
+#define GSSAPIGENERIC_END_DECLS
+#endif
+
+GSSAPIGENERIC_BEGIN_DECLS
+
 /* Deprecated MIT krb5 oid names provided for compatibility.
  * The correct oids (GSS_C_NT_USER_NAME, etc) from rfc 2744 
  * are defined in gssapi.h. */
@@ -43,5 +53,7 @@ GSS_DLLIMP extern gss_OID gss_nt_string_uid_name;
 GSS_DLLIMP extern gss_OID gss_nt_service_name_v2;
 GSS_DLLIMP extern gss_OID gss_nt_service_name;
 GSS_DLLIMP extern gss_OID gss_nt_exported_name;
+
+GSSAPIGENERIC_END_DECLS
 
 #endif /* _GSSAPI_GENERIC_H_ */
