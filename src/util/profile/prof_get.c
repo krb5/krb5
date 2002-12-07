aae8df84aade9b538efc6ac2db392b884cec9d1a
Ken Raeburn <raeburn@mit.edu>
2002-12-07 01:55:47 +0000

    * prof_get.c (conf_yes, conf_no): Entries now point to const. (profile_parse_boolean): Updated type of 'p' correspondingly.
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@15036 dc483132-0cff-0310-8789-dd5450dbe970

--- src/util/profile/prof_get.c
+++ src/util/profile/prof_get.c
@@ -291,12 +291,12 @@ profile_get_integer(profile, name, subname, subsubname,
 	return 0;
 }
 
-static char *const conf_yes[] = {
+static const char *const conf_yes[] = {
     "y", "yes", "true", "t", "1", "on",
     0,
 };
 
-static char *const conf_no[] = {
+static const char *const conf_no[] = {
     "n", "no", "false", "nil", "0", "off",
     0,
 };
@@ -306,7 +306,7 @@ profile_parse_boolean(s, ret_boolean)
      char *s;
      int* ret_boolean;
 {
-    char *const *p;
+    const char *const *p;
     
     if (ret_boolean == NULL)
     	return PROF_EINVAL;
