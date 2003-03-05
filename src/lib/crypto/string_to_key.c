32828edef9de2f5b41492467439160a3888ae083
Ken Raeburn <raeburn@mit.edu>
2003-03-05 04:00:09 +0000

    string_to_key.c (krb5_c_string_to_key_with_params): Declare before use
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@15231 dc483132-0cff-0310-8789-dd5450dbe970

--- src/lib/crypto/string_to_key.c
+++ src/lib/crypto/string_to_key.c
@@ -27,6 +27,16 @@
 #include "k5-int.h"
 #include "etypes.h"
 
+/* Eventually this declaration should move to krb5.h.  */
+krb5_error_code KRB5_CALLCONV
+krb5_c_string_to_key_with_params(krb5_context context,
+				 krb5_enctype enctype,
+				 const krb5_data *string,
+				 const krb5_data *salt,
+				 const krb5_data *params,
+				 krb5_keyblock *key);
+
+
 krb5_error_code KRB5_CALLCONV
 krb5_c_string_to_key(context, enctype, string, salt, key)
      krb5_context context;
