7c69e073da8f6afc0a73ca35cda50d103ce970ef
Ken Raeburn <raeburn@mit.edu>
2003-12-09 23:39:06 +0000

    define SFILE for debugging
    git-svn-id: svn://anonsvn.mit.edu/krb5/branches/raeburn-gssapi-cfx@15872 dc483132-0cff-0310-8789-dd5450dbe970

--- src/lib/kadm5/clnt/client_rpc.c
+++ src/lib/kadm5/clnt/client_rpc.c
@@ -302,6 +302,7 @@ init_1(argp, clnt)
      memset((char *)&res, 0, sizeof(res));
      x = clnt_call(clnt, INIT, xdr_u_int32, argp,
 		   xdr_generic_ret, &res, TIMEOUT);
+#define SFILE (strrchr(__FILE__,'/') ? 1+strrchr(__FILE__,'/') : __FILE__)
      if (x != RPC_SUCCESS) {
 	 _log("%s:%d: init_1: clnt_call returned %d\n", SFILE, __LINE__, x);
 	  return (NULL);
