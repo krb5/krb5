b2b4e89cd2d41731ac62df6551b50f935d11d9fb
Sam Hartman <hartmans@mit.edu>
2003-12-11 23:14:06 +0000

    Remove kadmind4 and v5passwdd
    Per email to kerberos-announce remove kadmind4 and v5passwdd from the distribution.
    
    Ticket: new
    Tags: enhancement
    
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@15880 dc483132-0cff-0310-8789-dd5450dbe970

--- src/kadmin/v4server/unit-test/getpid.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# tcl sucks big fat hairy rocks
-
-$PS_ALL | awk "/$1/"' && !/awk/ && !/getpid/ && !/expect/ && !/kadmind4/ { print $2 }'
--- src/kadmin/v4server/unit-test/remove_changepw_perms.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# tcl sucks big fat hairy rocks
-
-ed $K5ROOT/ovsec_adm.acl <<EOF >/dev/null 2>&1
-g/changepw\/kerberos/s/^/#/
-w
-q
-EOF
