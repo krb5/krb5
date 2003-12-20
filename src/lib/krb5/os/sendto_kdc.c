1e9da801ec5a4053783eae3aeccda7889f154609
Ken Raeburn <raeburn@mit.edu>
2003-12-20 02:48:23 +0000

    oops.  actually do the #undef DEBUG
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@15950 dc483132-0cff-0310-8789-dd5450dbe970

--- src/lib/krb5/os/sendto_kdc.c
+++ src/lib/krb5/os/sendto_kdc.c
@@ -59,7 +59,7 @@
 #define DEFAULT_UDP_PREF_LIMIT	 1465
 #define HARD_UDP_LIMIT		32700 /* could probably do 64K-epsilon ? */
 
-#define DEBUG
+#undef DEBUG
 
 #ifdef DEBUG
 int krb5int_debug_sendto_kdc = 0;
