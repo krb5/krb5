67f6f264d8aafabe18a1966651a1fbade1038f2c
Ken Raeburn <raeburn@mit.edu>
2004-03-03 05:48:40 +0000

    * rc_base.c: Delete unused and untested semaphore support
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@16140 dc483132-0cff-0310-8789-dd5450dbe970

--- src/lib/krb5/rcache/rc_base.c
+++ src/lib/krb5/rcache/rc_base.c
@@ -11,9 +11,6 @@
  * Base "glue" functions for the replay cache.
  */
 
-#ifdef SEMAPHORE
-#include <semaphore.h>
-#endif
 #include "rc_base.h"
 
 #define FREE(x) ((void) free((char *) (x)))
@@ -26,49 +23,27 @@ struct krb5_rc_typelist
 static struct krb5_rc_typelist krb5_rc_typelist_dfl = { &krb5_rc_dfl_ops, 0 };
 static struct krb5_rc_typelist *typehead = &krb5_rc_typelist_dfl;
 
-#ifdef SEMAPHORE
-semaphore ex_typelist = 1;
-#endif
-
 krb5_error_code krb5_rc_register_type(krb5_context context,
 				      const krb5_rc_ops *ops)
 {
  struct krb5_rc_typelist *t;
-#ifdef SEMAPHORE
- down(&ex_typelist);
-#endif
  for (t = typehead;t && strcmp(t->ops->type,ops->type);t = t->next)
    ;
-#ifdef SEMAPHORE
- up(&ex_typelist);
-#endif
  if (t)
    return KRB5_RC_TYPE_EXISTS;
  if (!(t = (struct krb5_rc_typelist *) malloc(sizeof(struct krb5_rc_typelist))))
    return KRB5_RC_MALLOC;
-#ifdef SEMAPHORE
- down(&ex_typelist);
-#endif
  t->next = typehead;
  t->ops = ops;
  typehead = t;
-#ifdef SEMAPHORE
- up(&ex_typelist);
-#endif
  return 0;
 }
 
 krb5_error_code krb5_rc_resolve_type(krb5_context context, krb5_rcache *id, char *type)
 {
  struct krb5_rc_typelist *t;
-#ifdef SEMAPHORE
- down(&ex_typelist);
-#endif
  for (t = typehead;t && strcmp(t->ops->type,type);t = t->next)
    ;
-#ifdef SEMAPHORE
- up(&ex_typelist);
-#endif
  if (!t)
    return KRB5_RC_TYPE_NOTFOUND;
  /* allocate *id? nah */
