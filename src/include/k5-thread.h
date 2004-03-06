bf465a1bc8657d3b6b1fefde16deeb1af1ed229e
Ken Raeburn <raeburn@mit.edu>
2004-03-06 02:19:35 +0000

    fix debug initializer and copyright
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@16152 dc483132-0cff-0310-8789-dd5450dbe970

--- src/include/k5-thread.h
+++ src/include/k5-thread.h
@@ -1,5 +1,31 @@
-/* Copyright 2004 Massachusetts Institute of Technology.
-   All Rights Reserved.  */
+/*
+ * include/k5-thread.h
+ *
+ * Copyright 2004 by the Massachusetts Institute of Technology.
+ * All Rights Reserved.
+ *
+ * Export of this software from the United States of America may
+ *   require a specific license from the United States Government.
+ *   It is the responsibility of any person or organization contemplating
+ *   export to obtain such a license before exporting.
+ * 
+ * WITHIN THAT CONSTRAINT, permission to use, copy, modify, and
+ * distribute this software and its documentation for any purpose and
+ * without fee is hereby granted, provided that the above copyright
+ * notice appear in all copies and that both that copyright notice and
+ * this permission notice appear in supporting documentation, and that
+ * the name of M.I.T. not be used in advertising or publicity pertaining
+ * to distribution of the software without specific, written prior
+ * permission.  Furthermore if you modify this software you must label
+ * your software as modified software and not distribute it in such a
+ * fashion that it might be confused with the original M.I.T. software.
+ * M.I.T. makes no representations about the suitability of
+ * this software for any purpose.  It is provided "as is" without express
+ * or implied warranty.
+ * 
+ *
+ * Preliminary thread support.
+ */
 
 #ifndef K5_MUTEX_INITIALIZER /* handle multiple inclusion */
 
@@ -38,7 +64,7 @@ typedef struct {
     short lineno;
     const char *filename;
 } k5_mutex_debug_info;
-#define K5_MUTEX_DEBUG_INITIALIZER	{ 1, 0, 0, 0 }
+#define K5_MUTEX_DEBUG_INITIALIZER	{ 1, K5_MUTEX_DEBUG_UNLOCKED, 0, 0 }
 #define K5_MUTEX_DEBUG_LOCKED		4
 #define K5_MUTEX_DEBUG_UNLOCKED		3
 #define k5_mutex_debug_init(M)			\
