4a2966754bb3dd71e0fefc110738e4e72330eef8
Tom Yu <tlyu@mit.edu>
2004-04-12 22:06:41 +0000

    pullup from trunk
    ticket: 2527
    version_fixed: 1.3.4
    ticket: 2528
    version_fixed: 1.3.4
    
    git-svn-id: svn://anonsvn.mit.edu/krb5/branches/krb5-1-3@16243 dc483132-0cff-0310-8789-dd5450dbe970

--- src/lib/krb5/keytab/ktbase.c
+++ src/lib/krb5/keytab/ktbase.c
@@ -98,6 +98,14 @@ krb5_kt_resolve (krb5_context context, const char *name, krb5_keytab *ktid)
     }
 
     pfxlen = cp - name;
+
+#if defined(_WIN32)
+    if ( pfxlen == 1 ) {
+        /* We found a drive letter not a prefix */
+        return (*krb5_kt_dfl_ops.resolve)(context, name, ktid);
+    }
+#endif
+
     resid = name + pfxlen + 1;
 	
     pfx = malloc (pfxlen+1);
