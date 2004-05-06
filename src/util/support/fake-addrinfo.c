55b507733cf76d48156684ebf5aa5eb1c8af8ca3
Ken Raeburn <raeburn@mit.edu>
2004-05-06 03:13:56 +0000

    * fake-addrinfo.c: Deleted
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@16319 dc483132-0cff-0310-8789-dd5450dbe970

--- src/util/support/fake-addrinfo.c
+++
@@ -1,8 +0,0 @@
-/* Placeholder.
-
-   Move most of the code from fake-addrinfo.h into here, someday.
-   Enhance the caching.  */
-
-#include "fake-addrinfo.h"
-
-static int krb5int__fake_addrinfo_c__dummy = 0;
