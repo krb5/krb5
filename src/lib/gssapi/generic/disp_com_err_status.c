97338a5a792e78c38242c4d76b4d4ecc8bc47102
Ken Raeburn <raeburn@mit.edu>
2004-06-02 23:40:12 +0000

    * disp_com_err_status.c (init_et): Variable deleted. (g_display_com_err_status): Don't call initialize_ggss_error_table conditionally; instead, always call gssint_initialize_library.
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@16391 dc483132-0cff-0310-8789-dd5450dbe970

--- src/lib/gssapi/generic/disp_com_err_status.c
+++ src/lib/gssapi/generic/disp_com_err_status.c
@@ -31,7 +31,6 @@
 
 /**/
 
-static int init_et = 0;
 static const char * const no_error = "No error";
 
 /**/
@@ -53,10 +52,7 @@ g_display_com_err_status(minor_status, status_value, status_string)
    status_string->length = 0;
    status_string->value = NULL;
 
-   if (!init_et) {
-      initialize_ggss_error_table();
-      init_et = 1;
-   }
+   (void) gssint_initialize_library();
 
    if (! g_make_string_buffer(((status_value == 0)?no_error:
 			       error_message(status_value)),
