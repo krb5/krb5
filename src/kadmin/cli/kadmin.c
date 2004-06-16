e6d0f1385f49591def16f4dc5e613afe343777f3
Tom Yu <tlyu@mit.edu>
2004-06-16 01:51:47 +0000

    * kadmin.c (kadmin_startup): Add option to force old AUTH_GSSAPI 	flavor.
    ticket: 2578
    status: open
    
    git-svn-id: svn://anonsvn.mit.edu/krb5/branches/tlyu-umich-rpc@16461 dc483132-0cff-0310-8789-dd5450dbe970

--- src/kadmin/cli/kadmin.c
+++ src/kadmin/cli/kadmin.c
@@ -194,7 +194,7 @@ char *kadmin_startup(argc, argv)
 	 exit(1);
     }
 		     
-    while ((optchar = getopt(argc, argv, "r:p:kq:w:d:s:mc:t:e:")) != EOF) {
+    while ((optchar = getopt(argc, argv, "r:p:kq:w:d:s:mc:t:e:O")) != EOF) {
 	switch (optchar) {
 	case 'r':
 	    def_realm = optarg;
@@ -241,6 +241,9 @@ char *kadmin_startup(argc, argv)
 		 exit(1);
 	    }
 	    params.mask |= KADM5_CONFIG_ENCTYPES;
+	    break;
+	case 'O':
+	    params.mask |= KADM5_CONFIG_OLD_AUTH_GSSAPI;
 	    break;
 	default:
 	    usage();
