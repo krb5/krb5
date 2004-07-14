60fa275402f76ae8fdce04f20b658d6e47a1fd4b
Ken Raeburn <raeburn@mit.edu>
2004-07-14 01:45:12 +0000

    * gss_libinit.c (gssint_lib_init): Initialize new keytab-name mutex, and register two new key values.
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@16592 dc483132-0cff-0310-8789-dd5450dbe970

--- src/lib/gssapi/gss_libinit.c
+++ src/lib/gssapi/gss_libinit.c
@@ -16,10 +16,20 @@ MAKE_FINI_FUNCTION(gssint_lib_fini);
 
 int gssint_lib_init(void)
 {
+    int err;
 #if !USE_BUNDLE_ERROR_STRINGS
     add_error_table(&et_k5g_error_table);
     add_error_table(&et_ggss_error_table);
 #endif
+    err = k5_mutex_finish_init(&gssint_krb5_keytab_lock);
+    if (err)
+	return err;
+    err = k5_key_register(K5_KEY_GSS_KRB5_SET_CCACHE_OLD_NAME, free);
+    if (err)
+	return err;
+    err = k5_key_register(K5_KEY_GSS_KRB5_CCACHE_NAME, free);
+    if (err)
+	return err;
     return k5_mutex_finish_init(&kg_vdb.mutex);
 }
 
