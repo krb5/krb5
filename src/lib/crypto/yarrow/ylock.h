4c79a6b345bbcf0383e8aa1204803ab4aa0034b8
Tom Yu <tlyu@mit.edu>
2004-12-08 03:25:56 +0000

    pullup from trunk
    ticket: 2755
    version_fixed: 1.4
    
    git-svn-id: svn://anonsvn.mit.edu/krb5/branches/krb5-1-4@16918 dc483132-0cff-0310-8789-dd5450dbe970

--- src/lib/crypto/yarrow/ylock.h
+++ src/lib/crypto/yarrow/ylock.h
@@ -11,8 +11,14 @@
  *    and YARROW_LOCKING on failure
  */
 
-
+#if 0
 static int LOCK( void ) {  return (YARROW_OK); }
 static int UNLOCK( void ) {  return (YARROW_OK); }
+#else
+#include "k5-thread.h"
+extern k5_mutex_t krb5int_yarrow_lock;
+#define LOCK()	(k5_mutex_lock(&krb5int_yarrow_lock) ? YARROW_LOCKING : YARROW_OK)
+#define UNLOCK() (k5_mutex_unlock(&krb5int_yarrow_lock) ? YARROW_LOCKING : YARROW_OK)
+#endif
 
 #endif /* YLOCK_H */
