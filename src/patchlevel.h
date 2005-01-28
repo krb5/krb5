c9a729acf549c1b9fe910f26e2f3765eddde564a
Tom Yu <tlyu@mit.edu>
2005-01-28 20:13:19 +0000

    krb5-1.4-postrelease
    git-svn-id: svn://anonsvn.mit.edu/krb5/branches/krb5-1-4@17073 dc483132-0cff-0310-8789-dd5450dbe970

--- src/patchlevel.h
+++ src/patchlevel.h
@@ -53,6 +53,6 @@
 #define KRB5_MAJOR_RELEASE 1
 #define KRB5_MINOR_RELEASE 4
 #define KRB5_PATCHLEVEL 0
-/* #undef KRB5_RELTAIL */
+#define KRB5_RELTAIL "postrelease"
 /* #undef KRB5_RELDATE */
-#define KRB5_RELTAG "krb5-1-4-final"
+#define KRB5_RELTAG "krb5-1-4"
