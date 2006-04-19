dcc9a3bb91928d7bf3e871fd469a536fa322ad48
Ezra Peisach <epeisach@mit.edu>
2006-04-19 19:34:55 +0000

    Prototype for krb5_c_prf missing const
    When krb5_c_prf was added in r17219 to implement RFC 3961 PRF, the
    prototype for krb5_c_prf has the incomming krb5_data not flagged as const. 
    The implementation usage in the dk_prf.c declare as const. Make everything
    consistent.
    
    ticket: new
    
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@17943 dc483132-0cff-0310-8789-dd5450dbe970

--- src/include/k5-int.h
+++ src/include/k5-int.h
@@ -612,9 +612,10 @@ typedef krb5_error_code (*krb5_str2key_func) (const struct krb5_enc_provider *en
   const krb5_data *salt, const krb5_data *parm, krb5_keyblock *key);
 
 typedef krb5_error_code (*krb5_prf_func)(
-					 const struct krb5_enc_provider *enc, const struct krb5_hash_provider *hash,
+					 const struct krb5_enc_provider *enc,
+					 const struct krb5_hash_provider *hash,
 					 const krb5_keyblock *key,
-					 krb5_data *in, krb5_data *out);
+					 const krb5_data *in, krb5_data *out);
 
 struct krb5_keytypes {
     krb5_enctype etype;
