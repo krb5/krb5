44fd88804effb041aaf59d22922f5cc89158fd9c
Tom Yu <tlyu@mit.edu>
2006-06-22 22:23:02 +0000

    * src/lib/krb5/krb/srv_rcache.c (krb5_get_server_rcache): Apply 	patch from Rainer Weikusat to avoid double-free when 	rc_resolve_full() fails due to misconfiguration.
    ticket: 3924
    tags: pullup
    
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@18206 dc483132-0cff-0310-8789-dd5450dbe970

--- src/lib/krb5/krb/srv_rcache.c
+++ src/lib/krb5/krb/srv_rcache.c
@@ -115,17 +115,13 @@ krb5_get_server_rcache(krb5_context context, const krb5_data *piece,
     retval = krb5_rc_recover_or_initialize(context, rcache, context->clockskew);
     if (retval) {
 	krb5_rc_close(context, rcache);
-	rcache = 0;
 	goto cleanup;
     }
 
     *rcptr = rcache;
-    rcache = 0;
     retval = 0;
 
 cleanup:
-    if (rcache)
-	krb5_xfree(rcache);
     if (cachename)
 	krb5_xfree(cachename);
     return retval;
