17485051a2b610cf496e78c7f644e6c8505e1e02
Tom Yu <tlyu@mit.edu>
2006-06-22 03:00:53 +0000

    * src/util/support/plugins.c (krb5int_open_plugin): Use RTLD_LOCAL 	instead of RTLD_GLOBAL.  Use RTLD_GROUP if available.
    ticket: 3909
    tags: pullup
    
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@18192 dc483132-0cff-0310-8789-dd5450dbe970

--- src/util/support/plugins.c
+++ src/util/support/plugins.c
@@ -95,9 +95,14 @@ krb5int_open_plugin (const char *filepath, struct plugin_file_handle **h, struct
 #if USE_DLOPEN
     if (!err && (statbuf.st_mode & S_IFMT) == S_IFREG) {
         void *handle = NULL;
+#ifdef RTLD_GROUP
+#define PLUGIN_DLOPEN_FLAGS (RTLD_NOW | RTLD_LOCAL | RTLD_GROUP)
+#else
+#define PLUGIN_DLOPEN_FLAGS (RTLD_NOW | RTLD_LOCAL)
+#endif
 
         if (!err) {
-            handle = dlopen(filepath, RTLD_NOW | RTLD_GLOBAL);
+            handle = dlopen(filepath, PLUGIN_DLOPEN_FLAGS);
             if (handle == NULL) {
                 const char *e = dlerror();
                 Tprintf ("dlopen(%s): %s\n", filepath, e);
