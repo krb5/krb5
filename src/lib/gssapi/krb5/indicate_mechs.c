6ba9f22b7f215937cf5c10c5ff8ff8fc480d9f3c
Tom Yu <tlyu@mit.edu>
2006-08-01 23:57:54 +0000

    pull up r18330 from trunk
     r18330@cathode-dark-space:  tlyu | 2006-07-17 12:39:35 -0400
     ticket: new
     target_version: 1.5.1
     tags: pullup
     subject: reverse test for copy_oid_set in lib/gssapi/krb5/indicate_mechs.c
     
     	* src/lib/gssapi/krb5/indicate_mechs.c: Reverse sense of test,
     	since gssint_copy_oid_set() returns 0 on success.
     
    
    ticket: 4012
    version_fixed: 1.5.1
    
    git-svn-id: svn://anonsvn.mit.edu/krb5/branches/krb5-1-5@18400 dc483132-0cff-0310-8789-dd5450dbe970

--- src/lib/gssapi/krb5/indicate_mechs.c
+++ src/lib/gssapi/krb5/indicate_mechs.c
@@ -34,7 +34,7 @@ krb5_gss_indicate_mechs(minor_status, mech_set)
 {
    *minor_status = 0;
 
-   if (! gssint_copy_oid_set(minor_status, gss_mech_set_krb5_both, mech_set)) {
+   if (gssint_copy_oid_set(minor_status, gss_mech_set_krb5_both, mech_set)) {
          *mech_set     = GSS_C_NO_OID_SET;
          *minor_status = ENOMEM;
          return(GSS_S_FAILURE);
