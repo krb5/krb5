349003557a8133ad019d346c83664923d3314037
Andrew Boardman <amb@mit.edu>
2006-08-15 22:49:21 +0000

    Add ccache reset hack
    git-svn-id: svn://anonsvn.mit.edu/krb5/users/amb/referrals@18442 dc483132-0cff-0310-8789-dd5450dbe970

--- setup.sh
+++ setup.sh
@@ -2,8 +2,10 @@ d=`pwd`
 if ! test -d $d/trunk/src ; then echo "Not in top-level.  Punting."
 else
   title kerberos hacking
+  cp $KRB5CCNAME $KRB5CCNAME.backup
   : cd $d/src
   kpath=$d/trunk/src
   export LD_LIBRARY_PATH=$kpath/lib:$LD_LIBRARY_PATH
   PATH=${kpath}/clients/kvno:${kpath}/clients/klist:${kpath}/appl/telnet/telnet:$PATH
+  alias kcopy="cp $KRB5CCNAME.backup $KRB5CCNAME"
 fi
