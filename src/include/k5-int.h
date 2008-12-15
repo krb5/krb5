fe8fb6eaac97aa0051dec027bd6a7a6f9f02ca30
Luke Howard <lukeh@padl.com>
2008-12-15 07:00:48 +0000

    Remove KRB5_LIBOPT_RD_REQ_TRY_HOST_SPN
    git-svn-id: svn://anonsvn.mit.edu/krb5/branches/mskrb-integ@21429 dc483132-0cff-0310-8789-dd5450dbe970

--- src/include/k5-int.h
+++ src/include/k5-int.h
@@ -1293,8 +1293,7 @@ struct _krb5_context {
 
 #define KRB5_LIBOPT_SYNC_KDCTIME	0x0001
 #define KRB5_LIBOPT_CASE_INSENSITIVE	0x0002
-#define KRB5_LIBOPT_RD_REQ_TRY_HOST_SPN	0x0004
-#define KRB5_LIBOPT_UTF8		0x0008
+#define KRB5_LIBOPT_UTF8		0x0004
 
 /* internal message representations */
 
