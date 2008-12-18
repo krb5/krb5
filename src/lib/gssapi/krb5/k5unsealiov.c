a3279caaa7aa5af5fc62d8b9950f00f928220680
Luke Howard <lukeh@padl.com>
2008-12-18 14:36:40 +0000

    cleanup
    git-svn-id: svn://anonsvn.mit.edu/krb5/branches/mskrb-integ@21541 dc483132-0cff-0310-8789-dd5450dbe970

--- src/lib/gssapi/krb5/k5unsealiov.c
+++ src/lib/gssapi/krb5/k5unsealiov.c
@@ -576,7 +576,9 @@ kg_unseal_stream_iov(OM_uint32 *minor_status,
     if (major_status == GSS_S_COMPLETE)
 	*data = *tdata;
     else if (tdata->type & GSS_IOV_BUFFER_FLAG_ALLOCATED) {
-	gss_release_buffer(NULL, &tdata->buffer);
+	OM_uint32 tmp;
+
+	gss_release_buffer(&tmp, &tdata->buffer);
 	tdata->type &= ~(GSS_IOV_BUFFER_FLAG_ALLOCATED);
     }
 
