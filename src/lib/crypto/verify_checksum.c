krb5/krb5

commit 59a9c832b85945ae4a69909782bc1524579f7003

Add a compatibility layer for new cryptosystems such as CCM that do not implement the hash and verify methods, but do implement hash_iov and veirfy_iov. This is similar to what we've done at for encryption callbacks.
git-svn-id: svn://anonsvn.mit.edu/krb5/branches/mskrb-integ@21615 dc483132-0cff-0310-8789-dd5450dbe970

src/lib/crypto/verify_checksum.c
@@ -51,10 +51,25 @@ krb5_c_verify_checksum(krb5_context context, const krb5_keyblock *key,
     indata.length = cksum->length;
     indata.data = (char *) cksum->contents;
 
-    if (krb5_cksumtypes_list[i].keyhash &&
-	krb5_cksumtypes_list[i].keyhash->verify)
-	return((*(krb5_cksumtypes_list[i].keyhash->verify))(key, usage, 0, data,
-							    &indata, valid));
+    if (krb5_cksumtypes_list[i].keyhash) {
+	const struct krb5_keyhash_provider *keyhash;
+
+	keyhash = krb5_cksumtypes_list[i].keyhash;
+
+	if (keyhash->verify == NULL) {
+	    krb5_crypto_iov iov[1];
+
+	    iov[0].flags = KRB5_CRYPTO_TYPE_DATA;
+	    iov[0].data = *data;
+
+	    assert(keyhash->verify_iov != NULL);
+
+	    ret = (*keyhash->verify_iov)(key, usage, 0, iov, 1, &indata, valid);
+	} else {
+	    ret = (*keyhash->verify)(key, usage, 0, data, &indata, valid);
+	}
+	return(ret);
+    }
 
     /* otherwise, make the checksum again, and compare */
 

src/lib/crypto/make_checksum.c
@@ -63,6 +63,9 @@ krb5_c_make_checksum(krb5_context context, krb5_cksumtype cksumtype,
 
     if (krb5_cksumtypes_list[i].keyhash) {
 	/* check if key is compatible */
+	const struct krb5_keyhash_provider *keyhash;
+
+	keyhash = krb5_cksumtypes_list[i].keyhash;
 
 	if (krb5_cksumtypes_list[i].keyed_etype) {
 	    for (e1=0; e1<krb5_enctypes_length; e1++) 
@@ -82,7 +85,18 @@ krb5_c_make_checksum(krb5_context context, krb5_cksumtype cksumtype,
 	    }
 	}
 
-	ret = (*(krb5_cksumtypes_list[i].keyhash->hash))(key, usage, 0, input, &data);
+	if (keyhash->hash == NULL) {
+	    krb5_crypto_iov iov[1];
+
+	    iov[0].flags = KRB5_CRYPTO_TYPE_DATA;
+	    iov[0].data = *input;
+
+	    assert(keyhash->hash_iov != NULL);
+
+	    ret = (*keyhash->hash_iov)(key, usage, 0, iov, 1, &data);
+	} else {
+	    ret = (*keyhash->hash)(key, usage, 0, input, &data);
+	}
     } else if (krb5_cksumtypes_list[i].flags & KRB5_CKSUMFLAG_DERIVE) {
 	ret = krb5_dk_make_checksum(krb5_cksumtypes_list[i].hash,
 				    key, usage, input, &data);

commit 52036822c053360355ca7428614ae74d84a7275f

fix a logic error introduced in r21615
git-svn-id: svn://anonsvn.mit.edu/krb5/branches/mskrb-integ@21617 dc483132-0cff-0310-8789-dd5450dbe970

src/lib/crypto/verify_checksum.c
@@ -56,19 +56,16 @@ krb5_c_verify_checksum(krb5_context context, const krb5_keyblock *key,
 
 	keyhash = krb5_cksumtypes_list[i].keyhash;
 
-	if (keyhash->verify == NULL) {
+	if (keyhash->verify == NULL && keyhash->verify_iov != NULL) {
 	    krb5_crypto_iov iov[1];
 
 	    iov[0].flags = KRB5_CRYPTO_TYPE_DATA;
 	    iov[0].data = *data;
 
-	    assert(keyhash->verify_iov != NULL);
-
-	    ret = (*keyhash->verify_iov)(key, usage, 0, iov, 1, &indata, valid);
-	} else {
-	    ret = (*keyhash->verify)(key, usage, 0, data, &indata, valid);
+	    return (*keyhash->verify_iov)(key, usage, 0, iov, 1, &indata, valid);
+	} else if (keyhash->verify != NULL) {
+	    return (*keyhash->verify)(key, usage, 0, data, &indata, valid);
 	}
-	return(ret);
     }
 
     /* otherwise, make the checksum again, and compare */