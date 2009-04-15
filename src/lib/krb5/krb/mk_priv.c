8f2bf12e45c7bd2638967b96c25df61b0f2ac16b
Tom Yu <tlyu@mit.edu>
2009-04-15 20:07:45 +0000

    pull up r22184 from trunk
     ------------------------------------------------------------------------
     r22184 | ghudson | 2009-04-08 11:58:24 -0400 (Wed, 08 Apr 2009) | 7 lines
     Changed paths:
        M /trunk/src/lib/krb5/error_tables/krb5_err.et
        M /trunk/src/lib/krb5/krb/mk_priv.c
        M /trunk/src/lib/krb5/krb/mk_safe.c
        M /trunk/src/lib/krb5/krb/rd_priv.c
        M /trunk/src/lib/krb5/krb/rd_safe.c
    
     ticket: 1165
    
     mk_safe and mk_priv require the local address to be set in the auth
     context; rd_safe and rd_priv require the remote address to be set.
     Create error codes for both kinds of missing addresses and stop trying
     futilely to handle the cases where they are not set.
    
    ticket: 1165
    version_fixed: 1.7
    
    git-svn-id: svn://anonsvn.mit.edu/krb5/branches/krb5-1-7@22254 dc483132-0cff-0310-8789-dd5450dbe970

--- src/lib/krb5/krb/mk_priv.c
+++ src/lib/krb5/krb/mk_priv.c
@@ -136,6 +136,9 @@ krb5_mk_priv(krb5_context context, krb5_auth_context auth_context,
 	/* Need a better error */
 	return KRB5_RC_REQUIRED;
 
+    if (!auth_context->local_addr)
+	return KRB5_LOCAL_ADDR_REQUIRED;
+
     if ((auth_context->auth_context_flags & KRB5_AUTH_CONTEXT_DO_TIME) ||
 	(auth_context->auth_context_flags & KRB5_AUTH_CONTEXT_RET_TIME)) {
 	if ((retval = krb5_us_timeofday(context, &replaydata.timestamp,
@@ -154,28 +157,26 @@ krb5_mk_priv(krb5_context context, krb5_auth_context auth_context,
 	} else {
     	    outdata->seq = replaydata.seq;
 	}
-    } 
+    }
 
 {
     krb5_address * premote_fulladdr = NULL;
-    krb5_address * plocal_fulladdr = NULL;
+    krb5_address * plocal_fulladdr;
     krb5_address remote_fulladdr;
     krb5_address local_fulladdr;
     CLEANUP_INIT(2);
 
-    if (auth_context->local_addr) {
-	if (auth_context->local_port) {
-	    if (!(retval = krb5_make_fulladdr(context, auth_context->local_addr,
-				  	      auth_context->local_port, 
-					      &local_fulladdr))) {
-	    	CLEANUP_PUSH(local_fulladdr.contents, free);
-	    	plocal_fulladdr = &local_fulladdr;
-            } else {
-    	    	goto error;
-            }
+    if (auth_context->local_port) {
+	if (!(retval = krb5_make_fulladdr(context, auth_context->local_addr,
+					  auth_context->local_port, 
+					  &local_fulladdr))) {
+	    CLEANUP_PUSH(local_fulladdr.contents, free);
+	    plocal_fulladdr = &local_fulladdr;
 	} else {
-	    plocal_fulladdr = auth_context->local_addr;
+	    goto error;
 	}
+    } else {
+	plocal_fulladdr = auth_context->local_addr;
     }
 
     if (auth_context->remote_addr) {
