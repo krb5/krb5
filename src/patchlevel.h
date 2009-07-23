aadcf90ccf4d808fcb5a52fd12b41cfb1bcb68d9
Tom Yu <tlyu@mit.edu>
2009-07-23 18:48:13 +0000

    back to krb5-1.6.4-beta1-postrelease
    git-svn-id: svn://anonsvn.mit.edu/krb5/branches/krb5-1-6@22454 dc483132-0cff-0310-8789-dd5450dbe970

--- src/patchlevel.h
+++ src/patchlevel.h
@@ -53,6 +53,6 @@
 #define KRB5_MAJOR_RELEASE 1
 #define KRB5_MINOR_RELEASE 6
 #define KRB5_PATCHLEVEL 4
-#define KRB5_RELTAIL "beta1-kfw-3.2.3-alpha1"
+#define KRB5_RELTAIL "beta1-postrelease"
 /* #undef KRB5_RELDATE */
-#define KRB5_RELTAG "tags/kfw-3_2_2-alpha1"
+#define KRB5_RELTAG "branches/krb5-1-6"
