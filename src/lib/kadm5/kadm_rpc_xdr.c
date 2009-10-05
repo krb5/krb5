a1d0f492bb26d6e5702736e0f5874e96c4dbbbcd
Luke Howard <lukeh@padl.com>
2009-10-05 18:08:47 +0000

    correct types for lockout policy; was failing on 64-bit builds
    git-svn-id: svn://anonsvn.mit.edu/krb5/users/lhoward/lockout@22844 dc483132-0cff-0310-8789-dd5450dbe970

--- src/lib/kadm5/kadm_rpc_xdr.c
+++ src/lib/kadm5/kadm_rpc_xdr.c
@@ -498,11 +498,11 @@ _xdr_kadm5_policy_ent_rec(XDR *xdrs, kadm5_policy_ent_rec *objp, int vers)
 		return (FALSE);
 	}
 	if (vers == KADM5_API_VERSION_3) {
-		if (!xdr_long(xdrs, &objp->pw_max_fail))
+		if (!xdr_krb5_kvno(xdrs, &objp->pw_max_fail))
 			return (FALSE);
-		if (!xdr_long(xdrs, &objp->pw_failcnt_interval))
+		if (!xdr_krb5_deltat(xdrs, &objp->pw_failcnt_interval))
 			return (FALSE);
-		if (!xdr_long(xdrs, &objp->pw_lockout_duration))
+		if (!xdr_krb5_deltat(xdrs, &objp->pw_lockout_duration))
 			return (FALSE);
 	} else if (xdrs->x_op == XDR_DECODE) {
 		objp->pw_max_fail = 0;
