ee7c901f80fc9057ed6bd1f91d04859d764fb046
Greg Hudson <ghudson@mit.edu>
2009-11-22 18:13:29 +0000

    Consolidate Makefile variables now that we have only a single global configure script:     $(SRCTOP) --> $(top_srcdir)     $(srcdir)/$(thisconfigdir) --> $(top_srcdir)     $(thisconfigdir) --> $(BUILDTOP)     $(myfulldir) --> $(mydir)
    ticket: 6583
    status: open
    
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@23308 dc483132-0cff-0310-8789-dd5450dbe970

--- src/util/depfix.pl
+++ src/util/depfix.pl
@@ -34,17 +34,17 @@ $0 =~ s/^.*?(\w+)[\.\w+]*$/$1/;
 
 # A typical set of inputs, produced with srcdir=.. at top level:
 #
-# SRCTOP = ../../../util/et/../..
+# top_srcdir = ../../../util/et/../..
 # thisdir = util/et
 # srcdir = ../../../util/et
 # BUILDTOP = ../..
 # libgcc file name = /usr/lib/gcc-lib/i386-redhat-linux/3.2.3/libgcc.a
 # STLIBOBJS = error_message.o et_name.o com_err.o
 
-my($SRCTOP,$thisdir,$srcdir,$BUILDTOP,$libgccpath,$STLIBOBJS) = @ARGV;
+my($top_srcdir,$thisdir,$srcdir,$BUILDTOP,$libgccpath,$STLIBOBJS) = @ARGV;
 
 if (0) {
-    print STDERR "SRCTOP = $SRCTOP\n";
+    print STDERR "top_srcdir = $top_srcdir\n";
     print STDERR "BUILDTOP = $BUILDTOP\n";
     print STDERR "STLIBOBJS = $STLIBOBJS\n";
 }
@@ -106,25 +106,25 @@ sub do_subs {
 	s,$x/[^ ]* ,,g;
 	s,$x/[^ ]*$,,g;
     }
-    # Recognize $(SRCTOP) and variants.
-    my($srct) = $SRCTOP . "/";
-    $_ = strrep(" $srct", " \$(SRCTOP)/", $_);
-#    s, $pat, \$(SRCTOP)/,go;
+    # Recognize $(top_srcdir) and variants.
+    my($srct) = $top_srcdir . "/";
+    $_ = strrep(" $srct", " \$(top_srcdir)/", $_);
+#    s, $pat, \$(top_srcdir)/,go;
     while ($srct =~ m,/[a-z][a-zA-Z0-9_.\-]*/\.\./,) {
 	$srct =~ s,/[a-z][a-zA-Z0-9_.\-]*/\.\./,/,;
-	$_ = strrep(" $srct", " \$(SRCTOP)/", $_);
+	$_ = strrep(" $srct", " \$(top_srcdir)/", $_);
     }
     # Now try to produce pathnames relative to $(srcdir).
     if ($thisdir eq ".") {
 	# blah
     } else {
-	my($pat) = " \$(SRCTOP)/$thisdir/";
+	my($pat) = " \$(top_srcdir)/$thisdir/";
 	my($out) = " \$(srcdir)/";
 	$_ = strrep($pat, $out, $_);
 	while ($pat =~ m,/[a-z][a-zA-Z0-9_.\-]*/$,) {
 	    $pat =~ s,/[a-z][a-zA-Z0-9_.\-]*/$,/,;
 	    $out .= "../";
-	    if ($pat ne " \$(SRCTOP)/") {
+	    if ($pat ne " \$(top_srcdir)/") {
 		$_ = strrep($pat, $out, $_);
 	    }
 	}
