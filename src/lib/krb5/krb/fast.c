02f4daf79f6170f880322793db70b0e79221bccc
Sam Hartman <hartmans@mit.edu>
2009-12-02 16:16:41 +0000

    Don't upgrade to FAST if already using FAST
    git-svn-id: svn://anonsvn.mit.edu/krb5/branches/fast-negotiate@23419 dc483132-0cff-0310-8789-dd5450dbe970

--- src/lib/krb5/krb/fast.c
+++ src/lib/krb5/krb/fast.c
@@ -593,6 +593,8 @@ krb5_error_code krb5int_fast_verify_nego
 krb5_boolean krb5int_upgrade_to_fast_p
 (krb5_context context, struct krb5int_fast_request_state *state, krb5_pa_data **padata)
 {
+    if (state->armor_key != NULL)
+        return 0; /*already using FAST*/
     if (! (state->fast_state_flags & KRB5INT_FAST_ARMOR_AVAIL))
         return 0;
     if (krb5int_find_pa_data(context, padata, KRB5_PADATA_FX_FAST) != NULL) {
