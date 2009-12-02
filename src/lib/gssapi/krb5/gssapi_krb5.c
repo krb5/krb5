022d1db07a6bf0af4c993939491f0a74a53b505d
Luke Howard <lukeh@padl.com>
2009-12-02 12:13:03 +0000

    make IAKERB default mechanism
    git-svn-id: svn://anonsvn.mit.edu/krb5/users/lhoward/iakerb-refonly@23400 dc483132-0cff-0310-8789-dd5450dbe970

--- src/lib/gssapi/krb5/gssapi_krb5.c
+++ src/lib/gssapi/krb5/gssapi_krb5.c
@@ -795,14 +795,10 @@ int gss_krb5int_lib_init(void)
         return err;
 #endif
 #ifdef _GSS_STATIC_LINK
-    err = gss_krb5mechglue_init();
+    err = gss_iakerbmechglue_init();
     if (err)
         return err;
-    /*
-     * Move this above the call to gss_krb5mechglue_init() if you want
-     * to negotiate IAKERB as a default mechanism through SPNEGO.
-     */
-    err = gss_iakerbmechglue_init();
+    err = gss_krb5mechglue_init();
     if (err)
         return err;
 #endif
