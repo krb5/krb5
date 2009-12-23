ba5014c6e89edbe3b2f52d39740d514fd792e1a9
Sam Hartman <hartmans@mit.edu>
2009-12-23 21:11:19 +0000

    Declare functions in this header inline to avoid warnings for unused code
    git-svn-id: svn://anonsvn.mit.edu/krb5/branches/anonymous@23518 dc483132-0cff-0310-8789-dd5450dbe970

--- src/plugins/preauth/fast_factor.h
+++ src/plugins/preauth/fast_factor.h
@@ -4,7 +4,7 @@
  * Returns success with a null armor_key if FAST is available but not in use.
  * Returns failure if the client library does not support FAST.
  */
-static krb5_error_code
+static inline krb5_error_code
 fast_get_armor_key(krb5_context context, preauth_get_client_data_proc get_data,
                    struct _krb5_preauth_client_rock *rock,
                    krb5_keyblock **armor_key)
@@ -21,7 +21,7 @@ fast_get_armor_key(krb5_context context, preauth_get_client_data_proc get_data,
     return retval;
 }
 
-static krb5_error_code
+static inline krb5_error_code
 fast_kdc_get_armor_key(krb5_context context,
                        preauth_get_entry_data_proc get_entry,
                        krb5_kdc_req *request,
@@ -43,7 +43,7 @@ fast_kdc_get_armor_key(krb5_context context,
 
 
 
-static krb5_error_code
+static inline krb5_error_code
 fast_kdc_replace_reply_key(krb5_context context,
                            preauth_get_entry_data_proc get_data,
                            krb5_kdc_req *request)
@@ -51,7 +51,7 @@ fast_kdc_replace_reply_key(krb5_context context,
     return 0;
 }
 
-static krb5_error_code
+static inline krb5_error_code
 fast_set_kdc_verified(krb5_context context,
                       preauth_get_client_data_proc get_data,
                       struct _krb5_preauth_client_rock *rock)
