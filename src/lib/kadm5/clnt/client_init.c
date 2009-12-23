af2f4ca5428145c11f1635a5be59dc868c0adf28
Sam Hartman <hartmans@mit.edu>
2009-12-23 21:10:59 +0000

    Add support for kadmin -n
    Add support for the -n option to kadmin to support anonymous
    
    * kadm5_init_anonymous: new API
    * kadmin.c: use it
    
    git-svn-id: svn://anonsvn.mit.edu/krb5/branches/anonymous@23512 dc483132-0cff-0310-8789-dd5450dbe970

--- src/lib/kadm5/clnt/client_init.c
+++ src/lib/kadm5/clnt/client_init.c
@@ -59,7 +59,7 @@
 
 #define ADM_CCACHE  "/tmp/ovsec_adm.XXXXXX"
 
-enum init_type { INIT_PASS, INIT_SKEY, INIT_CREDS };
+enum init_type { INIT_PASS, INIT_SKEY, INIT_CREDS , INIT_ANONYMOUS};
 
 static kadm5_ret_t _kadm5_init_any(krb5_context context,
                                    char *client_name,
@@ -125,6 +125,19 @@ kadm5_ret_t kadm5_init_with_password(krb5_context context, char *client_name,
                                      void **server_handle)
 {
     return _kadm5_init_any(context, client_name, INIT_PASS, pass, NULL,
+                           service_name, params, struct_version,
+                           api_version, db_args, server_handle);
+}
+
+kadm5_ret_t kadm5_init_anonymous(krb5_context context, char *client_name,
+                                     char *service_name,
+                                     kadm5_config_params *params,
+                                     krb5_ui_4 struct_version,
+                                     krb5_ui_4 api_version,
+                                     char **db_args,
+                                     void **server_handle)
+{
+    return _kadm5_init_any(context, client_name, INIT_ANONYMOUS, NULL, NULL,
                            service_name, params, struct_version,
                            api_version, db_args, server_handle);
 }
@@ -558,9 +571,11 @@ kadm5_gic_iter(kadm5_server_handle_t handle,
         krb5_get_init_creds_opt_set_forwardable(opt, 0);
         krb5_get_init_creds_opt_set_proxiable(opt, 0);
         krb5_get_init_creds_opt_set_out_ccache(ctx, opt, ccache);
+        if (init_type == INIT_ANONYMOUS)
+            krb5_get_init_creds_opt_set_anonymous(opt, 1);
             }
 
-    if (init_type == INIT_PASS) {
+    if (init_type == INIT_PASS || init_type == INIT_ANONYMOUS) {
         code = krb5_get_init_creds_password(ctx, &outcreds, client, pass,
                                             krb5_prompter_posix,
                                             NULL, 0,
