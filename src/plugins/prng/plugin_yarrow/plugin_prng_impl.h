5e13d5e3633c29f30993a15e61a321f7df83f58e
Zhanna Tsitkov <tsitkova@mit.edu>
2010-04-14 19:32:12 +0000

    Add missing files
    git-svn-id: svn://anonsvn.mit.edu/krb5/branches/plugins@23903 dc483132-0cff-0310-8789-dd5450dbe970

---
+++ src/plugins/prng/plugin_yarrow/plugin_prng_impl.h
@@ -0,0 +1,14 @@
+/*
+ * plugin_prng_impl.h
+ *
+ */
+
+#ifndef PLUGIN_PRNG_IMPL_H_
+#define PLUGIN_PRNG_IMPL_H_
+
+#include <plugin_manager.h>
+#include <plugin_prng.h>
+
+plhandle plugin_yarrow_prng_create(void);
+
+#endif /* PLUGIN_PRNG_IMPL_H_ */
