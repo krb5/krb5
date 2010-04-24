b11be41cec65d8fab8256c816178a3d41ecb7642
Greg Hudson <ghudson@mit.edu>
2010-04-24 19:12:34 +0000

    Add a sentinel to the sample gss-server after the socket is ready, and use it in gssapi.exp in the dejagnu test suite instead of sleeping.
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@23934 dc483132-0cff-0310-8789-dd5450dbe970

--- src/appl/gss-sample/gss-server.c
+++ src/appl/gss-sample/gss-server.c
@@ -731,6 +731,7 @@ main(int argc, char **argv)
         if ((stmp = create_socket(port)) >= 0) {
             if (listen(stmp, max_threads == 1 ? 0 : max_threads) < 0)
                 perror("listening on socket");
+            fprintf(stderr, "starting...\n");
 
             do {
                 struct _work_plan *work = malloc(sizeof(struct _work_plan));
