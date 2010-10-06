6768998b5d1b3ed9678a2eb94a27c3d284ab8e22
Greg Hudson <ghudson@mit.edu>
2010-10-06 01:37:24 +0000

    Adjust prototype files for easier extraction of copyright/license statements, per mailing list discussion.
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@24432 dc483132-0cff-0310-8789-dd5450dbe970

--- src/prototype/prototype.c
+++ src/prototype/prototype.c
@@ -1,7 +1,6 @@
 /* -*- mode: c; c-basic-offset: 4; indent-tabs-mode: nil -*- */
+/* prototype/prototype.c */
 /*
- * prototype/prototype.c
- *
  * Copyright (C) 2010 by the Massachusetts Institute of Technology.
  * All rights reserved.
  *
@@ -23,7 +22,8 @@
  * M.I.T. makes no representations about the suitability of
  * this software for any purpose.  It is provided "as is" without express
  * or implied warranty.
- *
- *
+ */
+
+/*
  * <<< Description >>>
  */
--- src/prototype/prototype.h
+++ src/prototype/prototype.h
@@ -1,7 +1,6 @@
 /* -*- mode: c; c-basic-offset: 4; indent-tabs-mode: nil -*- */
+/* prototype/prototype.h */
 /*
- * prototype/prototype.h
- *
  * Copyright (C) 2010 by the Massachusetts Institute of Technology.
  * All rights reserved.
  *
@@ -23,8 +22,9 @@
  * M.I.T. makes no representations about the suitability of
  * this software for any purpose.  It is provided "as is" without express
  * or implied warranty.
- *
- *
+ */
+
+/*
  * <<< Description >>>
  */
 
