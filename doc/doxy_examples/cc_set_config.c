b438c1fc7ac5fa509e2c44054787875a20aedd6b
Zhanna Tsitkov <tsitkova@mit.edu>
2011-04-06 19:44:07 +0000

    Documentation updates
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@24851 dc483132-0cff-0310-8789-dd5450dbe970

--- doc/doxy_examples/cc_set_config.c
+++ doc/doxy_examples/cc_set_config.c
@@ -1,16 +1,33 @@
 /** @example  cc_set_config.c
  *
- *  Demo for krb5_cc_set_config function
+ *  Usage examples for krb5_cc_set_config and krb5_cc_get_config functions
  */
 #include <k5-int.h>
 
 krb5_error_code
-func(krb5_context context, krb5_ccache id,
-     krb5_const_principal principal, const char *key)
+func_set(krb5_context context, krb5_ccache id,
+         krb5_const_principal principal, const char *key)
 {
    krb5_data config_data;
 
    config_data.data = "yes";
    config_data.length = strlen(config_data.data);
    return  krb5_cc_set_config(context, id, principal, key, &config_data);
+}
+
+krb5_error_code
+func_get(krb5_context context, krb5_ccache id,
+         krb5_const_principal principal, const char *key)
+{
+   krb5_data config_data;
+   krb5_error_code ret;
+
+   config_data.data = NULL;
+   ret = krb5_cc_get_config(context, id, principal, key, &config_data);
+   if (ret){
+        return ret;
+   }
+   /* do something */
+   krb5_free_data_contents(context, &config_data);
+   return ret;
 }
