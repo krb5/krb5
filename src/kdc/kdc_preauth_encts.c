5a9995350dbe2e6ab679d62e3d5074259ae2168a
Greg Hudson <ghudson@mit.edu>
2011-10-15 15:08:02 +0000

    Untabify kdc_preauth_encts.c
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@25349 dc483132-0cff-0310-8789-dd5450dbe970

--- src/kdc/kdc_preauth_encts.c
+++ src/kdc/kdc_preauth_encts.c
@@ -30,7 +30,7 @@
 
 static void
 enc_ts_get(krb5_context context, krb5_kdc_req *request,
-	   krb5_kdcpreauth_callbacks cb, krb5_kdcpreauth_rock rock,
+           krb5_kdcpreauth_callbacks cb, krb5_kdcpreauth_rock rock,
            krb5_kdcpreauth_moddata moddata, krb5_preauthtype pa_type,
            krb5_kdcpreauth_edata_respond_fn respond, void *arg)
 {
@@ -41,10 +41,10 @@ enc_ts_get(krb5_context context, krb5_kdc_req *request,
 
 static void
 enc_ts_verify(krb5_context context, krb5_data *req_pkt, krb5_kdc_req *request,
-	      krb5_enc_tkt_part *enc_tkt_reply, krb5_pa_data *pa,
-	      krb5_kdcpreauth_callbacks cb, krb5_kdcpreauth_rock rock,
-	      krb5_kdcpreauth_moddata moddata,
-	      krb5_kdcpreauth_verify_respond_fn respond, void *arg)
+              krb5_enc_tkt_part *enc_tkt_reply, krb5_pa_data *pa,
+              krb5_kdcpreauth_callbacks cb, krb5_kdcpreauth_rock rock,
+              krb5_kdcpreauth_moddata moddata,
+              krb5_kdcpreauth_verify_respond_fn respond, void *arg)
 {
     krb5_pa_enc_ts *            pa_enc = 0;
     krb5_error_code             retval;
