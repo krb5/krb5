cf3377d75d3e8df54be85faa9b37e319559aa191
Tom Yu <tlyu@mit.edu>
2011-10-25 19:58:29 +0000

    README and patchlevel.h for krb5-1.8.5-beta1
    git-svn-id: svn://anonsvn.mit.edu/krb5/branches/krb5-1-8@25415 dc483132-0cff-0310-8789-dd5450dbe970

--- src/patchlevel.h
+++ src/patchlevel.h
@@ -52,7 +52,7 @@
  */
 #define KRB5_MAJOR_RELEASE 1
 #define KRB5_MINOR_RELEASE 8
-#define KRB5_PATCHLEVEL 4
-#define KRB5_RELTAIL "postrelease"
+#define KRB5_PATCHLEVEL 5
+#define KRB5_RELTAIL "beta1"
 /* #undef KRB5_RELDATE */
-#define KRB5_RELTAG "branches/krb5-1-8"
+#define KRB5_RELTAG "tags/krb5-1-8-5-beta1"
