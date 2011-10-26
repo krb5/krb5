3e1de31181fb999840fd90f8df505fa7f0414646
Greg Hudson <ghudson@mit.edu>
2011-10-26 22:34:21 +0000

    Fix format string for TRACE_INIT_CREDS_SERVICE
    This should also be pulled up to 1.10.
    
    ticket: 6993
    tags: pullup
    target_version: 1.9.2
    
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@25417 dc483132-0cff-0310-8789-dd5450dbe970

--- src/include/k5-trace.h
+++ src/include/k5-trace.h
@@ -200,7 +200,7 @@
 #define TRACE_INIT_CREDS_SALT_PRINC(c, salt)                    \
     TRACE(c, (c, "Salt derived from principal: {data}", salt))
 #define TRACE_INIT_CREDS_SERVICE(c, service)                            \
-    TRACE(c, (c, "Setting initial creds service to {string}", service))
+    TRACE(c, (c, "Setting initial creds service to {str}", service))
 
 #define TRACE_KT_GET_ENTRY(c, keytab, princ, vno, enctype, err)         \
     TRACE(c, (c, "Retrieving {princ} from {keytab} (vno {int}, "        \
