a08ac46be6891e52729ef63eb060dabb6220543c
Kevin Wasserman <kevin.wasserman@painless-security.com>
2012-04-18 06:49:21 -0400

    kfw "Initialize Ticket" -> "Get Ticket"
    Signed-off-by: Kevin Wasserman <kevin.wasserman@painless-security.com>
    
    (cherry picked from commit d0b0d8e231b27ff9e27b4615fceee094b06cf60c)
    
    ticket: 7247
    status: resolved

--- src/windows/leash/Leash.cpp
+++ src/windows/leash/Leash.cpp
@@ -248,7 +248,7 @@ BOOL CLeashApp::InitInstance()
 
 				ldi.size = LSH_DLGINFO_EX_V1_SZ;
 				ldi.dlgtype = DLGTYPE_PASSWD;
-                ldi.title = "Initialize Ticket";
+                ldi.title = "Get Ticket";
                 ldi.username = username;
 				ldi.realm = realm;
                 ldi.dlgtype = DLGTYPE_PASSWD;
@@ -1532,7 +1532,7 @@ CLeashApp::ObtainTicketsViaUserIfNeeded(HWND hWnd)
             LSH_DLGINFO_EX ldi;
             ldi.size = LSH_DLGINFO_EX_V1_SZ;
             ldi.dlgtype = DLGTYPE_PASSWD;
-            ldi.title = "Initialize Ticket";
+            ldi.title = "Get Ticket";
             ldi.username = NULL;
             ldi.realm = NULL;
             ldi.dlgtype = DLGTYPE_PASSWD;
@@ -1565,7 +1565,7 @@ CLeashApp::ObtainTicketsViaUserIfNeeded(HWND hWnd)
             LSH_DLGINFO_EX ldi;
             ldi.size = LSH_DLGINFO_EX_V1_SZ;
             ldi.dlgtype = DLGTYPE_PASSWD;
-            ldi.title = "Initialize Ticket";
+            ldi.title = "Get Ticket";
             ldi.username = NULL;
             ldi.realm = NULL;
             ldi.dlgtype = DLGTYPE_PASSWD;
@@ -1591,7 +1591,7 @@ CLeashApp::ObtainTicketsViaUserIfNeeded(HWND hWnd)
             LSH_DLGINFO_EX ldi;
             ldi.size = LSH_DLGINFO_EX_V1_SZ;
             ldi.dlgtype = DLGTYPE_PASSWD;
-            ldi.title = "Initialize Ticket";
+            ldi.title = "Get Ticket";
             ldi.username = NULL;
             ldi.realm = NULL;
             ldi.dlgtype = DLGTYPE_PASSWD;
