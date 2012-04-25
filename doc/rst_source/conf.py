7236ee4f4fe6ec48d9d9e7a2cf0f22a7e2ee1119
Zhanna Tsitkov <tsitkova@mit.edu>
2012-04-25 19:10:41 +0000

    Use "agogo" HTML theme (with some modifications) for the Sphinx documentation. Do not show the ReST source files
    git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@25823 dc483132-0cff-0310-8789-dd5450dbe970

--- doc/rst_source/conf.py
+++ doc/rst_source/conf.py
@@ -42,7 +42,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'MIT Kerberos'
-copyright = u'2011, MIT'
+copyright = u'2012, MIT'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -93,19 +93,19 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 # html_theme = 'default'
-html_theme = 'sphinxdoc'
+html_theme = 'agogo'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = { "headerbg":  "#59121e", "linkcolor": "#a63019", "footerbg":  "#59121e", "bgcolor": "#59121e" }
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#html_title = None
+html_title = "MIT Kerberos Documentation"
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
@@ -149,7 +149,7 @@ html_use_smartypants = True
 html_split_index = True
 
 # If true, links to the reST sources are added to the pages.
-#html_show_sourcelink = True
+html_show_sourcelink = False
 
 # If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
 #html_show_sphinx = True
