02e523ca70bc77dc31931c3ad430b9c13ada438e
Zhanna Tsitkov <tsitkova@mit.edu>
2012-05-23 15:59:24 -0400

    Modified the Sphinx HTML page layout
    1. The Feedback button is moved into the footer;
    2. The default page/doc width are set to 960px;

--- doc/rst_source/conf.py
+++ doc/rst_source/conf.py
@@ -98,7 +98,7 @@ html_theme = 'agogo'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = { "headerbg":  "#59121e", "linkcolor": "#a63019", "footerbg":  "#59121e", "bgcolor": "#59121e" }
+html_theme_options = { "linkcolor": "#a63019", "footerbg":  "#59121e", "bgcolor": "#59121e", "documentwidth": "910px", "pagewidth": "960px" }
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
