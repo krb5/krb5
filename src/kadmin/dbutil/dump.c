74b3f961e15d2eee5bad93d2a224c10834bbaab8
rbasch <probe@tardis.internal.bright-prospects.com>
2013-03-03 22:55:41 -0500

    Reset ulog if database load failed
    If an iprop slave tries to load a dump from the master and it fails,
    reset the ulog header so we take another full dump, instead of
    reporting that the slave is current when it isn't.
    
    [ghudson@mit.edu: commit message]
    
    ticket: 7530
    version_fixed: 1.11.2
    status: resolved

--- src/kadmin/dbutil/dump.c
+++ src/kadmin/dbutil/dump.c
@@ -2973,6 +2973,20 @@ error:
      */
     if (!(flags & FLAG_UPDATE)) {
         if (exit_status) {
+
+	    /* Re-init ulog so we don't accidentally think we are current */
+            if (log_ctx && log_ctx->iproprole) {
+                log_ctx->ulog->kdb_last_sno = 0;
+                log_ctx->ulog->kdb_last_time.seconds = 0;
+                log_ctx->ulog->kdb_last_time.useconds = 0;
+
+                log_ctx->ulog->kdb_first_sno = 0;
+                log_ctx->ulog->kdb_first_time.seconds = 0;
+                log_ctx->ulog->kdb_first_time.useconds = 0;
+
+                ulog_sync_header(log_ctx->ulog);
+            }
+
             kret = krb5_db_destroy(kcontext, db5util_db_args);
             /*
              * Ignore a not supported error since there is nothing to do about
