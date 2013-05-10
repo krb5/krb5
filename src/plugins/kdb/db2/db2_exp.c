1e8ec647b9c3ee20298245fb7958fe6fbeb81619
Greg Hudson <ghudson@mit.edu>
2013-05-10 15:03:25 -0400

    Fix type mismatch in db2_exp.c
    The locking wrapper for audit_as_req used the wrong function
    signature, which was harmless but produced a couple of warnings.  Fix
    it.

--- src/plugins/kdb/db2/db2_exp.c
+++ src/plugins/kdb/db2/db2_exp.c
@@ -171,7 +171,7 @@ WRAP_K (krb5_db2_promote_db,
 WRAP_K (krb5_db2_check_policy_as,
         (krb5_context kcontext, krb5_kdc_req *request, krb5_db_entry *client,
          krb5_db_entry *server, krb5_timestamp kdc_time, const char **status,
-         krb5_data *e_data),
+         krb5_pa_data ***e_data),
         (kcontext, request, client, server, kdc_time, status, e_data));
 
 WRAP_VOID (krb5_db2_audit_as_req,
