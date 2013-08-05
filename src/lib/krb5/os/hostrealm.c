4f7f1fce6edca17db625d76c1f81ea098f29c313
Greg Hudson <ghudson@mit.edu>
2013-08-05 16:05:27 -0400

    Move utility functions to hostrealm.c
    Move the remaining internal functions from hst_realm.c to hostrealm.c,
    and get rid of hst_realm.c.
    
    ticket: 7687

--- src/lib/krb5/os/hostrealm.c
+++ src/lib/krb5/os/hostrealm.c
@@ -32,8 +32,15 @@
 
 #include "k5-int.h"
 #include "os-proto.h"
+#include "fake-addrinfo.h"
 #include <krb5/hostrealm_plugin.h>
 #include <ctype.h>
+
+#if defined(_WIN32) && !defined(__CYGWIN32__)
+#ifndef EAFNOSUPPORT
+#define EAFNOSUPPORT WSAEAFNOSUPPORT
+#endif
+#endif
 
 struct hostrealm_module_handle {
     struct krb5_hostrealm_vtable_st vt;
@@ -197,6 +204,131 @@ copy_list(char **in, char ***out)
     }
     *out = list;
     return 0;
+}
+
+static krb5_error_code
+translate_gai_error(int num)
+{
+    switch (num) {
+#ifdef EAI_ADDRFAMILY
+    case EAI_ADDRFAMILY:
+        return EAFNOSUPPORT;
+#endif
+    case EAI_AGAIN:
+        return EAGAIN;
+    case EAI_BADFLAGS:
+        return EINVAL;
+    case EAI_FAIL:
+        return KRB5_EAI_FAIL;
+    case EAI_FAMILY:
+        return EAFNOSUPPORT;
+    case EAI_MEMORY:
+        return ENOMEM;
+#if defined(EAI_NODATA) && EAI_NODATA != EAI_NONAME
+    case EAI_NODATA:
+        return KRB5_EAI_NODATA;
+#endif
+    case EAI_NONAME:
+        return KRB5_EAI_NONAME;
+#if defined(EAI_OVERFLOW)
+    case EAI_OVERFLOW:
+        return EINVAL;          /* XXX */
+#endif
+    case EAI_SERVICE:
+        return KRB5_EAI_SERVICE;
+    case EAI_SOCKTYPE:
+        return EINVAL;
+#ifdef EAI_SYSTEM
+    case EAI_SYSTEM:
+        return errno;
+#endif
+    }
+    abort();
+    return -1;
+}
+
+/* Get the canonical form of the local host name, using forward
+ * canonicalization only. */
+krb5_error_code
+krb5int_get_fq_local_hostname(char *buf, size_t bufsize)
+{
+    struct addrinfo *ai, hints;
+    int err;
+
+    buf[0] = '\0';
+    if (gethostname(buf, bufsize) == -1)
+        return SOCKET_ERRNO;
+
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_flags = AI_CANONNAME | AI_ADDRCONFIG;
+    err = getaddrinfo(buf, NULL, &hints, &ai);
+    if (err)
+        return translate_gai_error(err);
+    if (ai->ai_canonname == NULL) {
+        freeaddrinfo(ai);
+        return KRB5_EAI_FAIL;
+    }
+    if (strlcpy(buf, ai->ai_canonname, bufsize) >= bufsize)
+        return ENOMEM;
+    freeaddrinfo(ai);
+    return 0;
+}
+
+krb5_error_code
+k5_clean_hostname(krb5_context context, const char *host, char *cleanname,
+                  size_t lhsize)
+{
+    char *p;
+    krb5_error_code ret;
+    size_t l;
+
+    cleanname[0] = '\0';
+    if (host != NULL) {
+        if (strlcpy(cleanname, host, lhsize) >= lhsize)
+            return ENOMEM;
+    } else {
+        ret = krb5int_get_fq_local_hostname(cleanname, lhsize);
+        if (ret)
+            return ret;
+    }
+
+    /* Fold to lowercase. */
+    for (p = cleanname; *p; p++) {
+        if (isupper((unsigned char)*p))
+            *p = tolower((unsigned char)*p);
+    }
+
+    /* Strip off trailing dot. */
+    l = strlen(cleanname);
+    if (l > 0 && cleanname[l - 1] == '.')
+        cleanname[l - 1] = '\0';
+
+    return 0;
+}
+
+/* Return true if name appears to be an IPv4 or IPv6 address. */
+krb5_boolean
+k5_is_numeric_address(const char *name)
+{
+    int ndots = 0;
+    const char *p;
+
+    /* If name contains only numbers and three dots, consider it to be an IPv4
+     * address. */
+    if (strspn(name, "01234567890.") == strlen(name)) {
+        for (p = name; *p; p++) {
+            if (*p == '.')
+                ndots++;
+        }
+        if (ndots == 3)
+            return TRUE;
+    }
+
+    /* If name contains a colon, consider it to be an IPv6 address. */
+    if (strchr(name, ':') != NULL)
+        return TRUE;
+
+    return FALSE;
 }
 
 /* Construct a one-element realm list containing a copy of realm. */
