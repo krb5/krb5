5df4b25fe8e64ca1555662a11a4b8d8b042e048a
Greg Hudson <ghudson@mit.edu>
2013-10-08 17:07:34 -0400

    Fix gss_accept_sec_context error tokens
    A GSS krb5 error response contains a KRB-ERROR message, which is
    required to have a server principal name, although few recipients
    actually use it.  Starting in 1.3, accept_sec_context would fail to
    encode the error in the GSS_C_NO_NAME/GSS_C_NO_CREDENTIAL case
    (introduced by #1370) because cred->princ (which became
    cred->name->princ in 1.8) is unset.
    
    This problem got worse in 1.10 because we stopped setting the server
    field in all cases due to the changes for #6855.  In 1.11 the problem
    got worse again when a misguided change to the mechglue started
    discarding output tokens when the mechanism returns an error; the
    mechglue should only do so when it itself causes the error.
    
    Fix krb5 gss_accept_sec_context by unconditionally decoding the AP-REQ
    and using krb5_rd_req_decoded, and then using the requested ticket
    server in the KRB-ERROR message.  Fix the mechglue
    gss_accept_sec_context by reverting that part of commit
    56feee187579905c9101b0cdbdd8c6a850adcfc9.  Add a test program which
    artificially induces a replay cache failure (the easiest failure we
    can produce which has an associated RFC 4120 error code) and checks
    that this can be communicated back to the initiator via an error
    token.
    
    (cherry picked from commit c547bc16f2ab6ee66c076ef944c3fbac8a66f5d4)
    
    ticket: 1445
    version_fixed: 1.12
    status: resolved

--- src/lib/gssapi/mechglue/g_accept_sec_context.c
+++ src/lib/gssapi/mechglue/g_accept_sec_context.c
@@ -269,6 +269,9 @@ gss_cred_id_t *		d_cred;
 			status = temp_status;
 			*minor_status = temp_minor_status;
 			map_error(minor_status, mech);
+			if (output_token->length)
+			    (void) gss_release_buffer(&temp_minor_status,
+						      output_token);
 			goto error_out;
 		    }
 		    *src_name = tmp_src_name;
@@ -356,9 +359,6 @@ error_out:
     if (tmp_src_name != GSS_C_NO_NAME)
 	(void) gss_release_buffer(&temp_minor_status,
 				  (gss_buffer_t)tmp_src_name);
-
-    if (output_token->length)
-	(void) gss_release_buffer(&temp_minor_status, output_token);
 
     return (status);
 }
