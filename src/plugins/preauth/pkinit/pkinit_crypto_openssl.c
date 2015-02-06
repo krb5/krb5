deab4e58a2ba11acef60424bb24a56b6953d4d0b
Thomas Calderon <thomas.calderon@ssi.gouv.fr>
2015-02-06 16:58:14 +0100

    Cleanup the ca-pkinit feature request.

--- src/plugins/preauth/pkinit/pkinit_crypto_openssl.c
+++ src/plugins/preauth/pkinit/pkinit_crypto_openssl.c
@@ -5385,7 +5385,7 @@ cleanup:
 
 #ifndef WITHOUT_PKCS11
 static krb5_error_code
-load_cas_and_crls_pkcs11(krb5_context context,
+load_cas_pkcs11(krb5_context context,
                   pkinit_plg_crypto_context plg_cryptoctx,
                   pkinit_req_crypto_context req_cryptoctx,
                   pkinit_identity_opts *idopts,
@@ -5499,17 +5499,6 @@ load_cas_and_crls_pkcs11(krb5_context context,
                 return ENOMEM;
         }
         break;
-    /*
-    case CATYPE_CRLS:
-        if (id_cryptoctx->revoked != NULL)
-            ca_crls = id_cryptoctx->revoked;
-        else {
-            ca_crls = sk_X509_CRL_new_null();
-            if (ca_crls == NULL)
-                return ENOMEM;
-        }
-        break;
-    */
     default:
         return ENOTSUP;
     }
@@ -5602,64 +5591,6 @@ load_cas_and_crls_pkcs11(krb5_context context,
 
     }
     id_cryptoctx->p11->C_FindObjectsFinal(id_cryptoctx->session);
-//    return 0;
-//END
-
-    /* This loads from a file, a stack of x509/crl/pkey sets */
-    /*
-    if ((sk = PEM_X509_INFO_read_bio(in, NULL, NULL, NULL)) == NULL) {
-        pkiDebug("%s: error reading file '%s'\n", __FUNCTION__, filename);
-        retval = EIO;
-        goto cleanup;
-    }
-    */
-
-    /* scan over the stack created from loading the file contents,
-     * weed out duplicates, and push new ones onto the return stack
-     */
-    /*
-    for (i = 0; i < sk_X509_INFO_num(sk); i++) {
-        X509_INFO *xi = sk_X509_INFO_value(sk, i);
-        if (xi != NULL && xi->x509 != NULL && catype != CATYPE_CRLS) {
-            int j = 0, size = sk_X509_num(ca_certs), flag = 0;
-
-            if (!size) {
-                sk_X509_push(ca_certs, xi->x509);
-                xi->x509 = NULL;
-                continue;
-            }
-            for (j = 0; j < size; j++) {
-                X509 *x = sk_X509_value(ca_certs, j);
-                flag = X509_cmp(x, xi->x509);
-                if (flag == 0)
-                    break;
-                else
-                    continue;
-            }
-            if (flag != 0) {
-                sk_X509_push(ca_certs, X509_dup(xi->x509));
-            }
-        } else if (xi != NULL && xi->crl != NULL && catype == CATYPE_CRLS) {
-            int j = 0, size = sk_X509_CRL_num(ca_crls), flag = 0;
-            if (!size) {
-                sk_X509_CRL_push(ca_crls, xi->crl);
-                xi->crl = NULL;
-                continue;
-            }
-            for (j = 0; j < size; j++) {
-                X509_CRL *x = sk_X509_CRL_value(ca_crls, j);
-                flag = X509_CRL_cmp(x, xi->crl);
-                if (flag == 0)
-                    break;
-                else
-                    continue;
-            }
-            if (flag != 0) {
-                sk_X509_CRL_push(ca_crls, X509_CRL_dup(xi->crl));
-            }
-        }
-    }
-    */
 
     /* If we added something and there wasn't a stack in the
      * context before, add the temporary stack to the context.
@@ -5685,18 +5616,6 @@ load_cas_and_crls_pkcs11(krb5_context context,
                 id_cryptoctx->intermediateCAs = ca_certs;
         }
         break;
-    /*
-    case CATYPE_CRLS:
-        if (sk_X509_CRL_num(ca_crls) == 0) {
-            pkiDebug("no crls in file, %s\n", filename);
-            if (id_cryptoctx->revoked == NULL)
-                sk_X509_CRL_free(ca_crls);
-        } else {
-            if (id_cryptoctx->revoked == NULL)
-                id_cryptoctx->revoked = ca_crls;
-        }
-        break;
-    */
     default:
         /* Should have been caught above! */
         retval = EINVAL;
@@ -5785,7 +5704,7 @@ crypto_load_cas_and_crls(krb5_context context,
         break;
 #ifndef WITHOUT_PKCS11
     case IDTYPE_PKCS11:
-        return load_cas_and_crls_pkcs11(context, plg_cryptoctx, req_cryptoctx,
+        return load_cas_pkcs11(context, plg_cryptoctx, req_cryptoctx,
                                  idopts, id_cryptoctx, catype, id);
         break;
 #endif
