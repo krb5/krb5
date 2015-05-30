c1887eda950dfd84696f4f9bab9098f0bf1fd3c0
Greg Hudson <ghudson@mit.edu>
2015-05-30 13:05:52 -0400

    Add tests for GSS_KRB5_CRED_NO_CI_FLAGS_X
    ticket: 6938

--- src/tests/gssapi/t_gssapi.py
+++ src/tests/gssapi/t_gssapi.py
@@ -213,4 +213,7 @@ if krb5_mech not in out or spnego_mech not in out:
 # init_sec_context can interpret it.
 realm.run(['./t_err', 'p:' + realm.host_princ])
 
+# Test the GSS_KRB5_CRED_NO_CI_FLAGS_X cred option.
+realm.run(['./t_ciflags', 'p:' + realm.host_princ])
+
 success('GSSAPI tests')
