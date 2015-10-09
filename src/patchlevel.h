be37d5edc8be2c1372f98432b187fee6a41d9fb9
Tom Yu <tlyu@mit.edu>
2015-10-09 16:45:24 -0400

    Updates for krb5-1.14-beta1-postrelease

--- src/patchlevel.h
+++ src/patchlevel.h
@@ -52,6 +52,6 @@
 #define KRB5_MAJOR_RELEASE 1
 #define KRB5_MINOR_RELEASE 14
 #define KRB5_PATCHLEVEL 0
-#define KRB5_RELTAIL "beta1"
+#define KRB5_RELTAIL "beta1-postrelease"
 /* #undef KRB5_RELDATE */
-#define KRB5_RELTAG "krb5-1.14-beta1"
+#define KRB5_RELTAG "krb5-1.14"
