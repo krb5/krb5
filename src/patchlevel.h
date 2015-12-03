233f46816c44bfa974d1d1092426ed7f8616991d
Tom Yu <tlyu@mit.edu>
2015-12-03 16:26:25 -0500

    Updates for krb5-1.13.3

--- src/patchlevel.h
+++ src/patchlevel.h
@@ -51,7 +51,7 @@
  */
 #define KRB5_MAJOR_RELEASE 1
 #define KRB5_MINOR_RELEASE 13
-#define KRB5_PATCHLEVEL 2
-#define KRB5_RELTAIL "postrelease"
+#define KRB5_PATCHLEVEL 3
+/* #undef KRB5_RELTAIL */
 /* #undef KRB5_RELDATE */
-#define KRB5_RELTAG "krb5-1.13"
+#define KRB5_RELTAG "krb5-1.13.3-final"
