krb5/krb5

commit f07760088b72a11c54dd72efbc5739f231a4d4b0

Flatten KDC lookaside cache entries
Use krb5_data structures instead of pointers in the entry structure,
reducing the number of memory allocations.

src/kdc/replay.c
@@ -36,8 +36,8 @@ struct entry {
     TAILQ_ENTRY(entry) expire_links;
     int num_hits;
     krb5_timestamp timein;
-    krb5_data *req_packet;
-    krb5_data *reply_packet;
+    krb5_data req_packet;
+    krb5_data reply_packet;
 };
 
 #ifndef LOOKASIDE_HASH_SIZE
@@ -104,8 +104,8 @@ discard_entry(krb5_context context, struct entry *entry)
 {
     LIST_REMOVE(entry, bucket_links);
     TAILQ_REMOVE(&expiration_queue, entry, expire_links);
-    krb5_free_data(context, entry->req_packet);
-    krb5_free_data(context, entry->reply_packet);
+    krb5_free_data_contents(context, &entry->req_packet);
+    krb5_free_data_contents(context, &entry->reply_packet);
     free(entry);
 }
 
@@ -117,7 +117,7 @@ find_entry(krb5_data *req_packet)
     struct entry *e;
 
     LIST_FOREACH(e, &hash_table[hash], bucket_links) {
-        if (data_eq(*e->req_packet, *req_packet))
+        if (data_eq(e->req_packet, *req_packet))
             return e;
     }
     return NULL;
@@ -175,7 +175,7 @@ kdc_check_lookaside(krb5_data *req_packet, krb5_data **reply_packet_out)
 
     e->num_hits++;
     hits++;
-    return (krb5_copy_data(kdc_context, e->reply_packet,
+    return (krb5_copy_data(kdc_context, &e->reply_packet,
                            reply_packet_out) == 0);
 }
 
@@ -196,12 +196,14 @@ kdc_insert_lookaside(krb5_data *req_packet, krb5_data *reply_packet)
     if (e == NULL)
         return;
     e->timein = timenow;
-    if (krb5_copy_data(kdc_context, req_packet, &e->req_packet)) {
+    if (krb5int_copy_data_contents(kdc_context, req_packet, &e->req_packet)) {
         free(e);
         return;
     }
-    if (krb5_copy_data(kdc_context, reply_packet, &e->reply_packet)) {
-        krb5_free_data(kdc_context, e->req_packet);
+    if (reply_packet != NULL &&
+        krb5int_copy_data_contents(kdc_context, reply_packet,
+                                   &e->reply_packet)) {
+        krb5_free_data_contents(kdc_context, &e->req_packet);
         free(e);
         return;
     }

commit 847fc7b3caa823c219c97cc307ccb8d7d519a20f

Fix KDC to drop repeated in-progress requests
When a KDC receives a repeated request while the original request is
still in progress, it is supposed to be to drop the request.  Commit
f07760088b72a11c54dd72efbc5739f231a4d4b0 introduced a bug in this
logic, causing the KDC to instead send an empty reply.  In
kdc_check_lookaside(), return a NULL reply_packet for empty entries,
restoring the expected behavior.

[ghudson@mit.edu: edited commit message, added a comment]

ticket: 8477 (new)
target_version: 1.14-next
target_version: 1.13-next
tags: pullup

src/kdc/replay.c
@@ -177,6 +177,11 @@ kdc_check_lookaside(krb5_context kcontext, krb5_data *req_packet,
 
     e->num_hits++;
     hits++;
+
+    /* Leave *reply_packet_out as NULL for an in-progress entry. */
+    if (e->reply_packet.length == 0)
+        return TRUE;
+
     return (krb5_copy_data(kcontext, &e->reply_packet,
                            reply_packet_out) == 0);
 }