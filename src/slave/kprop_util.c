a0b43869cfeff5ded73460c8cc89472ac995a6d7
Greg Hudson <ghudson@mit.edu>
2016-08-25 14:58:28 -0400

    Rename kprop_sock.c to kprop_util.c
    Change the name of kprop_sock.c so that additional common functions
    can be added to it.

--- src/slave/kprop_util.c
+++ src/slave/kprop_util.c
@@ -1,5 +1,5 @@
 /* -*- mode: c; c-basic-offset: 4; indent-tabs-mode: nil -*- */
-/* slave/kprop_sock.c */
+/* slave/kprop_util.c */
 /*
  * Copyright (C) 2010 by the Massachusetts Institute of Technology.
  * All rights reserved.
