1dc619624421002b1e64d3b8c7e270508381b3e6
Greg Hudson <ghudson@mit.edu>
2017-04-25 17:05:23 -0400

    Don't cache referral and alternate TGT replies
    During a TGS request, if we get a TGT response that we didn't directly
    ask for (a referral TGT or an alternate TGT), don't cache it.  It
    would have limited value in the cache as similar operations won't look
    for that TGT.  If the overall TGS operation fails and is repeated, we
    could wind up caching the same entry multiple times, which doesn't
    work well with our current ccache implementations.
    
    ticket: 8579

--- src/tests/t_referral.py
+++ src/tests/t_referral.py
@@ -17,13 +17,18 @@ os.rename(realm.ccache, savefile)
 # Get credentials and check that we got a referral to REFREALM.
 def testref(realm, nametype):
     shutil.copyfile(savefile, realm.ccache)
-    realm.run(['./gcred', nametype, 'a/x.d'])
-    realm.klist(realm.user_princ, 'a/x.d@REFREALM')
+    realm.run(['./gcred', nametype, 'a/x.d@'])
+    out = realm.run([klist]).split('\n')
+    if len(out) != 8:
+        fail('unexpected number of lines in klist output')
+    if out[5].split()[4] != 'a/x.d@' or out[6].split()[4] != 'a/x.d@REFREALM':
+        fail('unexpected service principals in klist output')
+    exit(0)
 
 # Get credentials and check that we get an error, not a referral.
 def testfail(realm, nametype):
     shutil.copyfile(savefile, realm.ccache)
-    realm.run(['./gcred', nametype, 'a/x.d'], expected_code=1,
+    realm.run(['./gcred', nametype, 'a/x.d@'], expected_code=1,
               expected_msg='not found in Kerberos database')
 
 # Create a modified KDC environment and restart the KDC.
