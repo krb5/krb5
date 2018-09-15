13967567ca65a306c2d5fcb4a4aff71fb422cd80
Isaac Boukris <iboukris@gmail.com>
2018-09-15 10:28:48 +0300

    Don't rely on default realm in S4U2Self client
    When converting server principal to enterprise name (to be possibly
    used for cross-realm), ignore the realm when reparsing, to avoid a
    spurious error if a default realm isn't configured.
    
    [ghudson@mit.edu: added rewritten test case; edited commit message]
    
    ticket: 8741
    tags: pullup
    target_version: 1.16-next
    target_version: 1.15-next

--- src/lib/krb5/krb/s4u_creds.c
+++ src/lib/krb5/krb/s4u_creds.c
@@ -452,7 +452,9 @@ convert_to_enterprise(krb5_context context, krb5_principal princ,
     code = krb5_unparse_name(context, princ, &str);
     if (code != 0)
         return code;
-    code = krb5_parse_name_flags(context, str, KRB5_PRINCIPAL_PARSE_ENTERPRISE,
+    code = krb5_parse_name_flags(context, str,
+                                 KRB5_PRINCIPAL_PARSE_ENTERPRISE |
+                                 KRB5_PRINCIPAL_PARSE_IGNORE_REALM,
                                  eprinc_out);
     krb5_free_unparsed_name(context, str);
     return code;
