krb5/krb5

commit be95b52c2d0c21b1fe92f9f90166fc2fa8eecc95

* heuristic.c (get_authorized_princ_names): Fix to not call 	list_union() if cmd is NULL, otherwise freed memory gets 	referenced.
git-svn-id: svn://anonsvn.mit.edu/krb5/trunk@11239 dc483132-0cff-0310-8789-dd5450dbe970

src/clients/ksu/heuristic.c
@@ -253,17 +253,19 @@ get_authorized_princ_names(luser, cmd, princ_list)
    	}
     }	
 
-    if(retval = list_union(k5login_list, k5users_filt_list, &combined_list)){
-	close_time(k5users_flag,users_fp, k5login_flag,login_fp);
-	return retval;
-    }
-
     close_time(k5users_flag,users_fp, k5login_flag, login_fp);
 
     if (cmd) {
+	if (retval = list_union(k5login_list,
+			       k5users_filt_list, &combined_list)){
+	    close_time(k5users_flag,users_fp, k5login_flag,login_fp);
+	    return retval;
+	}
 	*princ_list = combined_list;
 	return 0;
     } else {
+	if (k5users_filt_list != NULL)
+	    free(k5users_filt_list);
 	*princ_list = k5login_list;
 	return 0;
     }

commit 0b8c6f3e1e8e561aa56a7d4e2171320119991dae

Fix double-close in ksu get_authorized_princ_names
If list_union() fails due to an allocation failure, then close_time()
will attempt to fclose users_fp and login_fp a second time.

This bug was originally introduced in commit
be95b52c2d0c21b1fe92f9f90166fc2fa8eecc95, and has been present in
every krb5 release since 1.1.

ticket: 8768 (new)
tags: pullup
target_version: 1.17
target_version: 1.16-next
target_version: 1.15-next

src/clients/ksu/heuristic.c
@@ -266,7 +266,6 @@ get_authorized_princ_names(luser, cmd, princ_list)
 
     retval = list_union(k5login_list, k5users_filt_list, &combined_list);
     if (retval){
-        close_time(k5users_flag,users_fp, k5login_flag,login_fp);
         return retval;
     }
     *princ_list = combined_list;