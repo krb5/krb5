4cad8bcee5fcea35955b8e4e5a7b63e82fc64ed6
Greg Hudson <ghudson@mit.edu>
2019-01-07 17:56:56 -0500

    Update for krb5-1.15.5-postrelease

--- src/patchlevel.h
+++ src/patchlevel.h
@@ -52,6 +52,6 @@
 #define KRB5_MAJOR_RELEASE 1
 #define KRB5_MINOR_RELEASE 15
 #define KRB5_PATCHLEVEL 5
-/* #undef KRB5_RELTAIL */
+#define KRB5_RELTAIL "postrelease"
 /* #undef KRB5_RELDATE */
-#define KRB5_RELTAG "krb5-1.15.5-final"
+#define KRB5_RELTAG "krb5-1.15"
