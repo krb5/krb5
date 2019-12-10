21b39d0196e3e0bb6b1bfbf5d60a0596cfc82e27
Greg Hudson <ghudson@mit.edu>
2019-12-10 12:06:05 -0500

    Fix xdr_bytes() strict-aliasing violations
    When xdr_bytes() is used for a gss_buffer_desc object, a temporary
    character pointer must be used for the data value to avoid a strict
    aliasing violation.
    
    When xdr_bytes() is used for a krb5_keyblock object, a temporary
    character pointer must also be used, even though the data pointer is
    of type unsigned char *, to avoid a clang warning on macOS due to the
    "#pragma pack" declaration in krb5.h.

--- src/lib/kadm5/kadm_rpc_xdr.c
+++ src/lib/kadm5/kadm_rpc_xdr.c
@@ -1125,14 +1125,16 @@ xdr_krb5_salttype(XDR *xdrs, krb5_int32 *objp)
 bool_t
 xdr_krb5_keyblock(XDR *xdrs, krb5_keyblock *objp)
 {
+   char *cp;
+
    /* XXX This only works because free_keyblock assumes ->contents
       is allocated by malloc() */
-
    if(!xdr_krb5_enctype(xdrs, &objp->enctype))
       return FALSE;
-   if(!xdr_bytes(xdrs, (char **) &objp->contents, (unsigned int *)
-		 &objp->length, ~0))
+   cp = (char *)objp->contents;
+   if(!xdr_bytes(xdrs, &cp, &objp->length, ~0))
       return FALSE;
+   objp->contents = (uint8_t *)cp;
    return TRUE;
 }
 
