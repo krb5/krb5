krb5/krb5

commit 7e8c41afc54db2ca75de5a1e2e440b034be8887b

Allow selective overrides in crypto back-ends
In the build system, always descend into all crypto subdirectories and
build all library objects.  Conditionalize each object (or
enc_provider/hash_provider entry) on a preprocessor symbol defined in
crypto_int.h.

Remove crypto_mod.h and the libk5crypto initialization and
finalization functions as they are not currently needed.  Remove stub
directories in the openssl back-end as it is no longer required to
maintain the same directory structure as builtin.

Make CRYPTO_IMPL_CFLAGS work if it is set manually at configure time
(previously only CRYPTO_IMPL_LIBS worked).

src/lib/crypto/openssl/des/des_keys.c
@@ -27,8 +27,10 @@
 #include "crypto_int.h"
 #include <openssl/des.h>
 
+#ifdef K5_OPENSSL_DES_KEY_PARITY
 void
 k5_des_fixup_key_parity(unsigned char *keybits)
 {
     DES_set_odd_parity((DES_cblock *)keybits);
 }
+#endif

src/lib/crypto/openssl/enc_provider/camellia.c
@@ -29,6 +29,8 @@
 #include <openssl/camellia.h>
 #include <openssl/modes.h>
 
+#ifdef K5_OPENSSL_CAMELLIA
+
 static krb5_error_code
 cbc_enc(krb5_key key, const krb5_data *ivec, krb5_crypto_iov *data,
         size_t num_data);
@@ -357,3 +359,5 @@ const struct krb5_enc_provider krb5int_enc_camellia256 = {
     krb5int_camellia_init_state,
     krb5int_default_free_state
 };
+
+#endif /* K5_OPENSSL_CAMELLIA */

src/lib/crypto/builtin/camellia/camellia.c
@@ -36,6 +36,9 @@
 
 #include "camellia.h"
 
+#include "crypto_int.h"
+#ifdef K5_BUILTIN_CAMELLIA
+
 /* key constants */
 
 #define CAMELLIA_SIGMA1L (0xA09E667FL)
@@ -1537,3 +1540,5 @@ camellia_dec_blk(const unsigned char in_blk[],  unsigned char out_blk[],
     Camellia_DecryptBlock(cx->keybitlen, in_blk, cx->k_sch, out_blk);
     return camellia_good;
 }
+
+#endif /* K5_BUILTIN_CAMELLIA */

src/lib/crypto/builtin/des/des_keys.c
@@ -27,8 +27,12 @@
 #include "crypto_int.h"
 #include "des_int.h"
 
+#ifdef K5_BUILTIN_DES_KEY_PARITY
+
 void
 k5_des_fixup_key_parity(unsigned char *keybits)
 {
     mit_des_fixup_key_parity(keybits);
 }
+
+#endif /* K5_BUILTIN_DES_KEY_PARITY */

src/lib/crypto/builtin/enc_provider/camellia.c
@@ -27,6 +27,8 @@
 #include "crypto_int.h"
 #include "camellia.h"
 
+#ifdef K5_BUILTIN_CAMELLIA
+
 /*
  * Private per-key data to cache after first generation.  We don't want to mess
  * with the imported Camellia implementation too much, so we'll just use two
@@ -313,3 +315,5 @@ const struct krb5_enc_provider krb5int_enc_camellia256 = {
     krb5int_default_free_state,
     camellia_key_cleanup
 };
+
+#endif /* K5_BUILTIN_CAMELLIA */

commit 6b1bc3801b57ece62d00a491bbd0819abd162dd2

Fix OpenSSL conditionals for two files
Commit 7e8c41afc54db2ca75de5a1e2e440b034be8887b mistakenly left
two files.  Fix them.

[ghudson@mit.edu: keep crypto_int.h include unconditional; wrote
commit message]

src/lib/crypto/openssl/des/des_keys.c
@@ -25,12 +25,15 @@
  */
 
 #include "crypto_int.h"
-#include <openssl/des.h>
 
 #ifdef K5_OPENSSL_DES_KEY_PARITY
+
+#include <openssl/des.h>
+
 void
 k5_des_fixup_key_parity(unsigned char *keybits)
 {
     DES_set_odd_parity((DES_cblock *)keybits);
 }
+
 #endif

src/lib/crypto/openssl/enc_provider/camellia.c
@@ -25,6 +25,9 @@
  */
 
 #include "crypto_int.h"
+
+#ifdef K5_OPENSSL_CAMELLIA
+
 #include <openssl/evp.h>
 #include <openssl/camellia.h>
 #if OPENSSL_VERSION_NUMBER >= 0x30000000L
@@ -32,8 +35,6 @@
 #else
 #include <openssl/modes.h>
 #endif
-
-#ifdef K5_OPENSSL_CAMELLIA
 
 static krb5_error_code
 cbc_enc(krb5_key key, const krb5_data *ivec, krb5_crypto_iov *data,