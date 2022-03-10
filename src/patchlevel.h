78e00c57d8aea567ab435fe802b730029fded242
Greg Hudson <ghudson@mit.edu>
2022-03-10 01:48:14 -0500

    Update for krb5-1.18.5

--- src/patchlevel.h
+++ src/patchlevel.h
@@ -51,7 +51,7 @@
  */
 #define KRB5_MAJOR_RELEASE 1
 #define KRB5_MINOR_RELEASE 18
-#define KRB5_PATCHLEVEL 4
+#define KRB5_PATCHLEVEL 5
 /* #undef KRB5_RELTAIL */
 /* #undef KRB5_RELDATE */
-#define KRB5_RELTAG "krb5-1.18.4-final"
+#define KRB5_RELTAG "krb5-1.18.5-final"
