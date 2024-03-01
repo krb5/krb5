77d5ba74577d1a807e02def37f1547a10165787e
Stefan Metzmacher <metze@samba.org>
2024-03-01 13:51:11 +0100

    gssapi/krb5: also verify zero channel bindings with KERB_AP_OPTIONS_CBT
    Signed-off-by: Stefan Metzmacher <metze@samba.org>

--- src/tests/gssapi/t_bindings.py
+++ src/tests/gssapi/t_bindings.py
@@ -4,7 +4,7 @@ realm = K5Realm()
 server = 'p:' + realm.host_princ
 
 mark('krb5 channel bindings')
-realm.run(['./t_bindings', server, '-', '-'], expected_msg='no')
+realm.run(['./t_bindings', server, '-', '-'], expected_msg='yes')
 realm.run(['./t_bindings', server, 'a', '-'], expected_msg='no')
 realm.run(['./t_bindings', server, 'a', 'a'], expected_msg='yes')
 realm.run(['./t_bindings', server, '-', 'a'], expected_msg='no')
@@ -12,7 +12,7 @@ realm.run(['./t_bindings', server, 'a', 'x'],
           expected_code=1, expected_msg='Incorrect channel bindings')
 
 mark('SPNEGO channel bindings')
-realm.run(['./t_bindings', '-s', server, '-', '-'], expected_msg='no')
+realm.run(['./t_bindings', '-s', server, '-', '-'], expected_msg='yes')
 realm.run(['./t_bindings', '-s', server, 'a', '-'], expected_msg='no')
 realm.run(['./t_bindings', '-s', server, 'a', 'a'], expected_msg='yes')
 realm.run(['./t_bindings', '-s', server, '-', 'a'], expected_msg='no')
@@ -23,8 +23,9 @@ client_aware_conf = {'libdefaults': {'client_aware_channel_bindings': 'true'}}
 e = realm.special_env('cb_aware', False, krb5_conf=client_aware_conf)
 
 mark('krb5 client_aware_channel_bindings')
-realm.run(['./t_bindings', server, '-', '-'], env=e, expected_msg='no')
-realm.run(['./t_bindings', server, 'a', '-'], env=e, expected_msg='no')
+realm.run(['./t_bindings', server, '-', '-'], env=e, expected_msg='yes')
+realm.run(['./t_bindings', server, 'a', '-'], env=e,
+          expected_code=1, expected_msg='Incorrect channel bindings')
 realm.run(['./t_bindings', server, 'a', 'a'], env=e, expected_msg='yes')
 realm.run(['./t_bindings', server, '-', 'a'], env=e,
           expected_code=1, expected_msg='Incorrect channel bindings')
@@ -32,8 +33,9 @@ realm.run(['./t_bindings', server, 'a', 'x'], env=e,
           expected_code=1, expected_msg='Incorrect channel bindings')
 
 mark('SPNEGO client_aware_channel_bindings')
-realm.run(['./t_bindings', '-s', server, '-', '-'], env=e, expected_msg='no')
-realm.run(['./t_bindings', '-s', server, 'a', '-'], env=e, expected_msg='no')
+realm.run(['./t_bindings', '-s', server, '-', '-'], env=e, expected_msg='yes')
+realm.run(['./t_bindings', '-s', server, 'a', '-'], env=e,
+          expected_code=1, expected_msg='Incorrect channel bindings')
 realm.run(['./t_bindings', '-s', server, 'a', 'a'], env=e, expected_msg='yes')
 realm.run(['./t_bindings', '-s', server, '-', 'a'], env=e,
           expected_code=1, expected_msg='Incorrect channel bindings')
