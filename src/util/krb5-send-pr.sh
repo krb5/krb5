ea7585ce3c8bc29b12cb2225fe5bc4d789544a08
Greg Hudson <ghudson@mit.edu>
2026-08-06 22:12:04 -0400

    Remove PGP request in krb5-send-pr.sh

--- src/util/krb5-send-pr.sh
+++ src/util/krb5-send-pr.sh
@@ -4,6 +4,6 @@ Please send email to krb5-bugs@mit.edu to report bugs.
 
 It is helpful to include the Kerberos version and operating system
 information with your bug report.  Please note that bug reports are
-public; if you are reporting a security vulnerability, send mail to
-krbcore-security@mit.edu instead, ideally using PGP encryption.
+public; if you are reporting a security vulnerability, send email to
+krbcore-security@mit.edu instead.
 EOF
